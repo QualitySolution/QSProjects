dbfd83348b5132dae09622cdf24c7b820c2be2e9
Gankov Andrey <gav@qsolution.ru>
2013-09-19 16:41:41 +0400

    Новый класс помощьник работы с БД.

---
+++ QSProjectsLib/DBWorks.cs
@@ -0,0 +1,45 @@
+using System;
+using MySql.Data.MySqlClient;
+
+namespace QSProjectsLib
+{
+	public class DBWorks
+	{
+		public DBWorks()
+		{
+		}
+
+		public static int GetInt(MySqlDataReader rdr, string Column, int NullValue)
+		{
+			if (rdr[Column] != DBNull.Value)
+				return rdr.GetInt32(Column);
+			else
+				return NullValue;
+		}
+
+		public static long GetLong(MySqlDataReader rdr, string Column, long NullValue)
+		{
+			if (rdr[Column] != DBNull.Value)
+				return rdr.GetInt64(Column);
+			else
+				return NullValue;
+		}
+
+		public static decimal GetDecimal(MySqlDataReader rdr, string Column, decimal NullValue)
+		{
+			if (rdr[Column] != DBNull.Value)
+				return rdr.GetDecimal(Column);
+			else
+				return NullValue;
+		}
+
+		public static DateTime GetDateTime(MySqlDataReader rdr, string Column, DateTime NullValue)
+		{
+			if (rdr[Column] != DBNull.Value)
+				return rdr.GetDateTime(Column);
+			else
+				return NullValue;
+		}
+	}
+}
+
