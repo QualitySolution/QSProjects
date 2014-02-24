04b9b75b5b913e58db83eedf2a12d8a808e59a20
Andrey Gankov <gav@qsolution.ru>
2014-02-24 23:33:05 +0400

    Вариант хрананения идентификационной онформации для виджетов.

---
+++ QSWidgetLib/WidgetId.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Gtk;
+
+namespace QSWidgetLib
+{
+	public class MenuItemId<I> : MenuItem
+	{
+		public I ID;
+
+		public MenuItemId(string label) : base (label) {}
+		public MenuItemId() : base () {}
+	}
+}
