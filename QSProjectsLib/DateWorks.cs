5f1dc0eaf2df3c79984beec2b68d48988e5a8afa
Andrey Gankov <gav@qsolution.ru>
2014-06-05 13:55:04 +0400

    Добавлена функция формирования строки диапазона дат.

---
+++ QSProjectsLib/DateWorks.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace QSProjectsLib
+{
+	public class DateWorks
+	{
+		/*public DateWorks()
+		{
+		}*/
+
+		public static string GetDateRangeText(DateTime dateS, DateTime dateE)
+		{
+			if(dateS == default(DateTime) || dateE == default(DateTime))
+				return "";
+			if (dateS.Year != dateE.Year)
+				return String.Format("{0:D}–{1:D}", dateS, dateE);
+			else if (dateS.Month != dateE.Month)
+				return String.Format("{0:dd MMMMM}–{1:D}", dateS, dateE);
+			else
+				return String.Format("{0:dd}–{1:D}", dateS, dateE);
+
+		}
+	}
+}
+
