c0157a209ac2e97d07233cb3e123f1fefd2da457
Andrey Gankov <gav@qsolution.ru>
2014-08-22 17:53:41 +0400

    Настройка NHibernate

---
+++ QSOrmProject/OrmMain.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace QSOrmProject
+{
+	public static class OrmMain
+	{
+		public static ISessionFactory Sessions;
+
+		public static void ConfigureOrm(string connectionString ,string[] assemblies)
+		{
+			var c = new Configuration(); 
+
+			c.Configure();
+			c.SetProperty("connection.connection_string", connectionString);
+
+			foreach(string ass in assemblies)
+			{
+				c.AddAssembly(ass);
+			}
+
+			Sessions = c.BuildSessionFactory ();
+		}
+	}
+}
+
