e40be85a311e3e9d7a92162ae1141c9013660377
Andrey Gankov <gav@qsolution.ru>
2014-09-05 10:17:58 +0400

    Набросок модуля банков

---
+++ Modules/QSBanks/AccountsView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace QSBanks
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class AccountsView : Gtk.Bin
+	{
+		public AccountsView()
+		{
+			this.Build();
+		}
+	}
+}
+
---
+++ Modules/QSBanks/gtk-gui/QSBanks.AccountsView.cs
@@ -0,0 +1,7 @@
+
+namespace QSBanks
+{
+	public partial class AccountsView
+	{
+	}
+}
