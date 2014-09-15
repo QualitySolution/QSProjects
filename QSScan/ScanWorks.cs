f8a31faf0f43fd2c65127d5d1a75d4bbf8f46291
Andrey Gankov <gav@qsolution.ru>
2014-09-15 13:31:07 +0300

    Пробуем получать изображение через буфер в памяти, частями.

--- QSScan/ScanWorks.cs
+++ QSScan/ScanWorks.cs
@@ -1,5 +1,6 @@
 using System;
 using System.IO;
+using System.Text;
 using System.Collections.Generic;
 using System.Drawing;
 using Gdk;
@@ -13,10 +14,13 @@ namespace QSScan
 	{
 		private static Logger logger = LogManager.GetCurrentClassLogger();
 		private bool WorkWithTwain;
-		Twain32 _twain32;
+		private Twain32 _twain32;
+		private MemoryStream stream;
+		private Twain32.ColorPalette palette;
 
 		public event EventHandler<ImageTransferEventArgs> ImageTransfer;
 		int TotalImages = -1;
+		int CurrentImage = 0;
 
 		public List<Pixbuf> Images;
 		private bool _isEnable=false;
@@ -57,6 +61,9 @@ namespace QSScan
 			if((e.TwainState&Twain32.TwainStateFlag.DSEnabled)==0&&this._isEnable) {
 				this._isEnable=false;
 				// <<< scaning finished (or closed)
+				if (stream != null)
+					FinishImageTransfer ();
+				logger.Debug ("Сканирование закончено...");
 				this.Close();
 			}
 			this._isEnable=(e.TwainState&Twain32.TwainStateFlag.DSEnabled)!=0;
@@ -126,10 +133,52 @@ namespace QSScan
 			{
 				if (AllAtOnce) // Получаем изображения или все сразу или по одному.
 					_twain32.AcquireCompleted += _twain32_AcquireCompleted;
-				else
-					_twain32.EndXfer += _twain32_EndXfer;
+				else 
+				{
+					//_twain32.EndXfer += _twain32_EndXfer;
+					_twain32.SetupMemXferEvent += OnSetupMemXferEvent;
+					_twain32.MemXferEvent += OnMemXferEvent;
+					_twain32.AcquireCompleted += _twain32_AcquireCompleted;
+				}
 				RunTwain();
 				//FIXME Здесь для повторного использования того же класса нужно почистить события.
+			}
+
+		}
+
+		void OnMemXferEvent (object sender, Twain32.MemXferEventArgs e)
+		{
+			try 
+			{
+				logger.Debug("On MemXfer Event {0} - {1}", e.ImageMemXfer.BytesWritten, e.ImageMemXfer.ImageData.Length);
+				int _bytesPerPixel=e.ImageInfo.BitsPerPixel>>3;
+				for(int i=0,_rowOffset=0; i<e.ImageMemXfer.Rows; i++,_rowOffset+=(int)e.ImageMemXfer.BytesPerRow) {
+					for(int ii=0,_colOffset=0; ii<e.ImageMemXfer.Columns; ii++,_colOffset+=_bytesPerPixel) {
+						switch(e.ImageInfo.BitsPerPixel) {
+						case 1:
+							for(int _mask=1; (_mask&0xff)!=0&&ii<e.ImageMemXfer.Columns; _mask<<=1,ii++) {
+								this.stream.WriteByte((e.ImageMemXfer.ImageData[_rowOffset+_colOffset]&_mask)!=0?byte.MaxValue:byte.MinValue);
+							}
+							_colOffset++;
+							ii--;
+							break;
+						case 8:
+						case 24:
+							if(e.ImageInfo.PixelType==TwPixelType.Palette) {
+								System.Drawing.Color _color=this.palette.Colors[e.ImageMemXfer.ImageData[_rowOffset+_colOffset]];
+								this.stream.Write(new byte[] { _color.R,_color.G,_color.B },0,3);
+							} else {
+								this.stream.Write(e.ImageMemXfer.ImageData,_rowOffset+_colOffset,_bytesPerPixel);
+							}
+							break;
+						}
+					}
+				}
+			} 
+			catch(Exception ex) 
+			{
+				logger.ErrorException ("Ошибка при получении изображения.", ex);
+				throw ex;
 			}
 
 		}
@@ -139,6 +188,7 @@ namespace QSScan
 			logger.Debug("Run Twain");
 			_twain32.OpenDataSource();
 			logger.Debug("DataSource is opened.");
+			_twain32.Capabilities.XferMech.Set (TwSX.Memory);
 			//Feeder
 			if((this._twain32.IsCapSupported(TwCap.Duplex)&TwQC.Get)!=0) {
 				var _duplexCapValue=(ushort)this._twain32.GetCap(TwCap.Duplex);
@@ -163,6 +213,63 @@ namespace QSScan
 			logger.Debug("Run Acquire");
 			_twain32.Acquire();
 			logger.Debug("After Acquire");
+		}
+
+		void OnSetupMemXferEvent (object sender, Twain32.SetupMemXferEventArgs e)
+		{
+			if (stream != null)
+				FinishImageTransfer ();
+			logger.Debug("SetupMemXfer");
+			try {
+				stream = new MemoryStream((int)e.BufferSize);
+				var _writer=new BinaryWriter(this.stream);
+				switch(e.ImageInfo.SamplesPerPixel) {
+				case 1:
+					if(e.ImageInfo.PixelType==TwPixelType.Palette) {
+						_writer.Write(Encoding.ASCII.GetBytes("P6\n"));
+						this.palette=this._twain32.Palette.Get();
+					} else {
+						_writer.Write(Encoding.ASCII.GetBytes("P5\n"));
+					}
+					break;
+				case 3:
+					_writer.Write(Encoding.ASCII.GetBytes("P6\n"));
+					break;
+				default:
+					_writer.Write(Encoding.ASCII.GetBytes("PX\n"));
+					break;
+				}
+				_writer.Write(Encoding.ASCII.GetBytes(string.Format("# (C) SARAFF SOFTWARE 2013.\n{0} {1}\n{2}\n",e.ImageInfo.ImageWidth,e.ImageInfo.ImageLength,byte.MaxValue)));
+			} catch(Exception ex) 
+			{
+				logger.ErrorException ("Ошибка при настройке буфера приема.", ex);
+				throw ex;
+			}
+
+		}
+
+		private void FinishImageTransfer()
+		{
+			if(stream != null) 
+			{
+				stream.Position = 0;
+				Pixbuf CurImg = new Pixbuf(stream);
+				stream.Close ();
+				stream = null;
+				if(ImageTransfer == null)
+				{// Записываем во внутренний массив
+					Images.Add(CurImg);
+				}
+				else
+				{// Передаем через событие
+					ImageTransferEventArgs arg = new ImageTransferEventArgs();
+					arg.AllImages = TotalImages;
+					arg.Image = CurImg;
+					CurrentImage++;
+					ImageTransfer(this, arg);
+				}
+			}
+
 		}
 
 		public void Close()
