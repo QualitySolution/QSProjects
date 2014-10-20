8d1f509444e6c13dec8e644b3c9aa5d5d6609384
anri-vin <anri.vin@gmail.com>
2014-10-20 13:53:17 +0300

    Added multiple phones support.

--- Modules/QSPhones/PhonesView.cs
+++ Modules/QSPhones/PhonesView.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Text.RegularExpressions;
+using Gtk;
+using Gtk.DataBindings;
 
 namespace QSPhones
 {
 	[System.ComponentModel.ToolboxItem (true)]
 	public partial class PhonesView : Gtk.Bin
 	{
+		uint RowNum;
+
 		public PhonesView ()
 		{
 			this.Build ();
 			entryPhone.IsEditable = true;
 			entryAdditional.IsEditable = true;
 			entryPhone.MaxLength = 19;
+			RowNum = datatable1.NRows;
+
+			//datat
+
 		}
 
 		protected void OnEntryPhoneTextInserted (object o, Gtk.TextInsertedArgs args)
 		{
-			FormatString ();
+			FormatString (o);
 			switch (args.Position) {
 			case 1:
 				args.Position += 1;
@@ -35,35 +43,35 @@ namespace QSPhones
 
 		protected void OnEntryPhoneTextDeleted (object o, Gtk.TextDeletedArgs args)
 		{
-			FormatString ();
-			if (args.StartPos > entryPhone.Text.Length)
-				entryPhone.Position = entryPhone.Text.Length;
+			FormatString (o);
+			if (args.StartPos > (o as DataEntry).Text.Length)
+				(o as DataEntry).Position = (o as DataEntry).Text.Length;
 			else
-				entryPhone.Position = args.StartPos;
+				(o as DataEntry).Position = args.StartPos;
 			if (args.StartPos == 16 && args.EndPos == 17) {			//Backspace
-				entryPhone.Text = entryPhone.Text.Remove (13, 1);
-				entryPhone.Position = 13;
+				(o as DataEntry).Text = (o as DataEntry).Text.Remove (13, 1);
+				(o as DataEntry).Position = 13;
 			} else if (args.StartPos == 11 && args.EndPos == 12) {
-				entryPhone.Text = entryPhone.Text.Remove (8, 1);
-				entryPhone.Position = 8;
+				(o as DataEntry).Text = (o as DataEntry).Text.Remove (8, 1);
+				(o as DataEntry).Position = 8;
 			} else if (args.StartPos == 5 && args.EndPos == 6) {
-				entryPhone.Text = entryPhone.Text.Remove (3, 1);
-				entryPhone.Position = 3;
+				(o as DataEntry).Text = (o as DataEntry).Text.Remove (3, 1);
+				(o as DataEntry).Position = 3;
 			} else if (args.StartPos == 14 && args.EndPos == 15) { 	//Delete
-				entryPhone.Text = entryPhone.Text.Remove (17, 1);
-				entryPhone.Position = 17;
+				(o as DataEntry).Text = (o as DataEntry).Text.Remove (17, 1);
+				(o as DataEntry).Position = 17;
 			} else if (args.StartPos == 9 && args.EndPos == 10) {
-				entryPhone.Text = entryPhone.Text.Remove (12, 1);
-				entryPhone.Position = 12;
+				(o as DataEntry).Text = (o as DataEntry).Text.Remove (12, 1);
+				(o as DataEntry).Position = 12;
 			} else if (args.StartPos == 4 && args.EndPos == 5) {
-				entryPhone.Text = entryPhone.Text.Remove (6, 1);
-				entryPhone.Position = 6;
+				(o as DataEntry).Text = (o as DataEntry).Text.Remove (6, 1);
+				(o as DataEntry).Position = 6;
 			}
 		}
 
-		private void FormatString()
+		private void FormatString(object o)
 		{
-			string Number = entryPhone.Text;
+			string Number = (o as DataEntry).Text;
 			Number = Regex.Replace (Number, "[^0-9]", "");
 			if (Number != String.Empty) {
 				if (Number.Length > 0)
@@ -74,8 +82,93 @@ namespace QSPhones
 					Number = Number.Insert (9, " - ");
 				if (Number.Length > 14)
 					Number = Number.Insert (14, " - ");
-				entryPhone.Text = Number;
+				(o as DataEntry).Text = Number;
 			}
+		}
+
+		protected void OnButtonAddClicked (object sender, EventArgs e)
+		{
+			datatable1.NRows = RowNum + 1;
+
+			DataComboBox phoneDataCombo = new DataComboBox ();
+			phoneDataCombo.InheritedDataSource = false;
+			phoneDataCombo.InheritedBoundaryDataSource = false;
+			phoneDataCombo.CursorPointsEveryType = false;
+			phoneDataCombo.InheritedDataSource = false;
+			phoneDataCombo.InheritedBoundaryDataSource = false;
+			phoneDataCombo.WidthRequest = 100;
+			datatable1.Attach (phoneDataCombo, (uint)0, (uint)1, RowNum, RowNum + 1, AttachOptions.Fill | AttachOptions.Expand, (AttachOptions)0, (uint)0, (uint)0);
+
+
+			Gtk.Label textPhoneLabel = new Gtk.Label ("+7");
+			datatable1.Attach (textPhoneLabel, (uint)1, (uint)2, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+
+			DataEntry phoneDataEntry = new DataEntry ();
+			phoneDataEntry.CanFocus = true;
+			phoneDataEntry.IsEditable = true;
+			phoneDataEntry.WidthChars = 19;
+			phoneDataEntry.InheritedDataSource = false;
+			phoneDataEntry.InheritedBoundaryDataSource = false;
+			phoneDataEntry.InheritedDataSource = false;
+			phoneDataEntry.InheritedBoundaryDataSource = false;
+			phoneDataEntry.TextInserted += OnEntryPhoneTextInserted;
+			phoneDataEntry.TextDeleted += OnEntryPhoneTextDeleted;
+			datatable1.Attach (phoneDataEntry, (uint)2, (uint)3, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+
+			Gtk.Label textAdditionalLabel = new Gtk.Label ("доб.");
+			datatable1.Attach (textAdditionalLabel, (uint)3, (uint)4, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+
+			DataEntry additionalDataEntry = new DataEntry ();
+			additionalDataEntry.WidthRequest = 50;
+			additionalDataEntry.CanFocus = true;
+			additionalDataEntry.IsEditable = true;
+			additionalDataEntry.InheritedDataSource = false;
+			additionalDataEntry.InheritedBoundaryDataSource = false;
+			additionalDataEntry.InheritedDataSource = false;
+			additionalDataEntry.InheritedBoundaryDataSource = false;
+			datatable1.Attach (additionalDataEntry, (uint)4, (uint)5, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+
+			Gtk.Button deleteButton = new Gtk.Button ();
+			deleteButton.CanFocus = true;
+			deleteButton.UseUnderline = true;
+			Gtk.Image image = new Gtk.Image ();
+			image.Pixbuf = global::Stetic.IconLoader.LoadIcon (this, "gtk-cancel", global::Gtk.IconSize.Menu);
+			deleteButton.Image = image;
+			deleteButton.Clicked += OnButtonDeleteClicked;
+			datatable1.Attach (deleteButton, (uint)5, (uint)6, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+
+			datatable1.ShowAll ();
+
+			RowNum++;
+		}
+
+		protected void OnButtonDeleteClicked (object sender, EventArgs e)
+		{
+			uint Row;
+			Table.TableChild child = ((Table.TableChild)(this.datatable1 [(Widget)sender]));
+			Row = child.TopAttach;
+			foreach (Widget w in datatable1.Children)
+				if (((Table.TableChild)(this.datatable1 [w])).TopAttach == Row) {
+					datatable1.Remove (w);
+					w.Destroy ();
+				}
+			for (uint i = Row + 1; i < datatable1.NRows; i++)
+				MoveRowUp (i);
+			datatable1.NRows = --RowNum;
+		}
+
+		protected void MoveRowUp(uint Row)
+		{
+			foreach (Widget w in datatable1.Children)
+				if (((Table.TableChild)(this.datatable1 [w])).TopAttach == Row) {
+					uint Left = ((Table.TableChild)(this.datatable1 [w])).LeftAttach;
+					uint Right = ((Table.TableChild)(this.datatable1 [w])).RightAttach;
+					datatable1.Remove (w);
+					if (w.GetType() == typeof(DataComboBox))
+						datatable1.Attach (w, Left, Right, Row - 1, Row, AttachOptions.Fill | AttachOptions.Expand, (AttachOptions)0, (uint)0, (uint)0);
+					else
+						datatable1.Attach (w, Left, Right, Row - 1, Row, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+				}
 		}
 	}
 }
--- Modules/QSPhones/gtk-gui/QSPhones.PhonesView.cs
+++ Modules/QSPhones/gtk-gui/QSPhones.PhonesView.cs
@@ -4,7 +4,11 @@ namespace QSPhones
 {
 	public partial class PhonesView
 	{
+		private global::Gtk.VBox vbox1;
+		
 		private global::Gtk.DataBindings.DataTable datatable1;
+		
+		private global::Gtk.Button buttonDelete;
 		
 		private global::Gtk.DataBindings.DataComboBox comboType;
 		
@@ -15,6 +19,8 @@ namespace QSPhones
 		private global::Gtk.Label label1;
 		
 		private global::Gtk.Label label2;
+		
+		private global::Gtk.Button buttonAdd;
 
 		protected virtual void Build ()
 		{
@@ -23,14 +29,30 @@ namespace QSPhones
 			global::Stetic.BinContainer.Attach (this);
 			this.Name = "QSPhones.PhonesView";
 			// Container child QSPhones.PhonesView.Gtk.Container+ContainerChild
-			this.datatable1 = new global::Gtk.DataBindings.DataTable (((uint)(1)), ((uint)(5)), false);
+			this.vbox1 = new global::Gtk.VBox ();
+			this.vbox1.Name = "vbox1";
+			// Container child vbox1.Gtk.Box+BoxChild
+			this.datatable1 = new global::Gtk.DataBindings.DataTable (((uint)(1)), ((uint)(6)), false);
 			this.datatable1.Name = "datatable1";
-			this.datatable1.RowSpacing = ((uint)(6));
 			this.datatable1.ColumnSpacing = ((uint)(6));
 			this.datatable1.InheritedDataSource = false;
 			this.datatable1.InheritedBoundaryDataSource = false;
 			this.datatable1.InheritedDataSource = false;
 			this.datatable1.InheritedBoundaryDataSource = false;
+			// Container child datatable1.Gtk.Table+TableChild
+			this.buttonDelete = new global::Gtk.Button ();
+			this.buttonDelete.CanFocus = true;
+			this.buttonDelete.Name = "buttonDelete";
+			this.buttonDelete.UseUnderline = true;
+			global::Gtk.Image w1 = new global::Gtk.Image ();
+			w1.Pixbuf = global::Stetic.IconLoader.LoadIcon (this, "gtk-cancel", global::Gtk.IconSize.Menu);
+			this.buttonDelete.Image = w1;
+			this.datatable1.Add (this.buttonDelete);
+			global::Gtk.Table.TableChild w2 = ((global::Gtk.Table.TableChild)(this.datatable1 [this.buttonDelete]));
+			w2.LeftAttach = ((uint)(5));
+			w2.RightAttach = ((uint)(6));
+			w2.XOptions = ((global::Gtk.AttachOptions)(0));
+			w2.YOptions = ((global::Gtk.AttachOptions)(0));
 			// Container child datatable1.Gtk.Table+TableChild
 			this.comboType = new global::Gtk.DataBindings.DataComboBox ();
 			this.comboType.WidthRequest = 100;
@@ -41,9 +63,8 @@ namespace QSPhones
 			this.comboType.InheritedDataSource = false;
 			this.comboType.InheritedBoundaryDataSource = false;
 			this.datatable1.Add (this.comboType);
-			global::Gtk.Table.TableChild w1 = ((global::Gtk.Table.TableChild)(this.datatable1 [this.comboType]));
-			w1.XOptions = ((global::Gtk.AttachOptions)(4));
-			w1.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w3 = ((global::Gtk.Table.TableChild)(this.datatable1 [this.comboType]));
+			w3.YOptions = ((global::Gtk.AttachOptions)(0));
 			// Container child datatable1.Gtk.Table+TableChild
 			this.entryAdditional = new global::Gtk.DataBindings.DataEntry ();
 			this.entryAdditional.WidthRequest = 50;
@@ -57,11 +78,11 @@ namespace QSPhones
 			this.entryAdditional.InheritedBoundaryDataSource = false;
 			this.entryAdditional.Editable = false;
 			this.datatable1.Add (this.entryAdditional);
-			global::Gtk.Table.TableChild w2 = ((global::Gtk.Table.TableChild)(this.datatable1 [this.entryAdditional]));
-			w2.LeftAttach = ((uint)(4));
-			w2.RightAttach = ((uint)(5));
-			w2.XOptions = ((global::Gtk.AttachOptions)(4));
-			w2.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w4 = ((global::Gtk.Table.TableChild)(this.datatable1 [this.entryAdditional]));
+			w4.LeftAttach = ((uint)(4));
+			w4.RightAttach = ((uint)(5));
+			w4.XOptions = ((global::Gtk.AttachOptions)(0));
+			w4.YOptions = ((global::Gtk.AttachOptions)(0));
 			// Container child datatable1.Gtk.Table+TableChild
 			this.entryPhone = new global::Gtk.DataBindings.DataEntry ();
 			this.entryPhone.CanFocus = true;
@@ -75,39 +96,60 @@ namespace QSPhones
 			this.entryPhone.InheritedBoundaryDataSource = false;
 			this.entryPhone.Editable = false;
 			this.datatable1.Add (this.entryPhone);
-			global::Gtk.Table.TableChild w3 = ((global::Gtk.Table.TableChild)(this.datatable1 [this.entryPhone]));
-			w3.LeftAttach = ((uint)(2));
-			w3.RightAttach = ((uint)(3));
-			w3.XOptions = ((global::Gtk.AttachOptions)(4));
-			w3.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w5 = ((global::Gtk.Table.TableChild)(this.datatable1 [this.entryPhone]));
+			w5.LeftAttach = ((uint)(2));
+			w5.RightAttach = ((uint)(3));
+			w5.XOptions = ((global::Gtk.AttachOptions)(0));
+			w5.YOptions = ((global::Gtk.AttachOptions)(0));
 			// Container child datatable1.Gtk.Table+TableChild
 			this.label1 = new global::Gtk.Label ();
 			this.label1.Name = "label1";
 			this.label1.Xalign = 1F;
 			this.label1.LabelProp = global::Mono.Unix.Catalog.GetString ("+7");
 			this.datatable1.Add (this.label1);
-			global::Gtk.Table.TableChild w4 = ((global::Gtk.Table.TableChild)(this.datatable1 [this.label1]));
-			w4.LeftAttach = ((uint)(1));
-			w4.RightAttach = ((uint)(2));
-			w4.XOptions = ((global::Gtk.AttachOptions)(4));
-			w4.YOptions = ((global::Gtk.AttachOptions)(4));
+			global::Gtk.Table.TableChild w6 = ((global::Gtk.Table.TableChild)(this.datatable1 [this.label1]));
+			w6.LeftAttach = ((uint)(1));
+			w6.RightAttach = ((uint)(2));
+			w6.XOptions = ((global::Gtk.AttachOptions)(0));
+			w6.YOptions = ((global::Gtk.AttachOptions)(0));
 			// Container child datatable1.Gtk.Table+TableChild
 			this.label2 = new global::Gtk.Label ();
 			this.label2.Name = "label2";
 			this.label2.LabelProp = global::Mono.Unix.Catalog.GetString ("доб.");
 			this.datatable1.Add (this.label2);
-			global::Gtk.Table.TableChild w5 = ((global::Gtk.Table.TableChild)(this.datatable1 [this.label2]));
-			w5.LeftAttach = ((uint)(3));
-			w5.RightAttach = ((uint)(4));
-			w5.XOptions = ((global::Gtk.AttachOptions)(4));
-			w5.YOptions = ((global::Gtk.AttachOptions)(4));
-			this.Add (this.datatable1);
+			global::Gtk.Table.TableChild w7 = ((global::Gtk.Table.TableChild)(this.datatable1 [this.label2]));
+			w7.LeftAttach = ((uint)(3));
+			w7.RightAttach = ((uint)(4));
+			w7.XOptions = ((global::Gtk.AttachOptions)(0));
+			w7.YOptions = ((global::Gtk.AttachOptions)(0));
+			this.vbox1.Add (this.datatable1);
+			global::Gtk.Box.BoxChild w8 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.datatable1]));
+			w8.Position = 0;
+			w8.Expand = false;
+			w8.Fill = false;
+			// Container child vbox1.Gtk.Box+BoxChild
+			this.buttonAdd = new global::Gtk.Button ();
+			this.buttonAdd.CanFocus = true;
+			this.buttonAdd.Name = "buttonAdd";
+			this.buttonAdd.UseUnderline = true;
+			this.buttonAdd.Label = global::Mono.Unix.Catalog.GetString ("Добавить");
+			global::Gtk.Image w9 = new global::Gtk.Image ();
+			w9.Pixbuf = global::Stetic.IconLoader.LoadIcon (this, "gtk-add", global::Gtk.IconSize.Menu);
+			this.buttonAdd.Image = w9;
+			this.vbox1.Add (this.buttonAdd);
+			global::Gtk.Box.BoxChild w10 = ((global::Gtk.Box.BoxChild)(this.vbox1 [this.buttonAdd]));
+			w10.Position = 1;
+			w10.Expand = false;
+			w10.Fill = false;
+			this.Add (this.vbox1);
 			if ((this.Child != null)) {
 				this.Child.ShowAll ();
 			}
 			this.Hide ();
 			this.entryPhone.TextInserted += new global::Gtk.TextInsertedHandler (this.OnEntryPhoneTextInserted);
 			this.entryPhone.TextDeleted += new global::Gtk.TextDeletedHandler (this.OnEntryPhoneTextDeleted);
+			this.buttonDelete.Clicked += new global::System.EventHandler (this.OnButtonDeleteClicked);
+			this.buttonAdd.Clicked += new global::System.EventHandler (this.OnButtonAddClicked);
 		}
 	}
 }
