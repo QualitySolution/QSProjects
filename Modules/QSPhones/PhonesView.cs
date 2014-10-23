259ab777bc2965f4a3bc63f9d3a21dcc8ba4e7eb
Andrey Gankov <gav@qsolution.ru>
2014-10-23 13:18:47 +0300

    Использование ValidationEntry

--- Modules/QSPhones/PhonesView.cs
+++ Modules/QSPhones/PhonesView.cs
@@ -1,13 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Data.Bindings;
+using System.Data.Bindings.Collections;
+using System.Data.Bindings.Collections.Generic;
 using System.Text.RegularExpressions;
 using Gtk;
 using Gtk.DataBindings;
 using NHibernate;
-using System.Data.Bindings.Collections;
-using System.Collections.Generic;
-using System.Data.Bindings.Collections.Generic;
-using System.Data.Bindings;
 using NLog;
+using QSOrmProject;
+using QSWidgetLib;
 
 namespace QSPhones
 {
@@ -98,71 +100,6 @@ namespace QSPhones
 			datatablePhones.NRows = RowNum = 0;
 		}
 
-		protected void OnEntryPhoneTextInserted (object o, Gtk.TextInsertedArgs args)
-		{
-			FormatString (o);
-			switch (args.Position) {
-			case 1:
-				args.Position += 1;
-				break;
-			case 5:
-				args.Position += 2;
-				break;
-			case 10:
-				args.Position += 3;
-				break;
-			case 15:
-				args.Position += 3;
-				break;
-			}
-		}
-
-		protected void OnEntryPhoneTextDeleted (object o, Gtk.TextDeletedArgs args)
-		{
-			FormatString (o);
-			DataEntry Entry = o as DataEntry;
-			if (args.StartPos > Entry.Text.Length)
-				Entry.Position = Entry.Text.Length;
-			else
-				Entry.Position = args.StartPos;
-			if (args.StartPos == 16 && args.EndPos == 17) {			//Backspace
-				Entry.Text = Entry.Text.Remove (13, 1);
-				Entry.Position = 13;
-			} else if (args.StartPos == 11 && args.EndPos == 12) {
-				Entry.Text = Entry.Text.Remove (8, 1);
-				Entry.Position = 8;
-			} else if (args.StartPos == 5 && args.EndPos == 6) {
-				Entry.Text = Entry.Text.Remove (3, 1);
-				Entry.Position = 3;
-			} else if (args.StartPos == 14 && args.EndPos == 15) { 	//Delete
-				Entry.Text = Entry.Text.Remove (17, 1);
-				Entry.Position = 17;
-			} else if (args.StartPos == 9 && args.EndPos == 10) {
-				Entry.Text = Entry.Text.Remove (12, 1);
-				Entry.Position = 12;
-			} else if (args.StartPos == 4 && args.EndPos == 5) {
-				Entry.Text = Entry.Text.Remove (6, 1);
-				Entry.Position = 6;
-			}
-		}
-
-		private void FormatString(object o)
-		{
-			string Number = (o as DataEntry).Text;
-			Number = Regex.Replace (Number, "[^0-9]", "");
-			if (Number != String.Empty) {
-				if (Number.Length > 0)
-					Number = Number.Insert (0, "(");
-				if (Number.Length > 4)
-					Number = Number.Insert (4, ") ");
-				if (Number.Length > 9)
-					Number = Number.Insert (9, " - ");
-				if (Number.Length > 14)
-					Number = Number.Insert (14, " - ");
-			}
-			(o as DataEntry).Text = Number;
-		}
-
 		protected void OnButtonAddClicked (object sender, EventArgs e)
 		{
 			PhonesList.Add(new Phone());
@@ -182,21 +119,18 @@ namespace QSPhones
 			Gtk.Label textPhoneLabel = new Gtk.Label ("+7");
 			datatablePhones.Attach (textPhoneLabel, (uint)1, (uint)2, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
 
-			DataEntry phoneDataEntry = new DataEntry (rowAdaptor, "Number");
-			phoneDataEntry.CanFocus = true;
-			phoneDataEntry.IsEditable = true;
+			DataValidatedEntry phoneDataEntry = new DataValidatedEntry (rowAdaptor, "Number");
+			phoneDataEntry.ValidationMode = ValidationType.phone;
 			phoneDataEntry.WidthChars = 19;
-			phoneDataEntry.MaxLength = 19;
-			phoneDataEntry.TextInserted += OnEntryPhoneTextInserted;
-			phoneDataEntry.TextDeleted += OnEntryPhoneTextDeleted;
-			datatablePhones.Attach (phoneDataEntry, (uint)2, (uint)3, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+			datatablePhones.Attach (phoneDataEntry, (uint)2, (uint)3, RowNum, RowNum + 1, AttachOptions.Expand | AttachOptions.Fill, (AttachOptions)0, (uint)0, (uint)0);
 
 			Gtk.Label textAdditionalLabel = new Gtk.Label ("доб.");
 			datatablePhones.Attach (textAdditionalLabel, (uint)3, (uint)4, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
 
 			DataEntry additionalDataEntry = new DataEntry (rowAdaptor,"Additional");
 			additionalDataEntry.WidthRequest = 50;
-			datatablePhones.Attach (additionalDataEntry, (uint)4, (uint)5, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+			additionalDataEntry.MaxLength = 10;
+			datatablePhones.Attach (additionalDataEntry, (uint)4, (uint)5, RowNum, RowNum + 1, AttachOptions.Expand | AttachOptions.Fill, (AttachOptions)0, (uint)0, (uint)0);
 
 			Gtk.Button deleteButton = new Gtk.Button ();
 			Gtk.Image image = new Gtk.Image ();
