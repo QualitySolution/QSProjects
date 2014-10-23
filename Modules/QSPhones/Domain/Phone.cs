9ded30eaaa1a14e6507e0ccb5df1407aebeb6e25
Andrey Gankov <gav@qsolution.ru>
2014-10-23 11:25:12 +0300

    Возможность, обновлять тип телефонов.

--- Modules/QSPhones/Domain/Phone.cs
+++ Modules/QSPhones/Domain/Phone.cs
@@ -1,16 +1,32 @@
 ﻿using System;
 using QSOrmProject;
+using System.Data.Bindings;
 
 namespace QSPhones
 {
 	[OrmSubjectAttibutes("Телефоны")]
-	public class Phone
+	public class Phone : BaseNotifyPropertyChanged
 	{
 		#region Свойства
 		public virtual int Id { get; set; }
 		public virtual string Number { get; set; }
 		public virtual string Additional { get; set; }
-		public virtual PhoneType NumberType { get; set; }
+
+		private PhoneType numberType;
+		public virtual PhoneType NumberType
+		{
+			get
+			{
+				return numberType;
+			}
+			set
+			{
+				if (numberType == value)
+					return;
+				numberType = value;
+				OnPropertyChanged("NumberType");
+			}
+		}
 		#endregion
 
 		public Phone()
