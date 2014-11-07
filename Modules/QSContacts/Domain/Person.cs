6872655e04b3104ba36a474aa44d766ddec82efd
anri-vin <anri.vin@gmail.com>
2014-11-07 14:09:14 +0300

    Добавлен виджет с ФИО человека.

---
+++ Modules/QSContacts/Domain/Person.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using QSOrmProject;
+using System.Collections.Generic;
+
+namespace QSContacts
+{
+	[OrmSubjectAttibutes("Сотрудники")]
+	public class Person
+	{
+		#region Свойства
+		public virtual int Id { get; set; }
+		public virtual string Name { get; set; }
+		public virtual string Surname { get; set; }
+		public virtual string Lastname { get; set; } 
+		#endregion
+
+		public Person ()
+		{
+			Name = String.Empty;
+			Surname = String.Empty;
+			Lastname = String.Empty;
+		}
+	}
+}
+
---
+++ Modules/QSContacts/PersonsView.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using NLog;
+using NHibernate;
+using System.Data.Bindings.Collections.Generic;
+using System.Collections.Generic;
+using System.Data.Bindings;
+using Gtk;
+using QSOrmProject;
+using QSWidgetLib;
+
+namespace QSContacts
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	public partial class PersonsView : Gtk.Bin
+	{
+		private static Logger logger = LogManager.GetCurrentClassLogger();
+		private ISession session;
+		private GenericObservableList<Person> PersonsList;
+
+		public ISession Session
+		{
+			get {
+				return session;
+			}
+			set {
+				session = value;
+			}
+		}
+
+		private IList<Person> persons;
+		public IList<Person> Persons
+		{
+			get {
+				return persons;
+			}
+			set {
+				if (persons == value)
+					return;
+				if (PersonsList != null)
+					CleanList();
+				persons = value;
+				buttonAdd.Sensitive = persons != null;
+				if (value != null) {
+					PersonsList = new GenericObservableList<Person> (persons);
+					PersonsList.ElementAdded += OnPersonsListElementAdded;
+					PersonsList.ElementRemoved += OnPersonsListElementRemoved;
+					if (PersonsList.Count == 0)
+						PersonsList.Add(new Person());
+					else
+					{
+						foreach (Person person in PersonsList)
+							AddPersonRow(person);
+					}
+				}
+			}
+		}
+
+		void OnPersonsListElementRemoved (object aList, int[] aIdx, object aObject)
+		{
+			Widget foundWidget = null;
+			foreach(Widget wid in datatablePersons.AllChildren)
+			{
+				if(wid is IAdaptableContainer && (wid as IAdaptableContainer).Adaptor.Adaptor.FinalTarget == aObject)
+				{
+					foundWidget = wid;
+					break;
+				}
+			}
+			if(foundWidget == null)
+			{
+				logger.Warn("Не найден виджет ассоциированный с удаленным телефоном.");
+				return;
+			}
+
+			Table.TableChild child = ((Table.TableChild)(this.datatablePersons [foundWidget]));
+			RemoveRow(child.TopAttach);
+		}
+
+		void OnPersonsListElementAdded (object aList, int[] aIdx)
+		{
+			foreach(int i in aIdx)
+			{
+				AddPersonRow(PersonsList[i]);
+			}
+		}
+
+		uint RowNum;
+
+		public PersonsView ()
+		{
+			this.Build ();
+			datatablePersons.NRows = RowNum = 0;
+		}
+
+		protected void OnButtonAddClicked (object sender, EventArgs e)
+		{
+			PersonsList.Add(new Person());
+		}
+
+		private void AddPersonRow(Person newPerson) 
+		{
+			datatablePersons.NRows = RowNum + 1;
+			Adaptor rowAdaptor = new Adaptor(newPerson);
+
+			Gtk.Label labelSurame = new Gtk.Label ("Фамилия:");
+			datatablePersons.Attach (labelSurame, (uint)0, (uint)1, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+
+			DataValidatedEntry nameDataEntry = new DataValidatedEntry (rowAdaptor, "Surname");
+			nameDataEntry.WidthChars = 20;
+			datatablePersons.Attach (nameDataEntry, (uint)1, (uint)2, RowNum, RowNum + 1, AttachOptions.Expand | AttachOptions.Fill, (AttachOptions)0, (uint)0, (uint)0);
+
+			Gtk.Label labelName = new Gtk.Label ("Имя:");
+			datatablePersons.Attach (labelName, (uint)2, (uint)3, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+
+			DataValidatedEntry surnameDataEntry = new DataValidatedEntry (rowAdaptor, "Name");
+			nameDataEntry.WidthChars = 20;
+			datatablePersons.Attach (surnameDataEntry, (uint)3, (uint)4, RowNum, RowNum + 1, AttachOptions.Expand | AttachOptions.Fill, (AttachOptions)0, (uint)0, (uint)0);
+
+			Gtk.Label labelLastname = new Gtk.Label ("Отчество:");
+			datatablePersons.Attach (labelLastname, (uint)4, (uint)5, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+
+			DataValidatedEntry lastnameDataEntry = new DataValidatedEntry (rowAdaptor, "Lastname");
+			nameDataEntry.WidthChars = 20;
+			datatablePersons.Attach (lastnameDataEntry, (uint)5, (uint)6, RowNum, RowNum + 1, AttachOptions.Expand | AttachOptions.Fill, (AttachOptions)0, (uint)0, (uint)0);
+
+			Gtk.Button deleteButton = new Gtk.Button ();
+			Gtk.Image image = new Gtk.Image ();
+			image.Pixbuf = Stetic.IconLoader.LoadIcon (this, "gtk-delete", global::Gtk.IconSize.Menu);
+			deleteButton.Image = image;
+			deleteButton.Clicked += OnButtonDeleteClicked;
+			datatablePersons.Attach (deleteButton, (uint)6, (uint)7, RowNum, RowNum + 1, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+
+			datatablePersons.ShowAll ();
+
+			RowNum++;
+		}
+
+		protected void OnButtonDeleteClicked (object sender, EventArgs e)
+		{
+			Table.TableChild delButtonInfo = ((Table.TableChild)(this.datatablePersons [(Widget)sender]));
+			Widget foundWidget = null;
+			foreach(Widget wid in datatablePersons.AllChildren)
+			{
+				if(wid is IAdaptableContainer && delButtonInfo.TopAttach == (datatablePersons[wid] as Table.TableChild).TopAttach)
+				{
+					foundWidget = wid;
+					break;
+				}
+			}
+			if(foundWidget == null)
+			{
+				logger.Warn("Не найден виджет ассоциированный с удаленным человеком.");
+				return;
+			}
+
+			PersonsList.Remove((Person)(foundWidget as IAdaptableContainer).Adaptor.Adaptor.FinalTarget);
+		}
+
+		private void RemoveRow(uint Row)
+		{
+			foreach (Widget w in datatablePersons.Children)
+				if (((Table.TableChild)(this.datatablePersons [w])).TopAttach == Row) {
+					datatablePersons.Remove (w);
+					w.Destroy ();
+				}
+			for (uint i = Row + 1; i < datatablePersons.NRows; i++)
+				MoveRowUp (i);
+			datatablePersons.NRows = --RowNum;
+		}
+
+		protected void MoveRowUp(uint Row)
+		{
+			foreach (Widget w in datatablePersons.Children)
+				if (((Table.TableChild)(this.datatablePersons [w])).TopAttach == Row) {
+					uint Left = ((Table.TableChild)(this.datatablePersons [w])).LeftAttach;
+					uint Right = ((Table.TableChild)(this.datatablePersons [w])).RightAttach;
+					datatablePersons.Remove (w);
+					datatablePersons.Attach (w, Left, Right, Row - 1, Row, (AttachOptions)0, (AttachOptions)0, (uint)0, (uint)0);
+				}
+		}
+
+		private void CleanList()
+		{
+			while (PersonsList.Count > 0)
+			{
+				PersonsList.RemoveAt(0);
+			}
+		}
+
+		public void SaveChanges()
+		{
+			foreach(Person person in PersonsList)
+			{
+				if(person.Name != String.Empty && person.Surname != String.Empty)
+					Session.SaveOrUpdate(person);
+			}
+		}
+
+	}
+}
+
