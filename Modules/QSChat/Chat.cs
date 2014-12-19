f1190c813b98f3e70fd5a00520d175ab2d040f36
Andrey Gankov <gav@qsolution.ru>
2014-12-19 16:21:08 +0300

    Вылетевший комит.

--- Modules/QSChat/Chat.cs
+++ Modules/QSChat/Chat.cs
@@ -103,7 +103,7 @@ namespace QSChat
 			TextBuffer tempBuffer = new TextBuffer(textTags);
 			TextIter iter = tempBuffer.EndIter;
 			NewMessageCount = 0;
-			DateTime MaxDate = DateTime.MinValue;
+			DateTime MaxDate = default(DateTime);
 			using (MySqlDataReader rdr = cmd.ExecuteReader())
 			{
 				while (rdr.Read())
