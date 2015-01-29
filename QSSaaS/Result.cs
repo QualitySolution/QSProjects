67032b48df0b3e15a245cfb3c89150ba1e6cabf1
anri-vin <anri.vin@gmail.com>
2015-01-29 10:17:19 +0300

    Проект для сервиса SaaS

---
+++ QSSaaS/Result.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace QSSaaS
+{
+	[DataContract]
+	public class Result
+	{
+		[DataMember]
+		public bool Success;
+		[DataMember]
+		public string Description;
+
+		public Result (bool success, string description = "")
+		{
+			Success = success;
+			Description = description;
+		}
+	}
+
+	[DataContract]
+	public class AccountAuthResult : Result
+	{
+		[DataMember]
+		public string SessionID;
+
+		public AccountAuthResult (bool success, string description = "") : base (success, description)
+		{
+		}
+
+		public AccountAuthResult (bool success, string sessionID, string description) : base (success, description)
+		{
+			SessionID = sessionID;
+		}
+	}
+
+	[DataContract]
+	public class UserAuthResult : AccountAuthResult
+	{
+		[DataMember]
+		public string Server;
+		[DataMember]
+		public bool IsAdmin;
+		[DataMember]
+		public string Login;
+
+		public UserAuthResult (bool success, string description = "") : base (success, description)
+		{
+		}
+
+		public UserAuthResult (bool success, string sessionID, string server, string login, bool isAdmin = false) : base (success, sessionID, "")
+		{
+			Login = login;
+			Server = server;
+			IsAdmin = isAdmin;
+		}
+	}
+}
+
