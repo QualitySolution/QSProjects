cf8a483273c55deed9f55387e368eed43187d1d0
anri-vin <anri.vin@gmail.com>
2015-01-29 10:16:18 +0300

    Проект для сервиса обновлений.

---
+++ QSUpdater/Result.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace QSUpdater
+{
+	[DataContract]
+	public class UpdateResult
+	{
+		[DataMember]
+		public bool HasUpdate;
+		[DataMember]
+		public string NewVersion;
+		[DataMember]
+		public string FileLink;
+		[DataMember]
+		public string InfoLink;
+		[DataMember]
+		public string UpdateDescription;
+
+		public UpdateResult (bool hasUpdate, string newVersion = "", string fileLink = "", string infoLink = "", string updateDescription = "")
+		{
+			HasUpdate = hasUpdate;
+			NewVersion = newVersion;
+			FileLink = fileLink;
+			InfoLink = infoLink;
+			UpdateDescription = updateDescription;
+		}
+	}
+}
+
