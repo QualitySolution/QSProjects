55f2a180c607117443fd96920bc622247ef11d9b
anri-vin <anri.vin@gmail.com>
2015-02-03 17:26:04 +0300

    Добавлено удаление из SaaS

--- QSProjectsLib/Users.cs
+++ QSProjectsLib/Users.cs
@@ -1,6 +1,5 @@
 using System;
 using Gtk;
-using MySql.Data;
 using MySql.Data.MySqlClient;
 using NLog;
 
@@ -8,118 +7,118 @@ namespace QSProjectsLib
 {
 	public partial class Users : Gtk.Dialog
 	{
-		private static Logger logger = LogManager.GetCurrentClassLogger();
+		private static Logger logger = LogManager.GetCurrentClassLogger ();
 		Gtk.ListStore UsersListStore;
-		
+
 		public Users ()
 		{
 			this.Build ();
 			
 			//Создаем таблицу "Пользователей"
-			UsersListStore = new Gtk.ListStore (typeof (int), typeof (string), typeof (string), 
-			                                    typeof (bool));
+			UsersListStore = new Gtk.ListStore (typeof(int), typeof(string), typeof(string), 
+			                                    typeof(bool));
 			
-			treeviewUsers.AppendColumn("Код", new Gtk.CellRendererText (), "text", 0);
-			treeviewUsers.AppendColumn("Логин", new Gtk.CellRendererText (), "text", 1);
-			treeviewUsers.AppendColumn("Имя", new Gtk.CellRendererText (), "text", 2);
-			treeviewUsers.AppendColumn("Администратор", new Gtk.CellRendererToggle (), "active", 3);
+			treeviewUsers.AppendColumn ("Код", new Gtk.CellRendererText (), "text", 0);
+			treeviewUsers.AppendColumn ("Логин", new Gtk.CellRendererText (), "text", 1);
+			treeviewUsers.AppendColumn ("Имя", new Gtk.CellRendererText (), "text", 2);
+			treeviewUsers.AppendColumn ("Администратор", new Gtk.CellRendererToggle (), "active", 3);
 			
 			treeviewUsers.Model = UsersListStore;
-			treeviewUsers.ShowAll();
+			treeviewUsers.ShowAll ();
 			UpdateUsers ();
 		}
-		
-		void UpdateUsers()
+
+		void UpdateUsers ()
 		{
-			if(!QSMain.TestConnection ())
+			if (!QSMain.TestConnection ())
 				return;
-			QSMain.CheckConnectionAlive();
-			logger.Info("Получаем таблицу пользователей...");
+			QSMain.CheckConnectionAlive ();
+			logger.Info ("Получаем таблицу пользователей...");
 			
 			string sql = "SELECT * FROM users ";
-			MySqlCommand cmd = new MySqlCommand(sql, QSMain.connectionDB);
+			MySqlCommand cmd = new MySqlCommand (sql, QSMain.connectionDB);
 			
-			MySqlDataReader rdr = cmd.ExecuteReader();
+			MySqlDataReader rdr = cmd.ExecuteReader ();
 			
-			UsersListStore.Clear();
-			while (rdr.Read())
-			{
-				UsersListStore.AppendValues(int.Parse(rdr["id"].ToString()),
-				                            rdr["login"].ToString(),
-				                            rdr["name"].ToString(),
-				                            (bool)rdr["admin"]);
+			UsersListStore.Clear ();
+			while (rdr.Read ()) {
+				UsersListStore.AppendValues (int.Parse (rdr ["id"].ToString ()),
+				                             rdr ["login"].ToString (),
+				                             rdr ["name"].ToString (),
+				                             (bool)rdr ["admin"]);
 			}
-			rdr.Close();
+			rdr.Close ();
 			
-			logger.Info("Ok");
+			logger.Info ("Ok");
 			
-			OnTreeviewUsersCursorChanged (null,null);
+			OnTreeviewUsersCursorChanged (null, null);
 		}
-		
+
 		protected void OnTreeviewUsersCursorChanged (object sender, EventArgs e)
 		{
-			bool isSelect = treeviewUsers.Selection.CountSelectedRows() == 1;
+			bool isSelect = treeviewUsers.Selection.CountSelectedRows () == 1;
 			buttonEdit.Sensitive = isSelect;
 			buttonDelete.Sensitive = isSelect;
 		}
-		
+
 		protected void OnTreeviewUsersRowActivated (object o, RowActivatedArgs args)
 		{
 			buttonEdit.Click ();
 		}
-		
+
 		protected void OnButtonAddClicked (object sender, EventArgs e)
 		{
-			UserProperty winUser = new UserProperty();
+			UserProperty winUser = new UserProperty ();
 			winUser.NewUser = true;
-			winUser.Show();
-			winUser.Run();
-			winUser.Destroy();
-			UpdateUsers();
+			winUser.Show ();
+			winUser.Run ();
+			winUser.Destroy ();
+			UpdateUsers ();
 		}
-		
+
 		protected void OnButtonEditClicked (object sender, EventArgs e)
 		{
 			TreeIter iter;
 			
-			treeviewUsers.Selection.GetSelected(out iter);
-			int itemid = Convert.ToInt32(UsersListStore.GetValue(iter,0));
-			UserProperty winUser = new  UserProperty();
-			winUser.UserFill(itemid);
-			winUser.Show();
-			winUser.Run();
-			winUser.Destroy();
-			UpdateUsers();
+			treeviewUsers.Selection.GetSelected (out iter);
+			int itemid = Convert.ToInt32 (UsersListStore.GetValue (iter, 0));
+			UserProperty winUser = new  UserProperty ();
+			winUser.UserFill (itemid);
+			winUser.Show ();
+			winUser.Run ();
+			winUser.Destroy ();
+			UpdateUsers ();
 		}
-		
+
 		protected void OnButtonDeleteClicked (object sender, EventArgs e)
 		{
 			TreeIter iter;
 			
-			treeviewUsers.Selection.GetSelected(out iter);
-			int itemid = (int) UsersListStore.GetValue(iter,0);
-			string loginname = UsersListStore.GetValue(iter,1).ToString ();
-			Delete winDel = new Delete();
-			if (winDel.RunDeletion("users", itemid))
-			{
-				logger.Info("Удаляем пользователя MySQL...");
+			treeviewUsers.Selection.GetSelected (out iter);
+			int itemid = (int)UsersListStore.GetValue (iter, 0);
+			string loginname = UsersListStore.GetValue (iter, 1).ToString ();
+			Delete winDel = new Delete ();
+			if (winDel.RunDeletion ("users", itemid)) {
+				logger.Info ("Удаляем пользователя MySQL...");
 				string sql;
-				sql = String.Format("DROP USER {0}, {0}@localhost", loginname);
-				try 
-				{
-					QSMain.CheckConnectionAlive();
-					MySqlCommand cmd = new MySqlCommand(sql, QSMain.connectionDB);
-					cmd.ExecuteNonQuery();
-					logger.Info("Пользователь удалён. Ok");
-				} 
-				catch (Exception ex) 
-				{
-					logger.ErrorException("Ошибка удаления пользователя!", ex);
-					QSMain.ErrorMessage(this,ex);
+				sql = String.Format ("DROP USER {0}, {0}@localhost", loginname);
+				try {
+					QSMain.CheckConnectionAlive ();
+					MySqlCommand cmd = new MySqlCommand (sql, QSMain.connectionDB);
+					cmd.ExecuteNonQuery ();
+					logger.Info ("Пользователь удалён. Ok");
+				} catch (Exception ex) {
+					logger.ErrorException ("Ошибка удаления пользователя!", ex);
+					QSMain.ErrorMessage (this, ex);
 				}
 			}
-			winDel.Destroy();
-			UpdateUsers();
+			if (QSSaaS.Session.IsSaasConnection) {
+				QSSaaS.ISaaSService svc = QSSaaS.Session.GetSaaSService ();
+				if (!svc.revokeBaseAccess (loginname, QSSaaS.Session.Account, QSSaaS.Session.BaseName))
+					logger.Error ("Ошибка удаления доступа к базе на сервере SaaS.");
+			}
+			winDel.Destroy ();
+			UpdateUsers ();
 		}
 	}
 }
