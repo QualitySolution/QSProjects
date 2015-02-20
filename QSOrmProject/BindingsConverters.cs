b795ffc2b7a9b30cacf8264806e2fea3e4c913d4
Andrey Gankov <gav@qsolution.ru>
2015-02-20 13:03:30 +0300

    Добавлен конвертер для отображения id.

---
+++ QSOrmProject/BindingsConverters.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Data.Bindings;
+using System.Globalization;
+
+namespace QSOrmProject
+{
+	public class IdToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			int id = (int)value;
+			if (id > 0)
+				return id.ToString ();
+			else
+				return "не определён";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
+
