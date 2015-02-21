e0c53308f8b4e86cda5f61be6af06eca92c86fc1
Andrey Gankov <gav@qsolution.ru>
2015-02-21 23:15:28 +0300

    Первый рабочий вариант HistoryLog

---
+++ QSHistoryLog/HistoryMain.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace QSHistoryLog
+{
+	public class HistoryMain
+	{
+		public HistoryMain ()
+		{
+		}
+	}
+
+	public enum ChangeSetType{
+		create,
+		change,
+		delete
+	}
+}
+
