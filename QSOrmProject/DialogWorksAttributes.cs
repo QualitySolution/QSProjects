8381fc6a1636e60e0dbd61de9df935e4cbb03a11
Andrey Gankov <gav@qsolution.ru>
2015-03-02 10:50:12 +0300

    Возможность установить размеры окна.

---
+++ QSOrmProject/DialogWorksAttributes.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace QSOrmProject
+{
+	[AttributeUsage(AttributeTargets.Class)]
+	public class WidgetWindowAttribute : Attribute 
+	{
+		public int DefaultHeight;
+		public int DefaultWidth;
+	}
+}
--- QSOrmProject/OneWidgetDialog.cs
+++ QSOrmProject/OneWidgetDialog.cs
@@ -9,12 +9,19 @@ namespace QSOrmProject
 		public OneWidgetDialog (Widget widget)
 		{
 			this.Build ();
+
+			var att = widget.GetType ().GetCustomAttributes (typeof(WidgetWindowAttribute), false);
+			if (att.Length > 0)
+				this.SetDefaultSize ((att [0] as WidgetWindowAttribute).DefaultWidth,
+				                     (att [0] as WidgetWindowAttribute).DefaultHeight);
+
 			widget.Show ();
 			VBox.Add (widget);
 
-			var att = widget.GetType ().GetCustomAttributes (typeof(DisplayNameAttribute), true);
+			att = widget.GetType ().GetCustomAttributes (typeof(DisplayNameAttribute), true);
 			if (att.Length > 0)
 				Title = (att [0] as DisplayNameAttribute).DisplayName;
+			this.ReshowWithInitialSize ();
 		}
 	}
 }
