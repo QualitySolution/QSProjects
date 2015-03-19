bd75ed0863613e2d166094ef53f7e7c123d6c3ab
anri-vin <anri.vin@gmail.com>
2015-03-19 14:39:02 +0300

    Добавлен метод клонирования объектов через рефлексию.

---
+++ QSOrmProject/ObjectCloner.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace QSOrmProject
+{
+	
+	public static class ObjectCloner
+	{
+		public static void ReflectionClone<T> (T cloneObject, ref T newObject) where T : class
+		{
+			Type type = cloneObject.GetType ();
+			List<FieldInfo> fields = new List<FieldInfo> ();
+
+			while (type != null) {
+				fields.AddRange (type.GetFields (BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic));
+				type = type.BaseType;
+			}
+			foreach (FieldInfo f in fields)
+				f.SetValue (newObject, f.GetValue (cloneObject));
+		}
+	}
+}
+
