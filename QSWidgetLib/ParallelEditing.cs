3c47dfd637dc8331ec60cd22e0fed4424939ca95
Andrey Gankov <gav@qsolution.ru>
2015-03-30 15:24:45 +0300

    Помощник в паралельном редактировании виджетов.

---
+++ QSWidgetLib/ParallelEditing.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Gtk;
+
+namespace QSWidgetLib
+{
+	public class ParallelEditing
+	{
+		private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+		public Entry OutEntry;
+		public Func<object, string> GetParallelTextFunc;
+
+		private bool IsParallelMode = false;
+
+		public ParallelEditing (Entry outEntry)
+		{
+			OutEntry = outEntry;
+		}
+
+		public void SubscribeOnChanges(Entry sourceEntry)
+		{
+			sourceEntry.FocusInEvent += OnSourceFocusInEvent;
+			sourceEntry.FocusOutEvent += OnSourceFocusOutEvent;
+			sourceEntry.Changed += OnSourceChanged;
+		}
+
+		void OnSourceChanged (object sender, EventArgs e)
+		{
+			if(IsParallelMode)
+				OutEntry.Text = GetParallelTextFunc(sender);
+		}
+
+		void OnSourceFocusOutEvent (object o, FocusOutEventArgs args)
+		{
+			if (IsParallelMode) {
+				OutEntry.ModifyText (StateType.Normal);
+				IsParallelMode = false;
+			}
+		}
+
+		void OnSourceFocusInEvent (object o, FocusInEventArgs args)
+		{
+			IsParallelMode = false;
+			if(OutEntry != null && GetParallelTextFunc != null)
+			{
+				if (String.IsNullOrEmpty (OutEntry.Text))
+					IsParallelMode = true;
+				else
+					IsParallelMode = OutEntry.Text == GetParallelTextFunc(o);
+			}
+
+			if(IsParallelMode)
+			{
+				logger.Debug ("Включен режим паралельного редактирования.");
+				OutEntry.ModifyText (StateType.Normal, new Gdk.Color(0, 152, 190));
+			}
+		}
+	}
+}
+
