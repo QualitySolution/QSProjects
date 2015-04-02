1bef67cf2eb5645b8600b51129a6b6cb5daff345
anri-vin <anri.vin@gmail.com>
2015-04-02 13:56:52 +0300

    Добавил фильтр для банков.

---
+++ Modules/QSBanks/BankFilter.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using NHibernate;
+using NHibernate.Criterion;
+using QSOrmProject;
+
+
+namespace QSBanks
+{
+	[OrmDefaultIsFiltered (true)]
+	public partial class BankFilter : Gtk.Bin, IReferenceFilter
+	{
+		public ISession Session { set; get; }
+
+		public ICriteria BaseCriteria { set; get; }
+
+		public event EventHandler Refiltered;
+
+		public bool IsFiltred { get; private set; }
+
+		ICriteria filtredCriteria;
+
+		public ICriteria FiltredCriteria {
+			private set { filtredCriteria = value; }
+			get {
+				if (filtredCriteria == null)
+					UpdateCreteria ();
+				return filtredCriteria;
+			}
+		}
+
+		public BankFilter (ISession session)
+		{
+			this.Build ();
+			IsFiltred = false;
+		}
+
+		void UpdateCreteria ()
+		{
+			IsFiltred = false;
+			if (BaseCriteria == null)
+				return;
+			FiltredCriteria = (ICriteria)BaseCriteria.Clone ();
+
+			if (!checkShowDeleted.Active) {
+				FiltredCriteria.Add (Restrictions.Eq ("Deleted", false));
+				IsFiltred = true;
+			}
+
+			OnRefiltered ();
+		}
+
+		void OnRefiltered ()
+		{
+			if (Refiltered != null) {
+				Refiltered (this, new EventArgs ());
+			}
+		}
+
+		protected void OnCheckShowDeletedToggled (object sender, EventArgs e)
+		{
+			UpdateCreteria ();
+		}
+	}
+}
+
--- Modules/QSBanks/QSBanksMain.cs
+++ Modules/QSBanks/QSBanksMain.cs
@@ -7,12 +7,11 @@ namespace QSBanks
 	public static class QSBanksMain
 	{
 
-		public static List<OrmObjectMapping> GetModuleMaping()
+		public static List<OrmObjectMapping> GetModuleMaping ()
 		{
-			return new List<OrmObjectMapping>
-			{
-				new OrmObjectMapping(typeof(Bank), typeof(BankDlg), "{QSBanks.Bank} Bik[БИК]; Name[Имя]; City[Город];", new string[] {"Bik", "Name", "City"}),
-				new OrmObjectMapping(typeof(Account), typeof(AccountDlg)),
+			return new List<OrmObjectMapping> {
+				new OrmObjectMapping (typeof(Bank), typeof(BankDlg), typeof(BankFilter), "{QSBanks.Bank} Bik[БИК]; Name[Имя]; City[Город];", new string[] { "Bik", "Name", "City" }),
+				new OrmObjectMapping (typeof(Account), typeof(AccountDlg)),
 			};
 		}
 
