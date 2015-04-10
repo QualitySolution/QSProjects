c7973852aeb89ff271c57e7dd60e511ad7e8c54c
anri-vin <anri.vin@gmail.com>
2015-04-10 13:44:18 +0300

    Добавлен виджет отображения валюты.

--- QSProjectsLib/CurrencyWorks.cs
+++ QSProjectsLib/CurrencyWorks.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using Gtk;
 
 namespace QSProjectsLib
 {
@@ -11,6 +13,20 @@ namespace QSProjectsLib
 		public static string GetShortCurrencyString (Decimal value)
 		{
 			return String.Format (CurrencyShortFomat, value);
+		}
+	}
+
+	[ToolboxItem (true)]
+	public class CurrencyLabel : Label
+	{
+		public CurrencyLabel ()
+		{
+			Text = LabelProp = CurrencyWorks.CurrencyShortName;
+		}
+
+		new public string LabelProp {
+			get { return base.LabelProp; }
+			set { }
 		}
 	}
 }
