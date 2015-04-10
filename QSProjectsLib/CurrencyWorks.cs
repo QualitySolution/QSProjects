91b7291bac5eb8c99ee165b60bb87e8252ac7be1
anri-vin <anri.vin@gmail.com>
2015-04-10 13:01:20 +0300

    Добавлен класс валюты.

---
+++ QSProjectsLib/CurrencyWorks.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace QSProjectsLib
+{
+	public static class CurrencyWorks
+	{
+		public static string CurrencyShortName = "₽";
+
+		public static string CurrencyShortFomat = "{0:N2} ₽";
+
+		public static string GetShortCurrencyString (Decimal value)
+		{
+			return String.Format (CurrencyShortFomat, value);
+		}
+	}
+}
+
