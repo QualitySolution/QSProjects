904c00948cb6ad3b5a31438e6bd743f7a608fc13
Andrey Gankov <gav@qsolution.ru>
2015-04-15 15:25:15 +0300

    Алгоритм подсветки через Pango переписан что бы не создавать лишних span, тегов.

--- QSHistoryLog/PangoRender.cs
+++ QSHistoryLog/PangoRender.cs
@@ -7,9 +7,10 @@ namespace QSHistoryLog
 	public static class PangoRender
 	{
 		private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-		public const string PangoInsertFormat = "<span background=\"#A6F3A6\">{0}</span>";
-		public const string PangoDeleteFormat = "<span background=\"#F8CBCB\">{0}</span>";
-		public const string PangoChangeFormat = "<span background=\"#F0DB88\">{0}</span>";
+		public const string PangoInsertFormat = "<span background=\"#A6F3A6\">";
+		public const string PangoDeleteFormat = "<span background=\"#F8CBCB\">";
+		public const string PangoChangeFormat = "<span background=\"#F0DB88\">";
+		public const string PangoEnd = "</span>";
 
 		public static string RenderDiffLines(DiffPaneModel diffModel)
 		{
@@ -17,17 +18,16 @@ namespace QSHistoryLog
 			foreach (var line in diffModel.Lines)
 			{
 				if (line.Type == ChangeType.Deleted)
-					result.AppendLine (String.Format (PangoDeleteFormat, line.Text));
+					result.AppendLine (PangoDeleteFormat + line.Text + PangoEnd);
 				else if (line.Type == ChangeType.Inserted)
-					result.AppendLine (String.Format ( PangoInsertFormat, line.Text));
+					result.AppendLine (PangoInsertFormat + line.Text + PangoEnd);
 				else if (line.Type == ChangeType.Unchanged)
 					result.AppendLine (line.Text);
 				else if (line.Type == ChangeType.Modified)
 					result.AppendLine (RenderDiffWords(line));
 				else if (line.Type == ChangeType.Imaginary)
 				{
-					//fillColor = new SolidColorBrush(Color.FromArgb(255, 200, 200, 200));
-					//AddImaginaryLine(textBox, lineNumber);
+					result.AppendLine ();
 					logger.Debug ("Imaginary line {0}", line.Text);
 				}
 			}
@@ -37,39 +37,53 @@ namespace QSHistoryLog
 		private static string RenderDiffWords(DiffPiece line)
 		{
 			StringBuilder result = new StringBuilder ();
-			foreach (var word in line.SubPieces)
-			{
-				if (word.Type == ChangeType.Deleted)
-					result.AppendFormat (PangoDeleteFormat, word.Text);
-				else if (word.Type == ChangeType.Inserted)
-					result.AppendFormat (PangoInsertFormat, word.Text);
-				else if (word.Type == ChangeType.Modified)
-					result.Append (RenderDiffCharacter(word));
-				else if (word.Type == ChangeType.Imaginary)
+			ChangeType lastAction = ChangeType.Unchanged;
+			foreach (var word in line.SubPieces) {
+				if (word.Type == ChangeType.Imaginary)
 					continue;
+
+				if (word.Type == ChangeType.Modified)
+					result.Append (RenderDiffCharacter (word, ref lastAction));
 				else
-					result.Append (word.Text);
+				{
+					if (lastAction != ChangeType.Unchanged && lastAction != word.Type)
+						result.Append (PangoEnd);
+					result.Append (StartSpan (word.Type, lastAction)).Append (word.Text);
+					lastAction = word.Type;
+				}
 			}
+			if(lastAction != ChangeType.Unchanged)
+				result.Append (PangoEnd);
 			return result.ToString ();
 		}
 
-		private static string RenderDiffCharacter(DiffPiece word)
+		private static string RenderDiffCharacter(DiffPiece word, ref ChangeType lastAction)
 		{
 			StringBuilder result = new StringBuilder ();
 			foreach (var characters in word.SubPieces)
 			{
-				if (characters.Type == ChangeType.Deleted)
-					result.AppendFormat (PangoDeleteFormat, characters.Text);
-				else if (characters.Type == ChangeType.Inserted)
-					result.AppendFormat (PangoInsertFormat, characters.Text);
-				else if (characters.Type == ChangeType.Imaginary)
+				if (characters.Type == ChangeType.Imaginary)
 					continue;
-				else
-					result.Append (characters.Text);
+
+				if (lastAction != ChangeType.Unchanged && lastAction != characters.Type)
+					result.Append (PangoEnd);
+
+				result.Append (StartSpan (characters.Type, lastAction)).Append (characters.Text);
+				lastAction = characters.Type;
 			}
 			return result.ToString ();
 		}
 
+		private static string StartSpan(ChangeType current, ChangeType last)
+		{
+			if (current != last) {
+				if (current == ChangeType.Deleted)
+					return PangoDeleteFormat;
+				else if (current == ChangeType.Inserted)
+					return PangoInsertFormat;
+			}
+			return String.Empty;
+		}
 	}
 }
 
