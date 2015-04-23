f495546cdc672f7ffdf4739f62b539f012c32e04
anri-vin <anri.vin@gmail.com>
2015-04-23 17:21:50 +0300

    Убраны контакты. Перенесены в Vodovoz.

--- Modules/QSContacts/QSContactsMain.cs
+++ Modules/QSContacts/QSContactsMain.cs
@@ -7,13 +7,11 @@ namespace QSContacts
 	public static class QSContactsMain
 	{
 
-		public static List<OrmObjectMapping> GetModuleMaping()
+		public static List<OrmObjectMapping> GetModuleMaping ()
 		{
-			return new List<OrmObjectMapping>
-			{
-				new OrmObjectMapping(typeof(Contact), typeof(ContactDlg), "{QSContacts.Contact} Surname[Фамилия]; Name[Имя]; Lastname[Отчество]; Post[Должность]", new string[] {"Surname", "Name", "Lastname", "Post"}),
-				new OrmObjectMapping(typeof(PhoneType), null, "{QSContacts.PhoneType} Name[Название];"),
-				new OrmObjectMapping(typeof(EmailType), null, "{QSContacts.EmailType} Name[Название];")
+			return new List<OrmObjectMapping> {
+				new OrmObjectMapping (typeof(PhoneType), null, "{QSContacts.PhoneType} Name[Название];"),
+				new OrmObjectMapping (typeof(EmailType), null, "{QSContacts.EmailType} Name[Название];")
 			};
 		}
 	}
