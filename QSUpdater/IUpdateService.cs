33820189cc9f8398a0732848bb1110a581cf2a5c
Andrey Gankov <gav@qsolution.ru>
2015-05-08 15:16:59 +0300

    Быстрый фикс Updater-а

--- QSUpdater/IUpdateService.cs
+++ QSUpdater/IUpdateService.cs
@@ -5,7 +5,8 @@ namespace QSUpdater
 	[ServiceContract]
 	public interface IUpdateService
 	{
-		[OperationContract (IsInitiating = true, IsTerminating = true)]
+		[OperationContract]// (IsInitiating = true, IsTerminating = true)] 
+		//FIXME на Windows при наличии этих атрибутов стали получать Эксепшен. Отключил до дальнейших разбирательств.
 		UpdateResult checkForUpdate (string parameters);
 	}
 }
