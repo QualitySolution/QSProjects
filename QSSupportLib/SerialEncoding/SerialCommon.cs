a1f09b2f2ae87cfd3c51cf3c79b09d003a55145d
Andrey Gankov <gav@qsolution.ru>
2015-05-15 18:20:01 +0300

    Добавлен виджет ввода серийного номера. + перенесена часть кода для расшифровки.

---
+++ QSSupportLib/SerialEncoding/SerialCommon.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Text;
+
+namespace QSSupportLib.Serial
+{
+	public static class SerialCommon
+	{
+		static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger ();
+
+		public const int SplitBy = 4;
+
+		public static string AddHyphens(string dirtyNumber)
+		{
+			logger.Debug("Serial digits = {0}", dirtyNumber.Length);
+
+			StringBuilder result = new StringBuilder();
+			int untilHyphen = SplitBy;
+			foreach (var character in dirtyNumber)
+			{
+				if(untilHyphen == 0)
+				{
+					untilHyphen = SplitBy;
+					result.Append('-');
+				}
+				result.Append(character);
+				untilHyphen--;
+			}
+			return result.ToString();
+		}
+
+	}
+}
+
