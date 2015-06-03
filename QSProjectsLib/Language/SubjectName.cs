3d431fbbb3c52161ed3aba1825aaeb65c305cb0c
Andrey Gankov <gav@qsolution.ru>
2015-06-03 10:14:54 +0300

    Класс для работы с названиями существительных(например сущьностей доменной модели)

---
+++ QSProjectsLib/Language/SubjectName.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace QSProjectsLib
+{
+	public class SubjectName
+	{
+		/// <summary>
+		/// Именительный падеж (Кто? Что?)
+		/// </summary>
+		public string Nominative;
+
+		/// <summary>
+		/// Именительный падеж (Кто? Что?) можественное число.
+		/// </summary>
+		public string NominativePlural;
+
+		/// <summary>
+		/// Род
+		/// </summary>
+		public GrammaticalGender Gender;
+
+		/// <summary>
+		/// Родительный падеж (Кого? Чего?)
+		/// </summary>
+		public string Genitive;
+
+		/// <summary>
+		/// Родительный падеж (Кого? Чего?) можественное число.
+		/// </summary>
+		public string GenitivePlural;
+
+		/// <summary>
+		/// Винительный падеж (Кого? Что?)
+		/// </summary>
+		public string Accusative;
+
+		/// <summary>
+		/// Винительный падеж (Кого? Что?) можественное число.
+		/// </summary>
+		public string AccusativePlural;
+
+	}
+
+	public enum GrammaticalGender
+	{
+		/// <summary>
+		/// Средний род
+		/// </summary>
+		Neuter,
+		/// <summary>
+		/// Мужской род
+		/// </summary>
+		Masculine,
+		/// <summary>
+		/// Женский род
+		/// </summary>
+		Feminine
+	}
+}
+
