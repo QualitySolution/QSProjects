b7bb4fdfff3aa31b8475f6b42370b6cc6cce5d41
Andrey Gankov <gav@qsolution.ru>
2015-06-09 13:34:42 +0300

    Первая реализация модели представления.

---
+++ QSOrmProject/RepresentationModel/ColumnInfo.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq.Expressions;
+using Gtk;
+
+namespace QSOrmProject.RepresentationModel
+{
+	public interface IColumnInfo{
+		string Name { get;}
+		string NodePropertyName { get;}
+		int DataColumn { get;}
+		ColumnType Type { get;}
+	}
+
+	public class ColumnInfo : IColumnInfo
+	{
+		public string Name { get; set;}
+
+		public string NodePropertyName { get; set;}
+
+		public ColumnType Type { get; set;}
+
+		public int DataColumn { get; set;}
+
+		public ColumnInfo ()
+		{
+			
+		}
+
+		public ColumnInfo SetNodeProperty<TVMNode> (Expression<Func<TVMNode, object>> propertyRefExpr)
+		{
+			NodePropertyName = PropertyUtil.GetPropertyNameCore (propertyRefExpr.Body);
+			var prop = typeof(TVMNode).GetProperty (NodePropertyName);
+			var att = prop.GetCustomAttributes (typeof(TreeNodeValueAttribute), true);
+			if (att.Length == 0)
+				throw new InvalidOperationException (String.Format ("Колонка {0} ссылается на свойство {1} для которого не задан атрибут TreeNodeValueAttribute.",
+						Name,
+						NodePropertyName
+					));
+			DataColumn = (att [0] as TreeNodeValueAttribute).Column;
+
+			return this;
+		}
+	}
+
+	public enum ColumnType
+	{
+		Text
+	}
+}
+
---
+++ QSOrmProject/Widgets/OrmTreeView.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using QSOrmProject.RepresentationModel;
+using Gtk;
+using QSProjectsLib;
+
+namespace QSOrmProject
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	public class OrmTableView : NodeView
+	{
+
+		IRepresentationModel representationModel;
+		public IRepresentationModel RepresentationModel {
+			get {
+				return representationModel;
+			}
+			set {if (representationModel == value)
+					return;
+				representationModel = value;
+				PreparedView ();
+			}
+		}
+
+		public OrmTableView ()
+		{
+			
+		}
+
+		void PreparedView()
+		{
+			foreach (IColumnInfo column in RepresentationModel.Columns)
+			{
+				switch(column.Type)
+				{
+				case ColumnType.Text:
+					this.AppendColumn (column.Name, new CellRendererText (), "text", column.DataColumn);
+					break;
+				}
+			}
+
+			NodeStore = representationModel.NodeStore;
+		}
+
+		public int GetSelectedId()
+		{
+			var node = NodeSelection.SelectedNode as object;
+			var id = DBWorks.IdPropertyOrNull (node);
+			if (id == null)
+				return -1;
+			else
+				return (int)id;
+		}
+	}
+}
+
