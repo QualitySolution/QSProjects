b17f9afdcf20320249d62175a463576bf4e586ca
Andrey Gankov <gav@qsolution.ru>
2015-07-23 18:58:29 +0300

    Добавлен репозиторий.

---
+++ Modules/QSBanks/Repositories/BankRepository.cs
@@ -0,0 +1,14 @@
+﻿using QSOrmProject;
+using System.Collections.Generic;
+
+namespace QSBanks.Repository
+{
+	public static class BankRepository
+	{
+		public static IList<Bank> ActiveBanks (IUnitOfWork uow)
+		{
+			return uow.Session.QueryOver<Bank> ().Where (b => !b.Deleted).List<Bank> ();
+		}
+	}
+}
+
