9d930df1192ab8b8d7e5a8297086a50ce6515106
Andrey Gankov <gav@qsolution.ru>
2015-07-29 10:09:58 +0300

    Базовый клас для виджета на вкладке.

---
+++ QSTDI/WidgetOnTdiTabBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace QSTDI
+{
+	public abstract class WidgetOnTdiTabBase : Gtk.Bin
+	{
+		ITdiTab mytab;
+
+		protected ITdiTab MyTab{
+			get{
+				if(mytab == null)
+					mytab = TdiHelper.FindMyTab (this);
+				if (mytab == null) {
+					throw new InvalidOperationException ("Родительская вкладка не найдена.");
+				} else
+					return mytab;
+			}
+		}
+
+		protected override void OnParentSet (Gtk.Widget previous_parent)
+		{
+			mytab = null;
+			base.OnParentSet (previous_parent);
+		}
+	}
+}
+
