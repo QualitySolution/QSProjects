6b1093bb3e56dfd8c321090da19835ad854d0158
Andrey Gankov <gav@qsolution.ru>
2015-08-11 20:30:55 +0300

    Добавлен виджет ввода названия компании с формой собственности.

---
+++ QSOrmProject/DataWidgets/DataLegalName.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using GammaBinding;
+using QSWidgetLib;
+
+namespace QSOrmProject
+{
+	[ToolboxItem (true)]
+	public class DataLegalName : LegalName
+	{
+		public BindingControler<DataLegalName> Binding { get; private set;}
+
+		public DataLegalName ()
+		{
+			Binding = new BindingControler<DataLegalName> (this, new string[]{
+				this.GetPropertyName (w => w.OwnName),
+				this.GetPropertyName (w => w.Ownership),
+				this.GetPropertyName (w => w.FullName)
+			});
+			NameChanged += DataLegalName_NameChanged;
+			OwnershipChanged += DataLegalName_OwnershipChanged;
+			FullNameChanged += DataLegalName_FullNameChanged;
+		}
+
+		void DataLegalName_FullNameChanged (object sender, EventArgs e)
+		{
+			Binding.FireChange (w => w.FullName);
+		}
+
+		void DataLegalName_OwnershipChanged (object sender, EventArgs e)
+		{
+			Binding.FireChange (w => w.Ownership);
+		}
+
+		void DataLegalName_NameChanged (object sender, EventArgs e)
+		{
+			Binding.FireChange (w => w.OwnName);
+		}
+	}
+}
+
