56dd9357636ed91098270f08ec545e28c5a5f297
Andrei Vinogradov <anri.vin@gmail.com>
2015-08-17 11:49:09 +0300

    Добавил сервис отчетов об ошибках.

---
+++ QSBugReporting/IBugReportingService.cs
@@ -0,0 +1,14 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace QSBugReporting
+{
+	[ServiceContract]
+	public interface IBugReportingService
+	{
+		[OperationContract]
+		[WebGet (ResponseFormat = WebMessageFormat.Json)]
+		bool SubmitBugReport (string product, string version, string stackTrace, string description, string email);
+	}
+}
+
---
+++ QSBugReporting/ReportWorker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceModel.Web;
+using System.ServiceModel;
+using NLog;
+
+namespace QSBugReporting
+{
+	public class ReportWorker
+	{
+		public static String ServiceAddress = "http://saas.qsolution.ru:2048/BugReporting";
+
+		static Logger logger = LogManager.GetCurrentClassLogger ();
+
+		public IBugReportingService GetReportService ()
+		{
+			try {
+				var address = new Uri (ServiceAddress);
+				var factory = new WebChannelFactory<IBugReportingService> (new WebHttpBinding { AllowCookies = true }, address);
+				return factory.CreateChannel ();
+			} catch (Exception ex) {
+				logger.Error (ex, "Ошибка создания подключения к сервису BugReporting.");
+				return null;
+			}
+		}
+	}
+}
+
