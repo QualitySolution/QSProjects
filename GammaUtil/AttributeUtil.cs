3766a0d62f02247cab223c4d3ae88211ae4ae4d0
Andrey Gankov <gav@qsolution.ru>
2015-08-19 16:18:35 +0300

    Added attribute utilites

---
+++ GammaUtil/AttributeUtil.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Reflection;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gamma.Utilities
+{
+	public static class AttributeUtil
+	{
+
+		public static string GetEnumTitle (this Enum aEnum)
+		{
+			string desc = aEnum.ToString();
+			FieldInfo fi = aEnum.GetType().GetField (desc);
+			return (fi.GetEnumTitle());
+		}
+
+		public static string GetEnumTitle (this FieldInfo aFieldInfo)
+		{
+			var attrs = (DisplayAttribute[]) aFieldInfo.GetCustomAttributes (typeof(DisplayAttribute), false);
+			if ((attrs != null) && (attrs.Length > 0))
+				return (attrs[0].GetName ());
+			return (aFieldInfo.Name);
+		}
+
+	}
+}
+
