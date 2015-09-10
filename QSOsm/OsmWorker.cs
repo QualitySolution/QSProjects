ff8e16fac49d2b46fb092d535c51fbb5d9049cd2
Andrei Vinogradov <anri.vin@gmail.com>
2015-09-10 11:39:33 +0300

    Добавил nlog и класс для получения соединения.

---
+++ QSOsm/OsmWorker.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ServiceModel;
+using NLog;
+using QSOsm;
+
+namespace QSOsm
+{
+	public static class OsmWorker
+	{
+		public static string ServiceAddress = "http://saas.qsolution.ru:2048/OsmService";
+
+		static Logger logger = LogManager.GetCurrentClassLogger ();
+
+		public static IOsmService GetReportService ()
+		{
+			try {
+				var address = new Uri (ServiceAddress);
+				var factory = new ChannelFactory<IOsmService> (new WebHttpBinding (), ServiceAddress);
+				return factory.CreateChannel ();
+			} catch (Exception ex) {
+				logger.Error (ex, "Ошибка создания подключения к сервису Osm.");
+				return null;
+			}
+		}
+	}
+}
+
