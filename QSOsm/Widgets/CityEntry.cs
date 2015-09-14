3bf8175caa6ea831f759fccbc7c7ec6ae78ecdc2
Andrei Vinogradov <anri.vin@gmail.com>
2015-09-14 17:58:03 +0300

    Два отдельных поля по город и район.

--- QSOsm/Widgets/CityEntry.cs
+++ QSOsm/Widgets/CityEntry.cs
@@ -3,9 +3,6 @@ using Gtk;
 using Gamma.Binding.Core;
 using System.Linq.Expressions;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Runtime.Remoting.Messaging;
-using System.Threading.Tasks;
 using System.Threading;
 
 namespace QSOsm
@@ -14,6 +11,9 @@ namespace QSOsm
 	[System.ComponentModel.Category ("Gamma OSM Widgets")]
 	public class CityEntry : Entry
 	{
+		const int CityColumn = 1;
+		const int DistrictColumn = 2;
+
 		private ListStore completionListStore;
 
 		private delegate List<OsmCity> GetCitiesDelegate ();
@@ -22,13 +22,51 @@ namespace QSOsm
 
 		public BindingControler<CityEntry> Binding { get; private set; }
 
+		string city;
+
+		public string City {
+			get {
+				return city;
+			}
+			set {
+				city = value;
+				UpdateText ();
+			}
+		}
+
+		string cityDistrict;
+
+		public string CityDistrict {
+			get {
+				return cityDistrict;
+			}
+			set {
+				cityDistrict = value;
+				UpdateText ();
+			}
+		}
+
+		void UpdateText ()
+		{
+			this.Text = String.IsNullOrWhiteSpace (CityDistrict) ? City : String.Format ("{0} ({1})", City, CityDistrict);
+		}
+
 		public CityEntry ()
 		{
 			Binding = new BindingControler<CityEntry> (this, new Expression<Func<CityEntry, object>>[] {
-				(w => w.Text)
+				(w => w.City), (w => w.CityDistrict)
 			});
+
 			this.Completion = new EntryCompletion ();
+			this.Completion.MatchSelected += Completion_MatchSelected;
 			this.TextInserted += CityEntryTextInserted;
+		}
+
+		[GLib.ConnectBefore]
+		void Completion_MatchSelected (object o, MatchSelectedArgs args)
+		{
+			City = args.Model.GetValue (args.Iter, CityColumn).ToString ();
+			CityDistrict = args.Model.GetValue (args.Iter, DistrictColumn).ToString ();
 		}
 
 
@@ -46,20 +84,23 @@ namespace QSOsm
 		{
 			IOsmService svc = OsmWorker.GetOsmService ();
 			var cities = svc.GetCities ();
-			completionListStore = new ListStore (typeof(long), typeof(string));
-			foreach (var city in cities) {
+			completionListStore = new ListStore (typeof(string), typeof(string), typeof(string));
+			foreach (var c in cities) {
 				completionListStore.AppendValues (
-					city.OsmId, 
-					String.IsNullOrWhiteSpace (city.SuburbDistrict) ? city.Name : String.Format ("{0} ({1})", city.Name, city.SuburbDistrict));
+					String.IsNullOrWhiteSpace (c.SuburbDistrict) ? c.Name : String.Format ("{0} ({1})", c.Name, c.SuburbDistrict),
+					c.Name,
+					c.SuburbDistrict
+				);
 			}
 			this.Completion.Model = completionListStore;
-			this.Completion.TextColumn = 1;
+			this.Completion.TextColumn = 0;
+
 			queryIsRunning = false;
 		}
 
 		protected override void OnChanged ()
 		{
-			Binding.FireChange (w => w.Text);
+			Binding.FireChange (w => w.City, w => w.CityDistrict);
 			base.OnChanged ();
 		}
 	}
