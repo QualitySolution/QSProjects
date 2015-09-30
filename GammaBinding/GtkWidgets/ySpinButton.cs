fa72f3347c1bf4f91a1c44f6cc467d31de1ceb52
Andrey Gankov <gav@qsolution.ru>
2015-09-30 12:43:06 +0300

    Added Spin Widget

---
+++ GammaBinding/GtkWidgets/ySpinButton.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Gtk;
+using Gamma.Binding.Core;
+using System.Linq.Expressions;
+
+namespace Gamma.GtkWidgets
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	[System.ComponentModel.Category ("Gamma Gtk")]
+	public class ySpinButton : SpinButton
+	{
+		public BindingControler<ySpinButton> Binding { get; private set;}
+
+		public ySpinButton (double min, double max, double step) : base(min, max, step)
+		{
+			Binding = new BindingControler<ySpinButton> (this, new Expression<Func<ySpinButton, object>>[] {
+				(w => w.Value),
+				(w => w.ValueAsInt),
+			});
+		}
+
+		public ySpinButton (Adjustment adjustment, double climb_rate, uint digits) : base(adjustment, climb_rate, digits)
+		{
+			Binding = new BindingControler<ySpinButton> (this, new Expression<Func<ySpinButton, object>>[] {
+				(w => w.Value),
+				(w => w.ValueAsInt),
+			});
+		}
+
+		protected override void OnValueChanged ()
+		{
+			Binding.FireChange (
+				(w => w.Value),
+				(w => w.ValueAsInt)
+			);
+			base.OnValueChanged ();
+		}
+
+		public decimal ValueAsDecimal
+		{
+			get{ return Convert.ToDecimal (Value);
+			}
+			set{
+				Value = Convert.ToDouble (value);
+			}
+		}
+	}
+}
+
