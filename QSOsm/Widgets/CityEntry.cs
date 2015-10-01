bfad07cc5c56803d5701ff0d951f29d36ebf78ae
Andrei Vinogradov <anri.vin@gmail.com>
2015-10-01 16:32:44 +0300

    Дополнение виджета для работы с типами населенных пунктов.

--- QSOsm/Widgets/CityEntry.cs
+++ QSOsm/Widgets/CityEntry.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Bindings;
 using System.Linq.Expressions;
 using System.Threading;
 using Gamma.Binding.Core;
 using Gtk;
 using QSOsm.DTO;
+using System.Text.RegularExpressions;
 
 namespace QSOsm
 {
@@ -12,9 +14,13 @@ namespace QSOsm
 	[System.ComponentModel.Category ("Gamma OSM Widgets")]
 	public class CityEntry : Entry
 	{
-		const int CityColumn = 1;
-		const int DistrictColumn = 2;
-		const int OsmIdColumn = 3;
+		enum columns
+		{
+			City,
+			District,
+			Locality,
+			OsmId
+		}
 
 		private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger ();
 
@@ -54,9 +60,25 @@ namespace QSOsm
 			}
 		}
 
+		LocalityType locality;
+
+		public LocalityType Locality {
+			get { 
+				return locality;
+			}
+			set {
+				locality = value;
+				UpdateText ();
+			}
+		}
+
+
 		void UpdateText ()
 		{
-			this.Text = String.IsNullOrWhiteSpace (CityDistrict) ? City : String.Format ("{0} ({1})", City, CityDistrict);
+			this.Text = String.IsNullOrWhiteSpace (CityDistrict) ? 
+				String.Format ("{0} {1}", Locality.GetEnumTitle (), City) : 
+				String.Format ("{0} {1} ({2})", Locality.GetEnumTitle (), City, CityDistrict);
+			
 			if (osmId == default(long) && City != default(string) && CityDistrict != default(string)) {
 				logger.Debug ("Запрос id для города {0}({1})...", City, CityDistrict);
 				IOsmService svc = OsmWorker.GetOsmService ();
@@ -73,22 +95,48 @@ namespace QSOsm
 		public CityEntry ()
 		{
 			Binding = new BindingControler<CityEntry> (this, new Expression<Func<CityEntry, object>>[] {
-				(w => w.City), (w => w.CityDistrict)
+				(w => w.City), (w => w.CityDistrict), (w => w.Locality)
 			});
 
-			this.Completion = new EntryCompletion ();
-			this.Completion.TextColumn = 0;
-			this.Completion.MatchSelected += Completion_MatchSelected;
 			this.TextInserted += CityEntryTextInserted;
+
+			this.Completion = new EntryCompletion ();
+			this.Completion.MatchSelected += Completion_MatchSelected;
+
+			this.Completion.MatchFunc = Completion_MatchFunc;
+			var cell = new CellRendererText ();
+			this.Completion.PackStart (cell, true);
+			this.Completion.SetCellDataFunc (cell, OnCellLayoutDataFunc);
+		}
+
+		void OnCellLayoutDataFunc (CellLayout cell_layout, CellRenderer cell, TreeModel tree_model, TreeIter iter)
+		{
+			var cityName = (string)tree_model.GetValue (iter, (int)columns.City);
+			var district = (string)tree_model.GetValue (iter, (int)columns.District);
+			var localityType = (LocalityType)tree_model.GetValue (iter, (int)columns.Locality);
+			string pattern = String.Format ("\\b{0}", Regex.Escape (Text.ToLower ()));
+			cityName = Regex.Replace (cityName, pattern, (match) => String.Format ("<b>{0}</b>", match.Value), RegexOptions.IgnoreCase);
+			(cell as CellRendererText).Markup = String.IsNullOrWhiteSpace (district) ? 
+				String.Format ("{0} {1}", localityType.GetEnumTitle (), cityName) : 
+				String.Format ("{0} {1} ({2})", localityType.GetEnumTitle (), cityName, district);
+		}
+
+		bool Completion_MatchFunc (EntryCompletion completion, string key, TreeIter iter)
+		{
+			var val = completion.Model.GetValue (iter, (int)columns.City).ToString ().ToLower ();
+			return Regex.IsMatch (val, String.Format ("\\b{0}.*", Regex.Escape (key.ToLower ())));
 		}
 
 		[GLib.ConnectBefore]
 		void Completion_MatchSelected (object o, MatchSelectedArgs args)
 		{
-			City = args.Model.GetValue (args.Iter, CityColumn).ToString ();
-			CityDistrict = args.Model.GetValue (args.Iter, DistrictColumn).ToString ();
-			osmId = (long)args.Model.GetValue (args.Iter, OsmIdColumn);
+			city = args.Model.GetValue (args.Iter, (int)columns.City).ToString ();
+			cityDistrict = args.Model.GetValue (args.Iter, (int)columns.District).ToString ();
+			locality = (LocalityType)args.Model.GetValue (args.Iter, (int)columns.Locality);
+			UpdateText ();
+			osmId = (long)args.Model.GetValue (args.Iter, (int)columns.OsmId);
 			OnCitySelected ();
+			args.RetVal = true;
 		}
 
 		protected virtual void OnCitySelected ()
@@ -112,12 +160,12 @@ namespace QSOsm
 			logger.Info ("Запрос городов...");
 			IOsmService svc = OsmWorker.GetOsmService ();
 			var cities = svc.GetCities ();
-			completionListStore = new ListStore (typeof(string), typeof(string), typeof(string), typeof(long));
+			completionListStore = new ListStore (typeof(string), typeof(string), typeof(LocalityType), typeof(long));
 			foreach (var c in cities) {
 				completionListStore.AppendValues (
-					String.IsNullOrWhiteSpace (c.SuburbDistrict) ? c.Name : String.Format ("{0} ({1})", c.Name, c.SuburbDistrict),
 					c.Name,
 					c.SuburbDistrict,
+					c.LocalityType,
 					c.OsmId
 				);
 			}
@@ -128,7 +176,7 @@ namespace QSOsm
 
 		protected override void OnChanged ()
 		{
-			Binding.FireChange (w => w.City, w => w.CityDistrict);
+			Binding.FireChange (w => w.City, w => w.CityDistrict, w => w.Locality);
 			base.OnChanged ();
 		}
 	}
