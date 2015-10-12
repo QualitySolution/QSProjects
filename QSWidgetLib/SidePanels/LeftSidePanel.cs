a305beeab05a7a1a5a6a002c31e060b27dccfeec
Andrey Gankov <gav@qsolution.ru>
2015-10-12 23:26:32 +0300

    Добавлен вижет левой панели.

---
+++ QSWidgetLib/SidePanels/LeftSidePanel.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace QSWidgetLib
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	public partial class LeftSidePanel : Gtk.Bin
+	{
+		Gtk.Widget panel;
+
+		public Gtk.Widget Panel
+		{
+			get
+			{
+				return panel;
+			}
+			set
+			{ 
+				if (panel == value)
+					return;
+				if(panel != null)
+				{
+					hboxMain.Remove (panel);
+				}
+				panel = value;
+
+				if(panel != null)
+				{
+					hboxMain. PackStart (panel);
+					hboxMain.ReorderChild (panel, 0);
+					panel.Visible = !IsHided;
+				}
+			}
+		}
+
+		public string Title {
+			get {
+				return labelTitle.LabelProp;
+			}
+			set{ labelTitle.LabelProp = value;
+			}
+		}
+
+		public bool IsHided
+		{
+			get
+			{
+				return arrowSlider.ArrowType == Gtk.ArrowType.Right;
+			}
+			set
+			{
+				if(value)
+				{
+					arrowSlider.ArrowType = Gtk.ArrowType.Right;
+				}
+				else
+				{
+					arrowSlider.ArrowType = Gtk.ArrowType.Left;
+				}
+				if(Panel != null)
+					panel.Visible = !value;
+			}
+		}
+
+		public LeftSidePanel ()
+		{
+			this.Build ();
+		}
+
+		protected void OnEventboxArrowButtonPressEvent (object o, Gtk.ButtonPressEventArgs args)
+		{
+			IsHided = !IsHided;
+		}
+	}
+}
+
