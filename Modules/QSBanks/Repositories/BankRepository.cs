2f8de5803f70729af52fbb05c4da7906ceed851d
Andrei Vinogradov <anri.vin@gmail.com>
2015-10-15 16:25:42 +0300

    Выборка банка по БИК.

--- Modules/QSBanks/Repositories/BankRepository.cs
+++ Modules/QSBanks/Repositories/BankRepository.cs
@@ -1,5 +1,6 @@
 ﻿using QSOrmProject;
 using System.Collections.Generic;
+using System;
 
 namespace QSBanks.Repository
 {
@@ -8,6 +9,16 @@ namespace QSBanks.Repository
 		public static IList<Bank> ActiveBanks (IUnitOfWork uow)
 		{
 			return uow.Session.QueryOver<Bank> ().Where (b => !b.Deleted).List<Bank> ();
+		}
+
+		public static Bank GetBankByBik (IUnitOfWork uow, string bik)
+		{
+			if (String.IsNullOrEmpty (bik))
+				return null;
+			return uow.Session.QueryOver<Bank> ()
+				.Where (b => b.Bik == bik)
+				.Take (1)
+				.SingleOrDefault ();
 		}
 	}
 }
