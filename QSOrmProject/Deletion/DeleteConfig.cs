9c3bbdb83d7a660a0efd7895f140ef8dac7f0551
Andrei Vinogradov <anri.vin@gmail.com>
2015-10-27 14:31:26 +0300

    Добавил возможность дописывать конфиг удаления.

--- QSOrmProject/Deletion/DeleteConfig.cs
+++ QSOrmProject/Deletion/DeleteConfig.cs
@@ -3,12 +3,13 @@ using System.Collections.Generic;
 
 namespace QSOrmProject.Deletion
 {
-	public static partial class DeleteConfig{
+	public static partial class DeleteConfig
+	{
 		private static List<DeleteInfo> classInfos;
 
 		internal static List<DeleteInfo> ClassInfos {
-			get {if(classInfos == null)
-				{
+			get {
+				if (classInfos == null) {
 					classInfos = new List<DeleteInfo> ();
 					QSProjectsLib.QSMain.RunOrmDeletion += RunDeletionFromProjectLib;
 				}
@@ -26,7 +27,7 @@ namespace QSOrmProject.Deletion
 			e.Result = OrmMain.DeleteObject (e.TableName, e.ObjectId);
 		}
 
-		public static void AddDeleteInfo(DeleteInfo info)
+		public static void AddDeleteInfo (DeleteInfo info)
 		{
 			if (ClassInfos.Exists (i => i.TableName == info.TableName && i.ObjectClass == info.ObjectClass))
 				throw new InvalidOperationException (String.Format ("Описание удаления для класса {0} и таблицы {1}, уже существует.", info.ObjectClass, info.TableName));
@@ -34,30 +35,34 @@ namespace QSOrmProject.Deletion
 			ClassInfos.Add (info);
 		}
 
-		public static void AddDeleteDependence<ToClass>(DeleteDependenceInfo deleteDependence)
+		public static DeleteInfo ExistingConfig<T> ()
+		{
+			return ClassInfos.Find (i => i.ObjectClass == typeof(T));
+		}
+
+		public static void AddDeleteDependence<ToClass> (DeleteDependenceInfo deleteDependence)
 		{
 			var info = ClassInfos.Find (i => i.ObjectClass == typeof(ToClass));
 
-			if(info == null)
+			if (info == null)
 				throw new InvalidOperationException (String.Format ("Описание удаления для класса {0} не найдено.", typeof(ToClass)));
 
-			info.DeleteItems.Add(deleteDependence);
+			info.DeleteItems.Add (deleteDependence);
 		}
 
-		public static void AddClearDependence<ToClass>(ClearDependenceInfo clearDependence)
+		public static void AddClearDependence<ToClass> (ClearDependenceInfo clearDependence)
 		{
 			var info = ClassInfos.Find (i => i.ObjectClass == typeof(ToClass));
 
-			if(info == null)
+			if (info == null)
 				throw new InvalidOperationException (String.Format ("Описание удаления для класса {0} не найдено.", typeof(ToClass)));
 
-			info.ClearItems.Add(clearDependence);
+			info.ClearItems.Add (clearDependence);
 		}
-			
-		internal static void OnAfterDeletion(System.Data.Common.DbTransaction trans, List<DeletedItem> items)
+
+		internal static void OnAfterDeletion (System.Data.Common.DbTransaction trans, List<DeletedItem> items)
 		{
-			if(AfterDeletion != null)
-			{
+			if (AfterDeletion != null) {
 				AfterDeletion (null, new AfterDeletionEventArgs {
 					CurTransaction = trans,
 					DeletedItems = items
