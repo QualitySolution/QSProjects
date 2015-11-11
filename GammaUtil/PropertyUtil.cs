015a9a2a2574f78ae8c5db78746e88acaf4a8c02
Andrey Gankov <gav@qsolution.ru>
2015-11-11 12:39:06 +0300

    Added method to get PropertyInfo from untyped Expression.

--- GammaUtil/PropertyUtil.cs
+++ GammaUtil/PropertyUtil.cs
@@ -55,6 +55,35 @@ namespace Gamma.Utilities
 		{
 			return GetMemberInfo (propertyRefExpr) as PropertyInfo;
 		}
+
+		public static MemberInfo GetMemberInfo (Expression propertyRefExpr)
+		{
+			if (propertyRefExpr == null)
+				throw new ArgumentNullException ("propertyRefExpr", "propertyRefExpr is null.");
+
+			LambdaExpression lamda = propertyRefExpr as LambdaExpression;
+
+			if (lamda == null)
+				throw new ArgumentException ("propertyRefExpr will be lamda function.", "propertyRefExpr");
+
+			MemberExpression memberExpr = lamda.Body as MemberExpression;
+
+			if (memberExpr == null) {
+				UnaryExpression unaryExpr = lamda.Body as UnaryExpression;
+				if (unaryExpr != null && unaryExpr.NodeType == ExpressionType.Convert)
+					memberExpr = unaryExpr.Operand as MemberExpression;
+			}
+
+			if (memberExpr != null && memberExpr.Member.MemberType == MemberTypes.Property)
+				return memberExpr.Member;
+			else
+				return null;
+		}
+
+		public static PropertyInfo GetPropertyInfo (Expression propertyRefExpr)
+		{
+			return GetMemberInfo (propertyRefExpr) as PropertyInfo;
+		}
 	}
 }
 
