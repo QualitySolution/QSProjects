8d95860e10ccecfe9eab5375ba83794c287c04af
Andrey Gankov <gav@qsolution.ru>
2015-11-17 18:00:58 +0300

    При обновлении базы теперь так же устанавливаем микро обновления.

--- QSUpdater/DBUpdater/DBUpdateProcess.cs
+++ QSUpdater/DBUpdater/DBUpdateProcess.cs
@@ -3,6 +3,7 @@ using QSProjectsLib;
 using System.IO;
 using MySql.Data.MySqlClient;
 using System.Text.RegularExpressions;
+using QSSupportLib;
 
 namespace QSUpdater.DB
 {
@@ -32,30 +33,22 @@ namespace QSUpdater.DB
 			logger.Info("Обновяем базу данных до версии {0}", StringWorks.VersionToShortString(updateHop.Destanation));
 			try
 			{
-				string sql;
+				logger.Info ("Проверяем все ли микро обновления установленны.");
+				ExecuteMicroUpdates ();
 
-				using(Stream stream = System.Reflection.Assembly.GetEntryAssembly().GetManifestResourceStream(updateHop.Resource))
-				{
-					if(stream == null)
-						throw new InvalidOperationException( String.Format("Ресурс {0} указанный в обновлениях не найден.", updateHop.Resource));
-					StreamReader reader = new StreamReader(stream);
-					sql = reader.ReadToEnd();
-				}
-				progressbar1.Text = String.Format ("Обновление базы данных до версии {0}", 
+				logger.Info ("Устанавливаем основное обновление.");
+				RunOneUpdate (updateHop, String.Format ("Обновление базы данных до версии {0}", 
 					StringWorks.VersionToShortString (updateHop.Destanation)
-				);
-				int predictedCount = Regex.Matches( sql, ";").Count;
+				));
 
-				logger.Debug ("Предпологаем наличие {0} команд.", predictedCount);
+				if(MainSupport.BaseParameters.All.ContainsKey("micro_updates"))
+					MainSupport.BaseParameters.RemoveParameter (QSMain.ConnectionDB,
+						"micro_updates");
 
-				progressbar1.Adjustment.Value = 0;
-				progressbar1.Adjustment.Upper = predictedCount;
-				QSMain.WaitRedraw ();
+				MainSupport.LoadBaseParameters ();
 
-				var script = new MySqlScript(QSMain.connectionDB, sql);
-				script.StatementExecuted += Script_StatementExecuted;
-				var commands = script.Execute ();
-				logger.Debug ("Выполнено {0} команд.", commands);
+				logger.Info ("Доустанавливаем микро обновления.");
+				ExecuteMicroUpdates ();
 
 				progressbar1.Adjustment.Value = progressbar1.Adjustment.Upper;
 				logger.Info("Обновление до версии {0}, завершено.", StringWorks.VersionToShortString(updateHop.Destanation));
@@ -65,8 +58,59 @@ namespace QSUpdater.DB
 			catch(Exception ex)
 			{
 				QSMain.ErrorMessageWithLog("Ошибка при обновлении базы..", logger, ex);
+				buttonOk.Sensitive = false;
 				return;
 			}
+		}
+
+		void ExecuteMicroUpdates()
+		{
+			Version currentDB;
+			if(MainSupport.BaseParameters.All.ContainsKey("micro_updates"))
+				currentDB  = Version.Parse(MainSupport.BaseParameters.All["micro_updates"]);
+			else 
+				currentDB = Version.Parse(MainSupport.BaseParameters.Version);
+
+			while(DBUpdater.microUpdates.Exists(u => u.Source == currentDB))
+			{
+				var update = DBUpdater.microUpdates.Find(u => u.Source == currentDB);
+				RunOneUpdate (update, String.Format ("Устанавливаем микро-обновление {0}", StringWorks.VersionToShortString (update.Destanation)));
+				currentDB = update.Destanation;
+				MainSupport.BaseParameters.UpdateParameter(
+					QSMain.ConnectionDB,
+					"micro_updates",
+					StringWorks.VersionToShortString(currentDB)
+				);
+			}
+		}
+
+		void RunOneUpdate(UpdateHop updateScript, string operationName)
+		{
+			logger.Info (operationName);
+			textviewLog.Buffer.Text = textviewLog.Buffer.Text + operationName + "\n";
+
+			string sql;
+			using(Stream stream = System.Reflection.Assembly.GetEntryAssembly().GetManifestResourceStream(updateScript.Resource))
+			{
+				if(stream == null)
+					throw new InvalidOperationException( String.Format("Ресурс {0} указанный в обновлениях не найден.", updateScript.Resource));
+				StreamReader reader = new StreamReader(stream);
+				sql = reader.ReadToEnd();
+			}
+
+			int predictedCount = Regex.Matches( sql, ";").Count;
+
+			logger.Debug ("Предполагаем наличие {0} команд в скрипте.", predictedCount);
+
+			progressbar1.Text = operationName;
+			progressbar1.Adjustment.Value = 0;
+			progressbar1.Adjustment.Upper = predictedCount;
+			QSMain.WaitRedraw ();
+
+			var script = new MySqlScript(QSMain.connectionDB, sql);
+			script.StatementExecuted += Script_StatementExecuted;
+			var commands = script.Execute ();
+			logger.Debug ("Выполнено {0} SQL-команд.", commands);
 		}
 
 		void Script_StatementExecuted (object sender, MySqlScriptEventArgs args)
