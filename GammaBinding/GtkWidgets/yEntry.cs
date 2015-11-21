0f32f6ebb810f83256d5e2a05c1e6d8e4324c3a5
Andrey Gankov <gav@qsolution.ru>
2015-11-21 19:07:28 +0300

    Added yEntry widget.

---
+++ GammaBinding/GtkWidgets/yEntry.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gtk;
+using Gamma.Binding.Core;
+using System.Linq.Expressions;
+
+namespace Gamma.GtkWidgets
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	[System.ComponentModel.Category ("Gamma Gtk")]
+	public class yEntry : Entry
+	{
+		public BindingControler<yEntry> Binding { get; private set;}
+
+		public yEntry ()
+		{
+			Binding = new BindingControler<yEntry> (this, new Expression<Func<yEntry, object>>[] {
+				(w => w.Text)
+			});
+		}
+
+		protected override void OnChanged ()
+		{
+			Binding.FireChange (w => w.Text);
+			base.OnChanged ();
+		}
+	}
+}
+
