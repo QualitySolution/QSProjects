93f86a6da26ecfaadff6c2fe37177fb36117288b
Andrey Gankov <gav@qsolution.ru>
2015-11-22 05:52:12 +0300

    Added help method to get attribute from enum value

---
+++ GammaUtil/EnumUtil.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Gamma.Utilities
+{
+	public static class EnumUtil
+	{
+		public static T GetAttribute<T>(this Enum value) where T : Attribute {
+			var type = value.GetType();
+			var memberInfo = type.GetMember(value.ToString());
+			var attributes = memberInfo[0].GetCustomAttributes (typeof(T), false);
+			if (attributes.Length > 0)
+				return (T)attributes [0];
+			else
+				return null;
+		}
+	}
+}
+
