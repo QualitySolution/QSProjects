93fede9cc64d1a65ca45631264d3c9c45da1a87f
Andrey Gankov <gav@qsolution.ru>
2015-11-22 23:12:35 +0300

    Added yComboBox.

---
+++ GammaBinding/GtkWidgets/yComboBox.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Gtk;
+using Gamma.Binding.Core;
+using System.Linq.Expressions;
+
+namespace Gamma.GtkWidgets
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	[System.ComponentModel.Category ("Gamma Gtk")]
+	public class yComboBox : ComboBox
+	{
+		public BindingControler<yComboBox> Binding { get; private set;}
+
+		public yComboBox ()
+		{
+			Binding = new BindingControler<yComboBox> (this, new Expression<Func<yComboBox, object>>[] {
+				(w => w.Active),
+				(w => w.ActiveText),
+			});
+		}
+
+		protected override void OnChanged ()
+		{
+			Binding.FireChange (w => w.Active, w => w.ActiveText);
+			base.OnChanged ();
+		}
+	}
+}
+
