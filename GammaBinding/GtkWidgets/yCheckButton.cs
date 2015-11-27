8f7bf6063048e3565f8f184c65e3470366e4c93b
Andrei Vinogradov <anri.vin@gmail.com>
2015-11-27 13:18:19 +0300

    Добавил виджет CheckButton

---
+++ GammaBinding/GtkWidgets/yCheckButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gtk;
+using Gamma.Binding.Core;
+using System.Linq.Expressions;
+
+namespace Gamma.GtkWidgets
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	[System.ComponentModel.Category ("Gamma Gtk")]
+	public class yCheckButton : CheckButton
+	{
+		public BindingControler<yCheckButton> Binding { get; private set; }
+
+		public yCheckButton ()
+		{
+			Binding = new BindingControler<yCheckButton> (this, new Expression<Func<yCheckButton, object>>[] {
+				(w => w.Active)
+			});
+		}
+
+		protected override void OnToggled ()
+		{
+			Binding.FireChange (w => w.Active);
+			base.OnToggled ();
+		}
+	}
+}
+
