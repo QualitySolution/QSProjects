2bd0d31ad2b4714bd6d2bb39d8d3284174fc4c13
Andrei Vinogradov <anri.vin@gmail.com>
2015-11-27 14:39:40 +0300

    Добавил yEntryReferenceVM.

---
+++ QSOrmProject/yWidgets/yEntryReferenceVM.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel;
+using Gamma.Binding.Core;
+using System.Linq.Expressions;
+
+namespace QSOrmProject
+{
+	[ToolboxItem (true)]
+	[Category ("Gamma Widgets")]
+	public class yEntryReferenceVM : EntryReferenceVM
+	{
+		public BindingControler<yEntryReferenceVM> Binding { get; private set; }
+
+		public yEntryReferenceVM ()
+		{
+			Binding = new BindingControler<yEntryReferenceVM> (this, new Expression<Func<yEntryReferenceVM, object>>[] {
+				(w => w.Subject)
+			});
+		}
+
+		protected override void OnChanged ()
+		{
+			Binding.FireChange (new Expression<Func<yEntryReferenceVM, object>>[] {
+				(w => w.Subject)
+			});
+			base.OnChanged ();
+		}
+	}
+}
+
