0d9f4e41cd5ca0e9e29282b110e85e4e33469d6d
Andrey Gankov <gav@qsolution.ru>
2015-11-30 16:48:34 +0300

    Добавлено событие EntitySaved

--- QSTDI/ITdiDialog.cs
+++ QSTDI/ITdiDialog.cs
@@ -6,6 +6,19 @@ namespace QSTDI
 	{
 		bool HasChanges { get;}
 		bool Save();
+		event EventHandler<EntitySavedEventArgs> EntitySaved;
+	}
+
+	public class EntitySavedEventArgs : EventArgs
+	{
+		public object Entity { get; private set;}
+		public bool TabClosed { get; private set;}
+
+		public EntitySavedEventArgs(object entity, bool tabClosed = false)
+		{
+			Entity = entity;
+			TabClosed = tabClosed;
+		}
 	}
 }
 
