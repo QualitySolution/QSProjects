d4efc641f426a181981f78efac41f4bfdeffd801
Dmitry Nikitin <baragon92@gmail.com>
2016-03-15 12:16:39 +0300

    Добавлен виджет правой панели

--- QSWidgetLib/SidePanels/RightSidePanel.cs
+++ QSWidgetLib/SidePanels/RightSidePanel.cs
@@ -5,10 +5,70 @@ namespace QSWidgetLib
 	[System.ComponentModel.ToolboxItem(true)]
 	public partial class RightSidePanel : Gtk.Bin
 	{
-		public RightSidePanel()
+		Gtk.Widget panel;
+
+		public Gtk.Widget Panel
 		{
-			this.Build();
+			get
+			{
+				return panel;
+			}
+			set
+			{ 
+				if (panel == value)
+					return;
+				if(panel != null)
+				{
+					hboxMain.Remove (panel);
+				}
+				panel = value;
+
+				if(panel != null)
+				{
+					hboxMain. PackEnd (panel);
+					hboxMain.ReorderChild (panel, 1);
+					panel.Visible = !IsHided;
+				}
+			}
+		}
+
+		public string Title {
+			get {
+				return labelTitle.LabelProp;
+			}
+			set{ labelTitle.LabelProp = value;
+			}
+		}
+
+		public bool IsHided
+		{
+			get
+			{
+				return arrowSlider.ArrowType == Gtk.ArrowType.Left;
+			}
+			set
+			{
+				if(value)
+				{
+					arrowSlider.ArrowType = Gtk.ArrowType.Left;
+				}
+				else
+				{
+					arrowSlider.ArrowType = Gtk.ArrowType.Right;
+				}
+				if(Panel != null)
+					panel.Visible = !value;
+			}
+		}
+
+		public RightSidePanel ()
+		{
+			this.Build ();
+		}
+
+		protected void OnEventboxArrowButtonPressEvent (object o, Gtk.ButtonPressEventArgs args)
+		{
+			IsHided = !IsHided;
 		}
 	}
 }
-
