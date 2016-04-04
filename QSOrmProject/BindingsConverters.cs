4546493bf93ffa69ae9539fd418b43e68e03737e
Andrey Gankov <gav@qsolution.ru>
2016-04-04 15:49:06 +0300

    Добавлен конвертер из нулабле типов в ноль в виджете.

--- QSOrmProject/BindingsConverters.cs
+++ QSOrmProject/BindingsConverters.cs
@@ -38,5 +38,27 @@ namespace QSOrmProject
 		}
 	}
 
+	public class NullToZeroConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (targetType == typeof(int))
+			{
+				return value == null ? (int)0 : System.Convert.ToInt32(value);
+			}
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (targetType == typeof(int?))
+			{
+				var converted = System.Convert.ToInt32(value);
+				return converted == 0 ? (int?)null : converted;
+			}
+			return null;
+		}
+	}
+
 }
 
