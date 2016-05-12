ccee36957bf902d75fbab3cd5575a2294b9e0425
Andrey Gankov <gav@qsolution.ru>
2016-05-12 11:28:53 +0300

    Исправлено сокрашение улицы.

--- QSOsm/AddressHelper.cs
+++ QSOsm/AddressHelper.cs
@@ -15,77 +15,77 @@ namespace QSOsm
 		{
 			//Сокращаем статусную часть.
 			if (street.Contains("улица"))
-				street.Replace("улица", "ул.");
+				street = street.Replace("улица", "ул.");
 			else if (street.Contains("проспект"))
-				street.Replace("проспект", "пр.");
+				street = street.Replace("проспект", "пр.");
 			else if (street.Contains("бульвар"))
-				street.Replace("бульвар", "бульв.");
+				street = street.Replace("бульвар", "бульв.");
 			else if (street.Contains("дорога"))
-				street.Replace("дорога", "дор.");
+				street = street.Replace("дорога", "дор.");
 			else if (street.Contains("канал"))
-				street.Replace("канал", "кан.");
+				street = street.Replace("канал", "кан.");
 			else if (street.Contains("набережная"))
-				street.Replace("набережная", "наб.");
+				street = street.Replace("набережная", "наб.");
 			else if (street.Contains("переулок"))
-				street.Replace("переулок", "пер.");
+				street = street.Replace("переулок", "пер.");
 			else if (street.Contains("площадь"))
-				street.Replace("площадь", "пл.");
+				street = street.Replace("площадь", "пл.");
 
 			//Сокращаем пояснительное слово
 			if (street.Contains("Большой"))
-				street.Replace("Большой", "Б.");
+				street = street.Replace("Большой", "Б.");
 			else if (street.Contains("Большая"))
-				street.Replace("Большая", "Б.");
+				street = street.Replace("Большая", "Б.");
 			else if (street.Contains("Верхний"))
-				street.Replace("Верхний", "Верх.");
+				street = street.Replace("Верхний", "Верх.");
 			else if (street.Contains("Верхняя"))
-				street.Replace("Верхняя", "Верх.");
+				street = street.Replace("Верхняя", "Верх.");
 			else if (street.Contains("Малый"))
-				street.Replace("Малый", "М.");
+				street = street.Replace("Малый", "М.");
 			else if (street.Contains("Малая"))
-				street.Replace("Малая", "М.");
+				street = street.Replace("Малая", "М.");
 			else if (street.Contains("Новый"))
-				street.Replace("Новый", "Н.");
+				street = street.Replace("Новый", "Н.");
 			else if (street.Contains("Новая"))
-				street.Replace("Новая", "Н.");
+				street = street.Replace("Новая", "Н.");
 			else if (street.Contains("Нижний"))
-				street.Replace("Нижний", "Нижн.");
+				street = street.Replace("Нижний", "Нижн.");
 			else if (street.Contains("Нижняя"))
-				street.Replace("Нижняя", "Нижн.");
+				street = street.Replace("Нижняя", "Нижн.");
 			else if (street.Contains("Средний"))
-				street.Replace("Средний", "Ср.");
+				street = street.Replace("Средний", "Ср.");
 			else if (street.Contains("Средняя"))
-				street.Replace("Средняя", "Ср.");
+				street = street.Replace("Средняя", "Ср.");
 			else if (street.Contains("Старый"))
-				street.Replace("Старый", "Ст.");
+				street = street.Replace("Старый", "Ст.");
 			else if (street.Contains("Старая"))
-				street.Replace("Старая", "Ст.");
+				street = street.Replace("Старая", "Ст.");
 			else if (street.Contains("Восточный"))
-				street.Replace("Восточный", "Вост.");
+				street = street.Replace("Восточный", "Вост.");
 			else if (street.Contains("Восточная"))
-				street.Replace("Восточная", "Вост.");
+				street = street.Replace("Восточная", "Вост.");
 			else if (street.Contains("Южный"))
-				street.Replace("Южный", "Южн.");
+				street = street.Replace("Южный", "Южн.");
 			else if (street.Contains("Южная"))
-				street.Replace("Южная", "Южн.");
+				street = street.Replace("Южная", "Южн.");
 			else if (street.Contains("Западный"))
-				street.Replace("Западный", "Зап.");
+				street = street.Replace("Западный", "Зап.");
 			else if (street.Contains("Западная"))
-				street.Replace("Западная", "Зап.");
+				street = street.Replace("Западная", "Зап.");
 			else if (street.Contains("Северный"))
-				street.Replace("Северный", "Сев.");
+				street = street.Replace("Северный", "Сев.");
 			else if (street.Contains("Северная"))
-				street.Replace("Северная", "Сев.");
+				street = street.Replace("Северная", "Сев.");
 
 			//Сокращаем слова в основной части
 			if (street.Contains("Братьев"))
-				street.Replace("Братьев", "Бр.");
+				street = street.Replace("Братьев", "Бр.");
 			else if (street.Contains("Красного"))
-				street.Replace("Красного", "Кр.");
+				street = street.Replace("Красного", "Кр.");
 			else if (street.Contains("Красной"))
-				street.Replace("Красной", "Кр.");
+				street = street.Replace("Красной", "Кр.");
 			else if (street.Contains("Красных"))
-				street.Replace("Красных", "Кр.");
+				street = street.Replace("Красных", "Кр.");
 
 			return street;
 		}
