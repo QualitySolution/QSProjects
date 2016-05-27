c102e6f957281d769ec16fbe8ec05a586742cb81
Andrey Gankov <gav@qsolution.ru>
2016-05-27 16:48:30 +0300

    Добавлен виджет строки поиска.

---
+++ QSWidgetLib/SearchEntity.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace QSWidgetLib
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class SearchEntity : Gtk.Bin
+	{
+		public event EventHandler TextChanged;
+
+		public string Text {
+			get{
+				return entrySearchText.Text;
+			}
+			set{
+				entrySearchText.Text = value;
+			}
+		}
+
+		public SearchEntity()
+		{
+			this.Build();
+		}
+
+		protected void OnButtonClearClicked(object sender, EventArgs e)
+		{
+			entrySearchText.Text = String.Empty;
+		}
+
+		protected void OnEntrySearchTextChanged(object sender, EventArgs e)
+		{
+			if (TextChanged != null)
+				TextChanged(this, EventArgs.Empty);
+		}
+	}
+}
+
