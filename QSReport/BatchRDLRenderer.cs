17c00a1e4753db52d38b67ecf7e46c7682b31f17
Andrey Gankov <gav@qsolution.ru>
2016-06-10 12:37:57 +0300

    Перенес массовую печать документов из водовоза.

---
+++ QSReport/BatchRDLRenderer.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using fyiReporting.RDL;
+using QSProjectsLib;
+using System.Collections.Generic;
+using System.Linq;
+using Gtk;
+using fyiReporting.RdlGtkViewer;
+using NLog;
+
+namespace QSReport
+{
+	public class BatchRDLRenderer
+	{		
+		IList<Pages> reportPages;
+		IList<IPrintableDocument> documents;
+		private Logger log = LogManager.GetCurrentClassLogger ();
+
+		public int PageCount
+		{
+			get
+			{
+				return reportPages.Sum(pages => pages.PageCount);
+			}
+		}
+
+		public BatchRDLRenderer(IEnumerable<IPrintableDocument> documents)
+		{							
+			this.documents = documents.ToList();
+			reportPages = new List<Pages>();
+		}
+
+		public void PrepareDocuments(IWorker worker)
+		{
+			int step = 0;
+			foreach (var document in documents)
+			{
+				worker.ReportProgress(step, document.Name);
+				Prepare(document);
+				if (worker.IsCancelled)
+					return;
+				step++;
+			}
+		}
+
+		protected void Prepare(IPrintableDocument document)
+		{
+			if (document.PrintType == PrinterType.RDL)
+			{
+				Report report = GetReportFromFile(document.GetReportInfo().GetReportUri().LocalPath);
+				report.RunGetData(document.GetReportInfo().Parameters);
+				reportPages.Add(report.BuildPages());
+			}
+		}
+
+		private Report GetReportFromFile(string path)
+		{
+			RDLParser rdlp;
+			Report r;
+			string source = System.IO.File.ReadAllText(path);
+
+			rdlp = new RDLParser(source);
+			rdlp.Folder = System.IO.Path.GetDirectoryName (path);
+			rdlp.OverwriteConnectionString = QSMain.ConnectionString;
+			rdlp.OverwriteInSubreport = true;
+
+			r = rdlp.Parse();
+
+			return r;
+		}
+
+		public void DrawPage(object o,DrawPageArgs args){
+			var g = args.Context.CairoContext;
+			var printContext = args.Context;
+			int pageNumber = args.PageNr;
+			int reportNumber = 0;
+			while (pageNumber >= reportPages[reportNumber].PageCount)
+			{
+				pageNumber -= reportPages[reportNumber].PageCount;
+				reportNumber++;
+			}
+
+			g.Save();
+			if (documents[reportNumber].Orientation == DocumentOrientation.Landscape)
+			{
+				g.Translate(printContext.Width, 0);
+				g.Rotate(Math.PI / 2);
+			}
+			RenderCairo render = new RenderCairo(g);
+			render.RunPage(reportPages[reportNumber][pageNumber]);		
+			g.Restore();
+		}
+	}
+}
+
---
+++ QSReport/DocumentPrinter.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Gtk;
+using QSProjectsLib;
+
+namespace QSReport
+{
+	public class DocumentPrinter
+	{		
+		public static void Print(IPrintableDocument document)
+		{
+			PrintAll(new IPrintableDocument[]{ document });
+		}		
+
+
+		public static void PrintAll(IEnumerable<IPrintableDocument> documents)
+		{
+			PrintOperation printOp;
+			printOp = new PrintOperation();
+			printOp.Unit = Unit.Points;
+			printOp.UseFullPage = true;
+			printOp.ShowProgress = true;
+			var documentsRDL = documents.Where(doc=>doc.PrintType==PrinterType.RDL);
+			BatchRDLRenderer renderer = new BatchRDLRenderer(documentsRDL);
+
+			var result = LongOperationDlg.StartOperation(
+				renderer.PrepareDocuments,
+				"Подготовка к печати...",
+				documentsRDL.Count()
+			);
+			if (result == LongOperationResult.Canceled)
+				return;
+			
+			printOp.NPages = renderer.PageCount;
+
+			printOp.DrawPage += renderer.DrawPage;
+			printOp.Run(PrintOperationAction.PrintDialog, null);
+		}			
+			
+
+		public static QSTDI.TdiTabBase GetPreviewTab(IPrintableDocument document)
+		{
+			return new QSReport.ReportViewDlg (document.GetReportInfoForPreview());				
+		}
+	}		
+
+	public interface IPrintableDocument
+	{
+		PrinterType PrintType{ get; }
+		DocumentOrientation Orientation{ get; }
+		string Name { get;}
+		QSReport.ReportInfo GetReportInfo ();
+		QSReport.ReportInfo GetReportInfoForPreview();
+	}
+
+	public enum PrinterType{
+		None, RDL, ODT
+	}
+
+	public enum DocumentOrientation{
+		Portrait,Landscape
+	}
+
+
+}
