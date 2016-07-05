549df9178a12c375787dc75b8b1721843995350f
Andrey Gankov <gav@qsolution.ru>
2016-07-05 16:27:31 +0300

    Базовый класс для фильтра репрезентации.

---
+++ QSOrmProject/RepresentationModel/RepresentationFilterBase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace QSOrmProject.RepresentationModel
+{
+	public abstract class RepresentationFilterBase : Gtk.Bin, IRepresentationFilter
+	{
+		IUnitOfWork uow;
+
+		public IUnitOfWork UoW {
+			get {
+				return uow;
+			}
+			set {
+				uow = value;
+				OnUoWSet();
+			}
+		}
+
+		public event EventHandler Refiltered;
+
+		protected void OnRefiltered ()
+		{
+			if (Refiltered != null)
+				Refiltered (this, new EventArgs ());
+		}
+
+		public RepresentationFilterBase()
+		{
+		}
+
+		public RepresentationFilterBase(IUnitOfWork uow)
+		{
+			UoW = uow;
+		}
+
+		protected virtual void OnUoWSet()
+		{
+			
+		}
+	}
+}
+
