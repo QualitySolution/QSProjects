ae77333f508b5e4108c65375630a08c304e4b2f5
Andrey Gankov <gav@qsolution.ru>
2016-08-04 12:53:10 +0300

    Добавлена функция подсчета количества true.

---
+++ QSProjectsLib/Helpers/BooleanWorks.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace QSProjectsLib
+{
+	public static class BooleanWorks
+	{
+		public static int CountTrue(params bool[] args)
+		{
+			return args.Count(t => t);
+		}
+	}
+}
+
