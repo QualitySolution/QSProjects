55aa71b9e766a8d85264640bfe07536755ec3fba
Andrey Gankov <gav@qsolution.ru>
2016-10-17 09:47:19 +0300

    Добавлен конвертер пустой строки в null.

--- QSOrmProject/BindingsConverters.cs
+++ QSOrmProject/BindingsConverters.cs
@@ -77,5 +77,27 @@ namespace QSOrmProject
 		}
 	}
 
+	public class NullToEmptyStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (targetType == typeof(string))
+			{
+				return value ?? String.Empty;
+			}
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (targetType == typeof(string))
+			{
+				var converted = (string)value;
+				return String.IsNullOrWhiteSpace(converted) ? null : converted;
+			}
+			return null;
+		}
+	}
+
 }
 
