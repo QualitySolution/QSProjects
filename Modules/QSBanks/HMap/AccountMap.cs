e4d13b0fd03c2cf150f950ace69dc1870cc11cc5
Andrey Gankov <gav@qsolution.ru>
2017-01-08 23:36:07 +0300

    Мапинг счета перенесен в отдельный файл.

---
+++ Modules/QSBanks/HMap/AccountMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+
+namespace QSBanks.HMap
+{
+	public class AccountMap : ClassMap<Account>
+	{
+		public AccountMap ()
+		{
+			Table("accounts");
+
+			//ВАЖНО : Если будет включена ленивая загрузка перестанет работать слонер объекта в подчиненном UoW.
+			Not.LazyLoad();
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+
+			Map(x => x.Name).Column("name");
+			Map(x => x.Number).Column("number");
+			Map(x => x.Code1c).Column("code_1c");
+			Map(x => x.Inactive).Column("inactive");
+
+			References(x => x.InBank).Column("bank_id");
+		}
+	}
+}
+
