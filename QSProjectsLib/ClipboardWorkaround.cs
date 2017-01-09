b554e9137dd14b39da791d213fd864e729122ad1
Andrey Gankov <gav@qsolution.ru>
2017-01-09 11:40:53 +0300

    Перехват клавиш для обработки сохранения и обходного пути с буфером обмена, вынесены в библиотеку. Что бы использовать один код из разных проектов.

---
+++ QSProjectsLib/ClipboardWorkaround.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Gtk;
+
+namespace QSProjectsLib
+{
+	public static class ClipboardWorkaround
+	{
+		private static Gtk.Clipboard clipboard = Gtk.Clipboard.Get (Gdk.Atom.Intern ("CLIPBOARD", false));
+
+		public static void HandleKeyReleaseEvent (object o, KeyReleaseEventArgs args)
+		{
+			int platform = (int)Environment.OSVersion.Platform;
+			int version = (int)Environment.OSVersion.Version.Major;
+			Gdk.ModifierType modifier;
+
+			//Kind of MacOSX
+			if ((platform == 4 || platform == 6 || platform == 128) && version > 8)
+				modifier = Gdk.ModifierType.MetaMask | Gdk.ModifierType.Mod1Mask;
+			//Kind of Windows or Unix
+			else
+				modifier = Gdk.ModifierType.ControlMask;
+
+			//CTRL+C	
+			if ((args.Event.Key == Gdk.Key.Cyrillic_es || args.Event.Key == Gdk.Key.Cyrillic_ES) && args.Event.State.HasFlag(modifier)) {
+				Widget w = (o as Window).Focus;
+				CopyToClipboard (w);
+			}//CTRL+X
+			else if ((args.Event.Key == Gdk.Key.Cyrillic_che || args.Event.Key == Gdk.Key.Cyrillic_CHE) && args.Event.State.HasFlag(modifier)) {
+				Widget w = (o as Window).Focus;
+				CutToClipboard (w);
+			}//CTRL+V
+			else if ((args.Event.Key == Gdk.Key.Cyrillic_em || args.Event.Key == Gdk.Key.Cyrillic_EM) && args.Event.State.HasFlag(modifier)) {
+				Widget w = (o as Window).Focus;
+				PasteFromClipboard (w);
+			}
+		}
+
+		static void CopyToClipboard (Widget w)
+		{
+			int start, end;
+
+			if (w is Editable)
+				(w as Editable).CopyClipboard ();
+			else if (w is TextView)
+				(w as TextView).Buffer.CopyClipboard (clipboard);
+			else if (w is Label) {
+				(w as Label).GetSelectionBounds (out start, out end);
+				if (start != end)
+					clipboard.Text = (w as Label).Text.Substring (start, end - start);
+			}
+		}
+
+		static void CutToClipboard (Widget w)
+		{
+			int start, end;
+
+			if (w is Editable)
+				(w as Editable).CutClipboard ();
+			else if (w is TextView)
+				(w as TextView).Buffer.CutClipboard (clipboard, true);
+			else if (w is Label) {
+				(w as Label).GetSelectionBounds (out start, out end);
+				if (start != end)
+					clipboard.Text = (w as Label).Text.Substring (start, end - start);
+			}
+		}
+
+		static void PasteFromClipboard (Widget w)
+		{
+			if (w is Editable)
+				(w as Editable).PasteClipboard ();
+			else if (w is TextView)
+				(w as TextView).Buffer.PasteClipboard (clipboard);
+		}
+	}
+}
+
---
+++ QSTDI/TDIMain.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Gtk;
+
+namespace QSTDI
+{
+	public static class TDIMain
+	{
+		public static TdiNotebook MainNotebook;
+
+		public static void TDIHandleKeyReleaseEvent (object o, KeyReleaseEventArgs args)
+		{
+			if (MainNotebook == null)
+				return;
+
+			int platform = (int)Environment.OSVersion.Platform;
+			int version = (int)Environment.OSVersion.Version.Major;
+			Gdk.ModifierType modifier;
+
+			//Kind of MacOSX
+			if ((platform == 4 || platform == 6 || platform == 128) && version > 8)
+				modifier = Gdk.ModifierType.MetaMask | Gdk.ModifierType.Mod1Mask;
+			//Kind of Windows or Unix
+			else
+				modifier = Gdk.ModifierType.ControlMask;
+
+			//CTRL+S || CTRL+ENTER
+			if ((args.Event.Key == Gdk.Key.S
+				|| args.Event.Key == Gdk.Key.s
+				|| args.Event.Key == Gdk.Key.Cyrillic_yeru
+				|| args.Event.Key == Gdk.Key.Cyrillic_YERU
+				|| args.Event.Key == Gdk.Key.Return) && args.Event.State.HasFlag(modifier)) {
+				var w = MainNotebook.CurrentPageWidget;
+				if (w is TabVBox) {
+					var tab = (w as TabVBox).Tab;
+					if (tab is TdiSliderTab) {
+						var dialog = (tab as TdiSliderTab).ActiveDialog;
+						dialog.SaveAndClose ();
+					}
+					if(tab is ITdiDialog)
+					{
+						(tab as ITdiDialog).SaveAndClose();
+					}
+				}
+			}
+
+		}
+
+	}
+}
+
