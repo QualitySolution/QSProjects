abaad34de8d328e6a1d0ab816f19dfd388cb054b
Andrey Gankov <gav@qsolution.ru>
2017-01-10 18:36:13 +0300

    Превел оставшийся мапинг модуля на флуент.

---
+++ Modules/QSBanks/HMap/BankMap.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+
+namespace QSBanks.HMap
+{
+	public class BankMap : ClassMap<Bank>
+	{
+		public BankMap ()
+		{
+			Table("banks");
+
+			Not.LazyLoad();
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+
+			Map(x => x.Name).Column("name");
+			Map(x => x.Bik).Column("bik");
+			Map(x => x.CorAccount).Column("cor_account");
+			Map(x => x.City).Column("city");
+			Map(x => x.Deleted).Column("deleted");
+
+			References(x => x.Region).Column("region_id");
+		}
+	}
+}
+
---
+++ Modules/QSBanks/HMap/BankRegionMap.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FluentNHibernate.Mapping;
+
+namespace QSBanks.HMap
+{
+	public class BankRegionMap : ClassMap<BankRegion>
+	{
+		public BankRegionMap ()
+		{
+			Table("bank_regions");
+
+			Not.LazyLoad();
+
+			Id(x => x.Id).Column("id").GeneratedBy.Native();
+
+			Map(x => x.Region).Column("region");
+			Map(x => x.RegionNum).Column("region_num");
+			Map(x => x.City).Column("city");
+		}
+	}
+}
+
