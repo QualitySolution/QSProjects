ede5e1cc912b28936fd33ad195de435447c19b1e
Andrey Gankov <gav@qsolution.ru>
2017-01-12 10:38:12 +0300

    Обновил модель должности.

--- Modules/QSContacts/Domain/Post.cs
+++ Modules/QSContacts/Domain/Post.cs
@@ -1,14 +1,25 @@
 ﻿using System;
 using QSOrmProject;
+using System.ComponentModel.DataAnnotations;
 
 namespace QSContacts
 {
-	[OrmSubject("Должности")]
-	public class Post : IDomainObject
+	[OrmSubject (Gender = QSProjectsLib.GrammaticalGender.Feminine,
+		NominativePlural = "должности",
+		Nominative = "должность")]
+	public class Post : PropertyChangedBase, IDomainObject
 	{
 		#region Свойства
 		public virtual int Id { get; set; }
-		public virtual string Name { get; set; }
+
+		private string name;
+
+		[Display (Name = "Название")]
+		public virtual string Name {
+		    get { return name; }
+		    set { SetField (ref name, value, () => Name); }
+		}
+
 		#endregion
 		public Post()
 		{
