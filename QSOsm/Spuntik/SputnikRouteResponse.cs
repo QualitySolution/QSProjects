1efd3287a783bd1fa9d186f4ec50f7b3ab204c7c
Andrey Gankov <gav@qsolution.ru>
2017-03-17 12:40:49 +0300

    Добавлены классы для получения результатов расстояний из спутника.

---
+++ QSOsm/Spuntik/SputnikRouteResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using RestSharp.Deserializers;
+
+namespace QSOsm.Spuntik
+{
+	public class SputnikRouteResponse
+	{
+		[DeserializeAs(Name = "route_summary")]
+		public RouteSummary RouteSummary { get; set; }
+
+		[DeserializeAs(Name = "status_message")]
+		public string StatusMessage { get; set; }
+
+		[DeserializeAs(Name = "status")]
+		public int Status { get; set; }
+
+	}
+}
