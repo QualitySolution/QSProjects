b884efca3be389c81600a24f8a07377e0041c0b3
Andrey Gankov <gav@qsolution.ru>
2017-04-10 13:40:38 +0300

    Добавил файл для дебага, чтобы собиралось на других компьютерах. В последстии может не понадобится.

---
+++ QSOrmProject/DebugHelper/DebugLoadListener.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using NHibernate.Event;
+
+namespace QSOrmProject
+{
+	public class DebugLoadListener : ILoadEventListener 
+	{
+		private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+		public void OnLoad(LoadEvent theEvent, LoadType loadType)
+		{
+			logger.Debug("Load Class {0}", theEvent.EntityClassName);
+		}
+	}
+}
+
