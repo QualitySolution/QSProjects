7463c1247deddb0a2d974f9b3408103acf151dd9
Andrey Gankov <gav@qsolution.ru>
2017-10-13 14:26:19 +0300

    Добавлен класс с пользователем который присутстует во всех проектах.

---
+++ QSOrmProject/Domain/User.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace QSOrmProject.Domain
+{
+	[OrmSubject(Gender = QSProjectsLib.GrammaticalGender.Masculine,
+	NominativePlural = "пользователи",
+	Nominative = "пользователь")]
+	public class User : PropertyChangedBase, IDomainObject
+	{
+		#region Свойства
+
+		public virtual int Id { get; set; }
+
+		string name;
+
+		[Display(Name = "Имя пользователя")]
+		[Required(ErrorMessage = "Имя пользователя должно быть заполнено.")]
+		public virtual string Name {
+			get { return name; }
+			set { SetField(ref name, value, () => Name); }
+		}
+
+		string login;
+
+		[Display(Name = "Логин")]
+		[Required(ErrorMessage = "Логин пользователя должен быть заполнен.")]
+		public virtual string Login {
+			get { return login; }
+			set { SetField(ref login, value, () => Login); }
+		}
+
+		#endregion
+
+		public User()
+		{
+		}
+	}
+}
