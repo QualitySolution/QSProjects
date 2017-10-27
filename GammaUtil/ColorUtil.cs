e1b280a3511e80aa9f50a051a15b7b17f0822659
Andrey Gankov <gav@qsolution.ru>
2017-10-27 10:06:04 +0300

    Method for simple create Gtk.Color from text.

---
+++ GammaUtil/ColorUtil.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Gdk;
+
+namespace Gamma.Utilities
+{
+	public static class ColorUtil
+	{
+		public static Color Create(string text)
+		{
+			var color = new Color();
+			if (!Color.Parse(text, ref color))
+				throw new InvalidOperationException(String.Format("Can't parse color value \"{0}\".", text));
+			return color;
+		}
+	}
+}
