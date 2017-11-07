05032fa551f194cccee150afdc6caab41414623d
Andrey Gankov <gav@qsolution.ru>
2017-11-07 10:20:22 +0300

    Управление настройками пользователя.

---
+++ QSOrmProject/Users/UserSettingsManager.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+
+namespace QSOrmProject.Users
+{
+	public class UserSettingsManager<TUserSettings> where TUserSettings : IDomainObject
+	{
+		static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+		public Func<IUnitOfWork, TUserSettings> LoadUserSettings;
+		public Func<IUnitOfWork, TUserSettings> CreateUserSettings;
+
+		public UserSettingsManager()
+		{
+		}
+
+		IUnitOfWork uow;
+
+		IUnitOfWork UoW {
+			get {
+				if(uow == null || !uow.IsAlive)
+					uow = UnitOfWorkFactory.CreateWithoutRoot();
+				return uow;
+			}
+		}
+
+		TUserSettings settings;
+		public TUserSettings Settings {
+			get {
+				if(settings != null && UoW != null && UoW.IsAlive)
+					return settings;
+
+				ReloadSettings();
+
+				var map = OrmMain.GetObjectDescription<TUserSettings>();
+				map.ObjectUpdatedGeneric -= Map_ObjectUpdatedGeneric;
+				map.ObjectUpdatedGeneric += Map_ObjectUpdatedGeneric;
+
+				return settings;
+			}
+		}
+
+		private void ReloadSettings()
+		{
+			logger.Info("Обновляем настройки пользователя...");
+			settings = LoadUserSettings(UoW);
+
+			if(settings == null) {
+				logger.Info("Настроек пользователя нет, создаем новые.");
+				settings = CreateUserSettings(UoW);
+				SaveSettings();
+			}
+		}
+
+		void Map_ObjectUpdatedGeneric(object sender, QSOrmProject.UpdateNotification.OrmObjectUpdatedGenericEventArgs<TUserSettings> e)
+		{
+			if(e.UpdatedSubjects.Any(x => x.Id == Settings.Id))
+				UoW.Session.Refresh(Settings);
+		}
+
+		public void SaveSettings()
+		{
+			UoW.Save(settings);
+			UoW.Commit();
+		}
+
+	}
+}
