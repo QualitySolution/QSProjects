f9cf2f19d24d80952d2d139adc268b0dca138b37
Andrey Gankov <gav@qsolution.ru>
2018-02-02 18:27:34 +0300

    Добавлена возможность просто подсчитать ссылки. И исправлено поведение с коллекциями, такими ка цены в номенклатурах.

--- QSOrmProject/Deletion/ReplaceEntity.cs
+++ QSOrmProject/Deletion/ReplaceEntity.cs
@@ -7,6 +7,8 @@ namespace QSOrmProject.Deletion
 {
 	public static class ReplaceEntity
 	{
+		static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
 		public static int ReplaceEverywhere<TEntity>(IUnitOfWork uow, TEntity fromE, TEntity toE)
 			where TEntity : IDomainObject
 		{
@@ -28,6 +30,8 @@ namespace QSOrmProject.Deletion
 
 			foreach(var depend in delConfig.DeleteItems)
 			{
+				if(String.IsNullOrEmpty(depend.PropertyName))
+					continue;
 				var propInfo = depend.ObjectClass.GetProperty(depend.PropertyName);
 				foreach(var item in GetLinkedEntities(uow, depend, fromE))
 				{
@@ -59,6 +63,45 @@ namespace QSOrmProject.Deletion
 			}
 
 			return replacedLinks;
+		}
+
+		public static int CalculateTotalLinks<TEntity>(IUnitOfWork uow, TEntity fromE)
+	where TEntity : IDomainObject
+		{
+			logger.Info("Подсчет ссылок...");
+			int totalLinks = 0;
+
+			if(fromE == null)
+				throw new ArgumentNullException("fromE");
+			if(fromE.Id == 0)
+				throw new ArgumentException("Сущьность должна уже иметь ID", nameof(fromE));
+
+			var delConfig = DeleteConfig.GetDeleteInfo<TEntity>();
+			if(delConfig == null)
+				throw new InvalidOperationException($"Конфигурация удаления для типа {typeof(TEntity)} не найдена.");
+
+			if(!(delConfig is IDeleteInfoHibernate))
+				throw new NotSupportedException($"Поддерживаются только конфигурации удаляения Hibernate.");
+
+			foreach(var depend in delConfig.DeleteItems) {
+				if(String.IsNullOrEmpty(depend.PropertyName))
+					continue;
+				var propInfo = depend.ObjectClass.GetProperty(depend.PropertyName);
+				totalLinks += GetLinkedEntities(uow, depend, fromE).Count;
+			}
+
+			foreach(var depend in delConfig.ClearItems) {
+				var propInfo = depend.ObjectClass.GetProperty(depend.PropertyName);
+				totalLinks += GetLinkedEntities(uow, depend, fromE).Count;
+			}
+
+			foreach(var depend in delConfig.RemoveFromItems) {
+				var collPropInfo = depend.ObjectClass.GetProperty(depend.CollectionName);
+				totalLinks += GetLinkedEntities(uow, depend, fromE).Count;
+			}
+
+			logger.Info("Найдено {0} ссылок.", totalLinks);
+			return totalLinks;
 		}
 
 		private static IList GetLinkedEntities(IUnitOfWork uow, DeleteDependenceInfo depend, IDomainObject masterEntity)
