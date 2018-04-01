04e47b6967ca33b9ca180d199d5d0e7b3b052cd4
Andrey Gankov <gav@qsolution.ru>
2018-04-01 18:39:42 +0300

    Added Gamma FileChooserButton widget.

---
+++ GammaBinding/GtkWidgets/yFileChooserButton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Gtk;
+using Gamma.Binding.Core;
+using System.Linq.Expressions;
+
+namespace Gamma.GtkWidgets
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	[System.ComponentModel.Category ("Gamma Gtk")]
+	public class yFileChooserButton : FileChooserButton
+	{
+		public BindingControler<yFileChooserButton> Binding { get; private set;}
+
+		public yFileChooserButton() : base((Widget)null) {
+			Binding = new BindingControler<yFileChooserButton>(this, new Expression<Func<yFileChooserButton, object>>[] {
+				w => w.Filename,
+				w => w.Filenames
+			});
+		}
+
+        protected override void OnFileSet()
+        {
+			Binding.FireChange(w => w.Filename, w => w.Filenames);
+			base.OnFileSet();
+        }
+    }
+}
+
