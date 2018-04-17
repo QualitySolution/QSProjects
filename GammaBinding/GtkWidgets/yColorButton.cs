995e03928c0aada8e538388b178f6868812cabaf
ruSaendra <saendra.ru@gmail.com>
2018-04-17 12:13:42 +0300

    Добавлена кнопка выбора цвета

---
+++ GammaBinding/GtkWidgets/yColorButton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Linq.Expressions;
+using Gamma.Binding.Core;
+using Gtk;
+
+namespace Gamma.GtkWidgets
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	[System.ComponentModel.Category("Gamma Gtk")]
+	public class yColorButton : ColorButton
+	{
+		public BindingControler<yColorButton> Binding { get; private set; }
+
+		public yColorButton()
+		{
+			Binding = new BindingControler<yColorButton>(this, new Expression<Func<yColorButton, object>>[] {
+				(w => w.Color)
+			});
+		}
+
+		public yColorButton(object tag) : this()
+		{
+			Tag = tag;
+		}
+
+		/// <summary>
+		/// For store user data, like WinForms Tag.
+		/// </summary>
+		public object Tag;
+
+		protected override void OnColorSet()
+		{
+			Binding.FireChange(w => w.Color);
+			base.OnColorSet();
+		}
+	}
+}
