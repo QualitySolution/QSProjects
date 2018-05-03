e12080e122c0a3e516069c872a1590d4ce6934ab
Andrey Gankov <gav@qsolution.ru>
2018-05-03 04:56:55 +0300

    Добавлен атрибут для удобного связывания перечислений с идентификаторами печатных форм.

---
+++ QSReport/ReportIdentifierAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace QSReport
+{
+	[AttributeUsage(AttributeTargets.Field)]
+	public class ReportIdentifierAttribute : Attribute
+	{
+		public string Identifier { get; set; }
+
+		public ReportIdentifierAttribute(string identifier)
+		{
+			Identifier = identifier;
+		}
+	}
+}
