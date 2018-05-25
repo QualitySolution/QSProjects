44d59a09ca099236712d59e0cb74e594ef209b65
opopve <opopve@gmail.com>
2018-05-25 09:21:22 +0300

    I-1026 Добавлено булево св-во CanRevertCommon для отключения/включения кнопки buttonRevertCommon. По умолчанию кнопка включена.

--- Modules/QSDocTemplates/TemplateWidget.cs
+++ Modules/QSDocTemplates/TemplateWidget.cs
@@ -9,34 +9,38 @@ namespace QSDocTemplates
 	[System.ComponentModel.ToolboxItem(true)]
 	public partial class TemplateWidget : Gtk.Bin
 	{
-		public BindingControler<TemplateWidget> Binding { get; private set;}
+		public BindingControler<TemplateWidget> Binding { get; private set; }
 
 		private FileWorker worker = new FileWorker();
 
-		public bool FileChanged { get; private set;}
+		public bool FileChanged { get; private set; }
 
 		public event EventHandler BeforeOpen;
 
 		public bool CanOpenDocument = true;
 
+		private bool canRevertCommon = true;
+
+		public bool CanRevertCommon { 
+			get => canRevertCommon; 
+			set => canRevertCommon = value; 
+		}
+
+
 		IDocTemplate template;
-		public IDocTemplate Template
-		{
-			get
-			{
+		public IDocTemplate Template {
+			get {
 				return template;
 			}
-			set
-			{
-				if (template != null)
+			set {
+				if(template != null)
 					template.PropertyChanged -= Template_PropertyChanged;
 
 				template = value;
-				if(template != null)
-				{
+				if(template != null) {
 					template.PropertyChanged += Template_PropertyChanged;
 				}
-				if (comboTemplates.SelectedItem != template)
+				if(comboTemplates.SelectedItem != template)
 					UpdateTemplatesCombo();
 				UpdateState();
 				UpdateSize();
@@ -46,29 +50,26 @@ namespace QSDocTemplates
 
 		IList<IDocTemplate> availableTemplates;
 
-		public IList<IDocTemplate> AvailableTemplates{
-			get{
+		public IList<IDocTemplate> AvailableTemplates {
+			get {
 				return availableTemplates;
 			}
-			set{
+			set {
 				availableTemplates = value;
 				UpdateTemplatesCombo();
 			}
 		}
 
-		void Template_PropertyChanged (object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		void Template_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
 		{
 		}
-			
-		public byte[] ChangedDoc
-		{
-			get
-			{
+
+		public byte[] ChangedDoc {
+			get {
 				return Template?.ChangedDocFile;
 			}
-			set
-			{
-				if (Template == null)
+			set {
+				if(Template == null)
 					return;
 				Template.ChangedDocFile = value;
 				UpdateSize();
@@ -80,7 +81,7 @@ namespace QSDocTemplates
 		{
 			this.Build();
 
-			Binding = new BindingControler<TemplateWidget> (this, new Expression<Func<TemplateWidget, object>>[] {
+			Binding = new BindingControler<TemplateWidget>(this, new Expression<Func<TemplateWidget, object>>[] {
 				(w => w.Template),
 				(w => w.ChangedDoc),
 			});
@@ -94,7 +95,7 @@ namespace QSDocTemplates
 			CanOpenDocument = true;
 		}
 
-		void Worker_FileUpdated (object sender, FileUpdatedEventArgs e)
+		void Worker_FileUpdated(object sender, FileUpdatedEventArgs e)
 		{
 			Binding.FireChange(x => x.ChangedDoc);
 			FileChanged = true;
@@ -113,29 +114,22 @@ namespace QSDocTemplates
 
 		void UpdateState()
 		{
-			if (Template == null)
-			{
+			if(Template == null) {
 				labelStatus.Markup = "<span foreground=\"red\">Шаблон не определен!</span>";
 				buttonEdit.Sensitive = buttonPrint.Sensitive =
 					buttonRevertCommon.Sensitive = buttonOpen.Sensitive = false;
-			}
-			else if (Template.DocParser == null)
-			{
+			} else if(Template.DocParser == null) {
 				labelStatus.Markup = "<span foreground=\"red\">Парсер не задан!</span>";
 				buttonEdit.Sensitive = buttonPrint.Sensitive =
 					buttonRevertCommon.Sensitive = buttonOpen.Sensitive = false;
-			}
-			else if(ChangedDoc != null)
-			{
+			} else if(ChangedDoc != null) {
 				if(FileChanged)
 					labelStatus.Markup = "<span foreground=\"blue\">Собственный документ</span> <span foreground=\"green\">(изменён)</span>";
 				else
 					labelStatus.Markup = "<span foreground=\"blue\">Собственный документ</span>";
-				buttonEdit.Sensitive = buttonPrint.Sensitive =
-					buttonRevertCommon.Sensitive = buttonOpen.Sensitive = true;
-			}
-			else
-			{
+				buttonEdit.Sensitive = buttonPrint.Sensitive = buttonOpen.Sensitive = true;
+				buttonRevertCommon.Sensitive = CanRevertCommon;
+			} else {
 				labelStatus.Markup = "<span foreground=\"green\">Общий шаблон</span>";
 				buttonEdit.Sensitive = buttonPrint.Sensitive = buttonOpen.Sensitive = true;
 				buttonRevertCommon.Sensitive = false;
@@ -145,12 +139,9 @@ namespace QSDocTemplates
 		void UpdateSize()
 		{
 			ylabelSize.LabelProp = String.Empty;
-			if(ChangedDoc != null)
-			{
+			if(ChangedDoc != null) {
 				ylabelSize.LabelProp = StringWorks.BytesToIECUnitsString((uint)ChangedDoc.LongLength);
-			}
-			else if(Template != null)
-			{
+			} else if(Template != null) {
 				ylabelSize.LabelProp = StringWorks.BytesToIECUnitsString((uint)Template.File.LongLength);
 			}
 		}
@@ -178,7 +169,7 @@ namespace QSDocTemplates
 			ChangedDoc = null;
 		}
 
-		protected void OnButtonOpenClicked (object sender, EventArgs e)
+		protected void OnButtonOpenClicked(object sender, EventArgs e)
 		{
 			BeforeOpen?.Invoke(this, EventArgs.Empty);
 			if(CanOpenDocument) {
@@ -189,7 +180,7 @@ namespace QSDocTemplates
 
 		protected void OnComboTemplatesChanged(object sender, EventArgs e)
 		{
-			if (Template != comboTemplates.SelectedItem)
+			if(Template != comboTemplates.SelectedItem)
 				Template = (IDocTemplate)comboTemplates.SelectedItem;
 		}
 	}
