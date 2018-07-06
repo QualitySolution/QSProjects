466ed64dafd9d7ec9e1540a4a3fe65553a9edf40
Andrey Gankov <gav@qsolution.ru>
2018-07-06 14:54:31 +0300

    Базовый класс для многоуровневого справочника.

---
+++ QSOrmProject/DomainModel/Basis/DomainTreeNodeBase.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using QSOrmProject;
+
+namespace QS.DomainModel.Basis
+{
+	public abstract class DomainTreeNodeBase<TEntity> : PropertyChangedBase, IDomainObject 
+		where TEntity : DomainTreeNodeBase<TEntity>
+	{
+		protected static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+		#region Свойства
+
+		public virtual int Id { get; set; }
+
+		TEntity parent;
+
+		[Display(Name = "Родитель")]
+		public virtual TEntity Parent {
+			get { return parent; }
+			set {
+				if(NHibernate.NHibernateUtil.IsInitialized(Parent)) {
+					if(parent != null)
+						parent.Childs.Remove((TEntity)this);
+					if(value != null && value.CheckCircle((TEntity)this)) {
+						logger.Warn("Родитель не назначен, так как возникает зацикливание дерева.");
+						return;
+					}
+				}
+
+				SetField(ref parent, value, () => Parent);
+
+				if(parent != null && NHibernate.NHibernateUtil.IsInitialized(Parent))
+					parent.Childs.Add((TEntity)this);
+			}
+		}
+
+		IList<TEntity> childs = new List<TEntity>();
+
+		[Display(Name = "Дочерние элементы")]
+		public virtual IList<TEntity> Childs {
+			get { return childs; }
+			set { SetField(ref childs, value, () => Childs); }
+		}
+
+		#endregion
+
+		#region Внутренние
+
+		private bool CheckCircle(TEntity node)
+		{
+			if(Parent == null)
+				return false;
+
+			if(Parent == node)
+				return true;
+
+			return Parent.CheckCircle(node);
+		}
+
+		#endregion
+	}
+}
