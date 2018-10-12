17ef2863d4b242899b3fb5fd41108753974d65b1
Vadim Gordeev <enzogord@gmail.com>
2018-10-12 10:40:20 +0300

    Добавлены новые типы помещений

--- QSOsm/DTO/OsmEnums.cs
+++ QSOsm/DTO/OsmEnums.cs
@@ -31,8 +31,14 @@ namespace QSOsm.DTO
 		Apartment,
 		[Display (Name = "Офис", ShortName = "оф.")]
 		Office,
-		[Display (Name = "Помещение", ShortName = "пом.")]
-		Room
+		[Display(Name = "Склад", ShortName = "склад")]
+		Store,
+		[Display(Name = "Помещение", ShortName = "пом.")]
+		Room,
+		[Display(Name = "Комната", ShortName = "ком.")]
+		Chamber,
+		[Display(Name = "Секция", ShortName = "сек.")]
+		Section
 	}
 
 	public class RoomTypeStringType : NHibernate.Type.EnumStringType
