dca332262b34e974891ec199b4b4486c85da2242
Andrey Gankov <gav@qsolution.ru>
2018-11-07 19:09:17 +0300

    Добавлены еще 2 базовых класса для быстрого создания диалогов. Они реализуют необходимые интерфейсы для записи в лог красивого названия действия.

---
+++ QS.Project.Gtk/Dialog.Gtk/SingleUowDialogBase.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using QS.DomainModel.UoW;
+using QS.Tdi;
+
+namespace QS.Dialog.Gtk
+{
+	public abstract class SingleUowDialogBase : TdiTabBase, ITdiDialog, ISingleUoWDialog
+	{
+		public SingleUowDialogBase()
+		{
+		}
+
+		public virtual IUnitOfWork UoW { get; protected set; }
+
+		private bool manualChange = false;
+
+		public virtual bool HasChanges {
+			get { return manualChange || UoW.HasChanges; }
+			set { manualChange = value; }
+		}
+
+		public event EventHandler<EntitySavedEventArgs> EntitySaved;
+
+		public abstract bool Save();
+
+		public void SaveAndClose()
+		{
+			Save();
+			OnCloseTab(false);
+		}
+	}
+}
---
+++ QS.Project.Gtk/Dialog.Gtk/SingleUowTabBase.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using QS.DomainModel.UoW;
+
+namespace QS.Dialog.Gtk
+{
+	public abstract class SingleUowTabBase : TdiTabBase, ISingleUoWDialog
+	{
+		public SingleUowTabBase()
+		{
+		}
+
+		public virtual IUnitOfWork UoW { get; protected set; }
+	}
+}
