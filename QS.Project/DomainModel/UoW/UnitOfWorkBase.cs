712541ab9dc974894e856359f31c3bc0275f83d0
opopve <opopve@gmail.com>
2018-11-08 12:10:38 +0300

    Revert "Открываем транзакцию при создании сессии."
    This reverts commit 396ec2a4951f51c99c841c064ed84c8e6c2509ea.

--- QS.Project/DomainModel/UoW/UnitOfWorkBase.cs
+++ QS.Project/DomainModel/UoW/UnitOfWorkBase.cs
@@ -42,8 +42,6 @@ namespace QS.DomainModel.UoW
 			get {
 				if(session == null) {
 					session = OpenSession(null);
-					if(transaction == null)
-						transaction = Session.BeginTransaction();
 					NhEventListener.RegisterUow(this);
 					HibernateTracker = TrackerMain.Factory?.CreateHibernateTracker();
 				}
