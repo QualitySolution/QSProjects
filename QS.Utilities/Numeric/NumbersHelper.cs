9d0a66fecf390a22ef43e463528458848da4ff5c
Andrey Gankov <gav@qsolution.ru>
2018-11-30 19:51:26 +0300

    Добавлен метод позволяющий очень просто загнать число в нужный диапазон значений.

---
+++ QS.Utilities/Numeric/NumbersHelper.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace QS.Utilities.Numeric
+{
+	public static class NumbersHelper
+	{
+		public static int Clamp(this int value, int min, int max)
+		{
+			if(value < min) { return min; }
+			if(value > max) { return max; }
+			return value;
+		}
+	}
+}
