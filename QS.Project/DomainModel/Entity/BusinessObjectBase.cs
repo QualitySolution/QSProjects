2895c1ce42fb86b4e91a54895f3978440fe38430
Andrey Gankov <gav@qsolution.ru>
2019-01-10 17:47:02 +0300

    Удалил реально нигде не используемое свойство, оно нигде не заполняется. Это была тупковая идея.

--- QS.Project/DomainModel/Entity/BusinessObjectBase.cs
+++ QS.Project/DomainModel/Entity/BusinessObjectBase.cs
@@ -6,7 +6,6 @@ namespace QS.DomainModel.Entity
 	public abstract class BusinessObjectBase<TEntity> : PropertyChangedBase, IBusinessObject 
 		where TEntity : IDomainObject, new()
 	{
-		public virtual IUnitOfWorkGeneric<TEntity> UoWGeneric { set; get;}
 		public virtual IUnitOfWork UoW { set; get;}
 	}
 }
