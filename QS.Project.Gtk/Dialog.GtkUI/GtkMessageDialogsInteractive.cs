a10dd6ed8464057ce481d88b79f17de153044fe9
Andrey Gankov <gav@qsolution.ru>
2019-01-31 18:33:40 +0300

    Добавил начальную реализацию механизма вызова интерактивных диалогов или других интерактивных действий из слоя бизнес логики. Используя этот интерфейс можно в методы бизнес логики требующие взаимодействия с пользователем, передавать специальный класс, в базовой реалзиции которого в гтк будет выводится сообщения пользователю, но этот позволить реализовывать класс иначе. И каждый диалог во первых по своему может обрабатывать сообщения, во вторых это позволит тестировтаь бизнес логику. И в третьих позволит перетащить код бизнес логики из диалогов в бизнес слой.

---
+++ QS.Project.Gtk/Dialog.GtkUI/GtkMessageDialogsInteractive.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace QS.Dialog.GtkUI
+{
+	public class GtkMessageDialogsInteractive : IInteractiveMessage
+	{
+		public void ShowMessage(ImportanceLevel level, string message)
+		{
+			switch(level) {
+				case ImportanceLevel.Error:
+					MessageDialogHelper.RunErrorDialog(message);
+					break;
+				case ImportanceLevel.Warning:
+					MessageDialogHelper.RunWarningDialog(message);
+					break;
+				case ImportanceLevel.Info:
+				default:
+					MessageDialogHelper.RunInfoDialog(message);
+					break;
+			}
+		}
+	}
+}
---
+++ QS.Project/Dialog/IInteractiveMessage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace QS.Dialog
+{
+	public interface IInteractiveMessage
+	{
+		void ShowMessage(ImportanceLevel level, string message);
+	}
+
+	public enum ImportanceLevel
+	{
+		Info,
+		Warning,
+		Error
+	}
+}
