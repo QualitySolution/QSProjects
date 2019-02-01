c8914d95ec7b6d29a289d057d5d3fa15e2ba6936
Vadim Gordeev <enzogord@gmail.com>
2019-02-01 11:11:29 +0300

    Добавлены виджеты работы с правами

---
+++ QS.Project.Gtk/Widgets.GtkUI/UserPermissionWidget.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Gtk;
+using QS.DomainModel.UoW;
+using QS.Project.Repositories;
+
+namespace QS.Widgets.Gtk
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	public partial class UserPermissionWidget : Bin
+	{
+		IUnitOfWork UoW;
+
+		public UserPermissionWidget()
+		{
+			this.Build();
+			UoW = UnitOfWorkFactory.CreateWithoutRoot();
+		}
+
+		public void ConfigureDlg()
+		{
+			userEntityPermissionsView.ConfigureDlg(UoW, UserRepository.GetCurrentUserId());
+		}
+
+		public void Save()
+		{
+			userEntityPermissionsView.Save();
+			UoW.Commit();
+		}
+
+	}
+}
--- QS.Project/Static/PermissionSettings.cs
+++ QS.Project/Static/PermissionSettings.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Reflection;
 using QS.DomainModel.Entity;
 using QS.DomainModel.Entity.EntityPermissions;
@@ -23,6 +24,31 @@ namespace QS.Static
 				message = $"У Вас нет прав для просмотра следующих типов документов: {aa.Nominative}";
 			}
 			return message;
+		}
+
+
+		/// <summary>
+		/// Однократное нахождение всех типов помеченных аттрибутом <see cref="EntityPermissionAttribute"/>
+		/// </summary>
+		/// <param name="entitiesFinder">объект который ищет необходиые типы с настройками под конкретный проект</param>
+		public static void ConfigureEntityPermissionFinder(IEntitiesWithPermissionFinder entitiesFinder)
+		{
+			permissionsEntityTypes = entitiesFinder.FindTypes();
+		}
+
+		private static IEnumerable<Type> permissionsEntityTypes;
+
+		/// <summary>
+		/// Список типов помеченных аттрибутом <see cref="EntityPermissionAttribute"/>
+		/// </summary>
+		/// <value>The permissions entity types.</value>
+		public static IEnumerable<Type> PermissionsEntityTypes {
+			get{
+				if(permissionsEntityTypes == null) {
+					return new List<Type>();
+				}
+				return permissionsEntityTypes;
+			}
 		}
 	}
 }
