ec721dffb2c6ea40c9b95185e7b6ca27b6d4d48f
Vadim Gordeev <enzogord@gmail.com>
2019-02-04 15:18:25 +0300

    Проверка может ли администратор иметь полный доступ к сущностям

--- QS.Project/DomainModel/Entity/EntityPermissions/EntityPermissionValidator.cs
+++ QS.Project/DomainModel/Entity/EntityPermissions/EntityPermissionValidator.cs
@@ -10,11 +10,21 @@ namespace QS.DomainModel.Entity.EntityPermissions
 {
 	public class EntityPermissionValidator : IEntityPermissionValidator
 	{
+		public virtual bool NoRestrictPermissionsForAdmin { get; set; } = true;
+
 		public virtual EntityPermission Validate(Type entityType, int userId)
 		{
 			var permissionAttr = entityType.GetCustomAttribute<EntityPermissionAttribute>();
 			if(permissionAttr == null) {
 				return new EntityPermission(false, false, false, false);
+			}
+			UserBase user;
+			using(var uow = UnitOfWorkFactory.CreateWithoutRoot()) {
+				user = UserRepository.GetUserById(uow, userId);
+			}
+
+			if(user != null && user.IsAdmin && NoRestrictPermissionsForAdmin) {
+				return new EntityPermission(true, true, true, true);
 			}
 
 			var userPermission = UserPermissionRepository.GetUserEntityPermission(UnitOfWorkFactory.CreateWithoutRoot(), entityType.Name, userId);
