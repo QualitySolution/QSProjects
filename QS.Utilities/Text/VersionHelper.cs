ccec5b72630de50012fcd8f9d942918a8512c91d
Andrey Gankov <gav@qsolution.ru>
2019-02-18 20:07:28 +0300

    В новые библиотеки перенесены методы работы сформированием версии строки.

---
+++ QS.Utilities/Text/VersionHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+namespace QS.Utilities.Text
+{
+	public static class VersionHelper
+	{
+		public static string VersionToShortString(this Version version)
+		{
+			return version.ToString(version.Revision <= 0 ? (version.Build <= 0 ? 2 : 3) : 4);
+		}
+
+		public static string VersionToShortString(this string version)
+		{
+			var ver = Version.Parse(version);
+			return ver.ToString(ver.Revision == 0 ? (ver.Build == 0 ? 2 : 3) : 4);
+		}
+	}
+}
