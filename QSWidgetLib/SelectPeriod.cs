d96536a8cfc12b7621d4ca5af302dc017d9fbe92
opopve <opopve@gmail.com>
2019-03-11 12:49:47 +0300

    В виджет выбора периода добавлено свойство WithTime, позволяющее выбирать ещё и время в диапазоне дат.

--- QSWidgetLib/SelectPeriod.cs
+++ QSWidgetLib/SelectPeriod.cs
@@ -4,7 +4,7 @@ using Gtk;
 
 namespace QSWidgetLib
 {
-	public enum RadioModeType{SingleColumn, DoubleColumn};
+	public enum RadioModeType { SingleColumn, DoubleColumn };
 
 	[System.ComponentModel.ToolboxItem(true)]
 	public partial class SelectPeriod : Gtk.Bin
@@ -15,319 +15,340 @@ namespace QSWidgetLib
 		private bool IsRadioChange;
 		public event EventHandler DatesChanged;
 
-		public enum Period {None, Today, Week, Month, ThreeMonth, SixMonth, Year, AllTime, CurWeek, CurMonth, CurQuarter, CurYear};
+		public enum Period { None, Today, Week, Month, ThreeMonth, SixMonth, Year, AllTime, CurWeek, CurMonth, CurQuarter, CurYear };
 
 		public RadioModeType RadioMode {
-			get{return _RadioMode;}
-			set{_RadioMode = value;
-				OnRepackRadios ();}
+			get => _RadioMode;
+			set {
+				_RadioMode = value;
+				OnRepackRadios();
+			}
 		}
 
 		public DateTime DateBegin {
-			get{return StartDate.Date;}
-			set{StartDate.Date = value;}
+			get => StartDate.Date;
+			set => StartDate.Date = value;
 		}
 
 		public DateTime DateEnd {
-			get{return EndDate.Date;}
-			set{EndDate.Date = value;}
+			get => EndDate.Date;
+			set => EndDate.Date = value;
 		}
 
 		public bool AutoDateSeparation {
-			get{return StartDate.AutoSeparation;}
-			set{EndDate.AutoSeparation = value;
-				StartDate.AutoSeparation = value;}
+			get => StartDate.AutoSeparation;
+			set {
+				EndDate.AutoSeparation = value;
+				StartDate.AutoSeparation = value;
+			}
 		}
 
-		public bool IsAllTime {
-			get{return StartDate.IsEmpty && EndDate.IsEmpty;}
-		}
+		public bool IsAllTime => StartDate.IsEmpty && EndDate.IsEmpty;
 
 		public bool ShowToday {
-			get{return ShowRadio[Period.Today];}
-			set{ShowRadio[Period.Today] = value;
-				OnRepackRadios();}
+			get => ShowRadio[Period.Today];
+			set {
+				ShowRadio[Period.Today] = value;
+				OnRepackRadios();
+			}
 		}
 
 		public bool ShowWeek {
-			get{return ShowRadio[Period.Week];}
-			set{ShowRadio[Period.Week] = value;
-				OnRepackRadios();}
+			get => ShowRadio[Period.Week];
+			set {
+				ShowRadio[Period.Week] = value;
+				OnRepackRadios();
+			}
 		}
 
 		public bool ShowMonth {
-			get{return ShowRadio[Period.Month];}
-			set{ShowRadio[Period.Month] = value;
-				OnRepackRadios();}
+			get => ShowRadio[Period.Month];
+			set {
+				ShowRadio[Period.Month] = value;
+				OnRepackRadios();
+			}
 		}
 
 		public bool Show3Month {
-			get{return ShowRadio[Period.ThreeMonth];}
-			set{ShowRadio[Period.ThreeMonth] = value;
-				OnRepackRadios();}
+			get => ShowRadio[Period.ThreeMonth];
+			set {
+				ShowRadio[Period.ThreeMonth] = value;
+				OnRepackRadios();
+			}
 		}
 
 		public bool Show6Month {
-			get{return ShowRadio[Period.SixMonth];}
-			set{ShowRadio[Period.SixMonth] = value;
-				OnRepackRadios();}
+			get => ShowRadio[Period.SixMonth];
+			set {
+				ShowRadio[Period.SixMonth] = value;
+				OnRepackRadios();
+			}
 		}
 
 		public bool ShowYear {
-			get{return ShowRadio[Period.Year];}
-			set{ShowRadio[Period.Year] = value;
-				OnRepackRadios();}
+			get => ShowRadio[Period.Year];
+			set {
+				ShowRadio[Period.Year] = value;
+				OnRepackRadios();
+			}
 		}
 
 		public bool ShowAllTime {
-			get{return ShowRadio[Period.AllTime];}
-			set{ShowRadio[Period.AllTime] = value;
-				OnRepackRadios();}
+			get => ShowRadio[Period.AllTime];
+			set {
+				ShowRadio[Period.AllTime] = value;
+				OnRepackRadios();
+			}
 		}
 
 		public bool ShowCurWeek {
-			get{return ShowRadio[Period.CurWeek];}
-			set{ShowRadio[Period.CurWeek] = value;
-				OnRepackRadios();}
+			get => ShowRadio[Period.CurWeek];
+			set {
+				ShowRadio[Period.CurWeek] = value;
+				OnRepackRadios();
+			}
 		}
 
 		public bool ShowCurMonth {
-			get{return ShowRadio[Period.CurMonth];}
-			set{ShowRadio[Period.CurMonth] = value;
-				OnRepackRadios();}
+			get => ShowRadio[Period.CurMonth];
+			set {
+				ShowRadio[Period.CurMonth] = value;
+				OnRepackRadios();
+			}
 		}
 
 		public bool ShowCurQuarter {
-			get{return ShowRadio[Period.CurQuarter];}
-			set{ShowRadio[Period.CurQuarter] = value;
-				OnRepackRadios();}
+			get => ShowRadio[Period.CurQuarter];
+			set {
+				ShowRadio[Period.CurQuarter] = value;
+				OnRepackRadios();
+			}
 		}
 
 		public bool ShowCurYear {
-			get{return ShowRadio[Period.CurYear];}
-			set{ShowRadio[Period.CurYear] = value;
-				OnRepackRadios();}
+			get => ShowRadio[Period.CurYear];
+			set {
+				ShowRadio[Period.CurYear] = value;
+				OnRepackRadios();
+			}
 		}
 
-		public SelectPeriod ()
-		{
-			this.Build ();
+		public bool WithTime {
+			get => EndDate.WithTime || StartDate.WithTime;
+			set => EndDate.WithTime = StartDate.WithTime = value;
+		}
 
+		public SelectPeriod()
+		{
+			this.Build();
+			EndDate.WithTime = StartDate.WithTime = WithTime;
 			//Создаем переключатели
 			RadioButtons = new Dictionary<Period, RadioButton>();
-			RadioButtons.Add (Period.Today, new RadioButton("Сегодня"));
+			RadioButtons.Add(Period.Today, new RadioButton("Сегодня"));
 			RadioButtons[Period.Today].Clicked += OnRadioTodayClicked;
 			RadioButton RadioToday = RadioButtons[Period.Today];
-			RadioButtons.Add (Period.Week, new RadioButton(RadioToday, "За неделю"));
+			RadioButtons.Add(Period.Week, new RadioButton(RadioToday, "За неделю"));
 			RadioButtons[Period.Week].Clicked += OnRadioWeekClicked;
-			RadioButtons.Add (Period.Month, new RadioButton(RadioToday, "За месяц"));
+			RadioButtons.Add(Period.Month, new RadioButton(RadioToday, "За месяц"));
 			RadioButtons[Period.Month].Clicked += OnRadioMonthClicked;
-			RadioButtons.Add (Period.ThreeMonth, new RadioButton(RadioToday, "За 3 месяца"));
+			RadioButtons.Add(Period.ThreeMonth, new RadioButton(RadioToday, "За 3 месяца"));
 			RadioButtons[Period.ThreeMonth].Clicked += OnRadio3monthClicked;
-			RadioButtons.Add (Period.SixMonth, new RadioButton(RadioToday, "За полгода"));
+			RadioButtons.Add(Period.SixMonth, new RadioButton(RadioToday, "За полгода"));
 			RadioButtons[Period.SixMonth].Clicked += OnRadio6monthClicked;
-			RadioButtons.Add (Period.Year, new RadioButton(RadioToday, "За год"));
+			RadioButtons.Add(Period.Year, new RadioButton(RadioToday, "За год"));
 			RadioButtons[Period.Year].Clicked += OnRadioYearClicked;
-			RadioButtons.Add (Period.AllTime, new RadioButton(RadioToday, "Все время"));
+			RadioButtons.Add(Period.AllTime, new RadioButton(RadioToday, "Все время"));
 			RadioButtons[Period.AllTime].Clicked += OnRadioAllClicked;
 
-			RadioButtons.Add (Period.CurWeek, new RadioButton(RadioToday, "Тек. неделя"));
+			RadioButtons.Add(Period.CurWeek, new RadioButton(RadioToday, "Тек. неделя"));
 			RadioButtons[Period.CurWeek].Clicked += OnRadioCurWeekClicked;
-			RadioButtons.Add (Period.CurMonth, new RadioButton(RadioToday, "Тек. месяц"));
+			RadioButtons.Add(Period.CurMonth, new RadioButton(RadioToday, "Тек. месяц"));
 			RadioButtons[Period.CurMonth].Clicked += OnRadioCurMonthClicked;
-			RadioButtons.Add (Period.CurQuarter, new RadioButton(RadioToday, "Тек. квартал"));
+			RadioButtons.Add(Period.CurQuarter, new RadioButton(RadioToday, "Тек. квартал"));
 			RadioButtons[Period.CurQuarter].Clicked += OnRadioCurQuarterClicked;
-			RadioButtons.Add (Period.CurYear, new RadioButton(RadioToday, "Тек. год"));
+			RadioButtons.Add(Period.CurYear, new RadioButton(RadioToday, "Тек. год"));
 			RadioButtons[Period.CurYear].Clicked += OnRadioCurYearClicked;
 
 			ShowRadio = new Dictionary<Period, bool>();
-			ShowRadio.Add (Period.Today, true);
-			ShowRadio.Add (Period.Week, true);
-			ShowRadio.Add (Period.Month, true);
-			ShowRadio.Add (Period.ThreeMonth, false);
-			ShowRadio.Add (Period.SixMonth, false);
-			ShowRadio.Add (Period.Year, false);
-			ShowRadio.Add (Period.CurWeek, false);
-			ShowRadio.Add (Period.CurMonth, false);
-			ShowRadio.Add (Period.CurQuarter, false);
-			ShowRadio.Add (Period.CurYear, false);
-			ShowRadio.Add (Period.AllTime, true);
+			ShowRadio.Add(Period.Today, true);
+			ShowRadio.Add(Period.Week, true);
+			ShowRadio.Add(Period.Month, true);
+			ShowRadio.Add(Period.ThreeMonth, false);
+			ShowRadio.Add(Period.SixMonth, false);
+			ShowRadio.Add(Period.Year, false);
+			ShowRadio.Add(Period.CurWeek, false);
+			ShowRadio.Add(Period.CurMonth, false);
+			ShowRadio.Add(Period.CurQuarter, false);
+			ShowRadio.Add(Period.CurYear, false);
+			ShowRadio.Add(Period.AllTime, true);
 
 			_RadioMode = RadioModeType.DoubleColumn;
-			OnRepackRadios ();
+			OnRepackRadios();
 			ActiveRadio = Period.AllTime;
 		}
 
-		public Period ActiveRadio
-		{
-			get {foreach(KeyValuePair<Period, RadioButton> Pair in RadioButtons)
-				{
-					if (Pair.Value.Active)
+		public Period ActiveRadio {
+			get {
+				foreach(KeyValuePair<Period, RadioButton> Pair in RadioButtons) {
+					if(Pair.Value.Active)
 						return Pair.Key;
 				};
 				return Period.None;
 			}
-			set {if(value != Period.None)
+			set {
+				if(value != Period.None)
 					RadioButtons[value].Click();
 			}
 		}
 
-		protected void OnRadioTodayClicked (object sender, EventArgs e)
+		protected void OnRadioTodayClicked(object sender, EventArgs e)
 		{
-			if ((sender as RadioButton).Active == false)
-				return;
-			IsRadioChange = true;
-			StartDate.Date = DateTime.Now.Date;
-			EndDate.Date = DateTime.Now.Date;
-			EndRadioChange ();
+			if((sender as RadioButton).Active) {
+				IsRadioChange = true;
+				StartDate.Date = DateTime.Today.Date;
+				EndDate.Date = DateTime.Today.AddDays(1).AddMilliseconds(-1);
+				EndRadioChange();
+			}
 		}
 
-		protected void OnRadioWeekClicked (object sender, EventArgs e)
+		protected void OnRadioWeekClicked(object sender, EventArgs e)
 		{
-			if ((sender as RadioButton).Active == false)
+			if((sender as RadioButton).Active == false)
 				return;
 			IsRadioChange = true;
-			StartDate.Date = DateTime.Now.AddDays(-7);
-			EndDate.Date = DateTime.Now.Date;
-			EndRadioChange ();
+			StartDate.Date = DateTime.Today.AddDays(-7);
+			EndDate.Date = DateTime.Today.AddDays(1).AddMilliseconds(-1);
+			EndRadioChange();
 		}
 
-		protected void OnRadioMonthClicked (object sender, EventArgs e)
+		protected void OnRadioMonthClicked(object sender, EventArgs e)
 		{
-			if ((sender as RadioButton).Active == false)
+			if((sender as RadioButton).Active == false)
 				return;
 			IsRadioChange = true;
-			StartDate.Date = DateTime.Now.AddMonths(-1);
-			EndDate.Date = DateTime.Now.Date;
-			EndRadioChange ();
-		}
-		
-		protected void OnRadio3monthClicked (object sender, EventArgs e)
-		{
-			if ((sender as RadioButton).Active == false)
-				return;
-			IsRadioChange = true;
-			StartDate.Date = DateTime.Now.AddMonths(-3);
-			EndDate.Date = DateTime.Now.Date;
-			EndRadioChange ();
+			StartDate.Date = DateTime.Today.AddMonths(-1);
+			EndDate.Date = DateTime.Today.AddDays(1).AddMilliseconds(-1);
+			EndRadioChange();
 		}
 
-		protected void OnRadio6monthClicked (object sender, EventArgs e)
+		protected void OnRadio3monthClicked(object sender, EventArgs e)
 		{
-			if ((sender as RadioButton).Active == false)
+			if((sender as RadioButton).Active == false)
 				return;
 			IsRadioChange = true;
-			StartDate.Date = DateTime.Now.AddMonths(-6);
-			EndDate.Date = DateTime.Now.Date;
-			EndRadioChange ();
+			StartDate.Date = DateTime.Today.AddMonths(-3);
+			EndDate.Date = DateTime.Today.AddDays(1).AddMilliseconds(-1);
+			EndRadioChange();
 		}
-		
-		protected void OnRadioYearClicked (object sender, EventArgs e)
+
+		protected void OnRadio6monthClicked(object sender, EventArgs e)
 		{
-			if ((sender as RadioButton).Active == false)
+			if((sender as RadioButton).Active == false)
 				return;
 			IsRadioChange = true;
-			StartDate.Date = DateTime.Now.AddYears(-1);
-			EndDate.Date = DateTime.Now.Date;
-			EndRadioChange ();
+			StartDate.Date = DateTime.Today.AddMonths(-6);
+			EndDate.Date = DateTime.Today.AddDays(1).AddMilliseconds(-1);
+			EndRadioChange();
 		}
-		
-		protected void OnRadioAllClicked (object sender, EventArgs e)
+
+		protected void OnRadioYearClicked(object sender, EventArgs e)
 		{
-			if ((sender as RadioButton).Active == false)
+			if((sender as RadioButton).Active == false)
+				return;
+			IsRadioChange = true;
+			StartDate.Date = DateTime.Today.AddYears(-1);
+			EndDate.Date = DateTime.Today.AddDays(1).AddMilliseconds(-1);
+			EndRadioChange();
+		}
+
+		protected void OnRadioAllClicked(object sender, EventArgs e)
+		{
+			if((sender as RadioButton).Active == false)
 				return;
 			IsRadioChange = true;
 			StartDate.Date = new DateTime(1, 1, 1);
 			EndDate.Date = new DateTime(1, 1, 1);
-			EndRadioChange ();
+			EndRadioChange();
 		}
 
-		protected void OnRadioCurWeekClicked (object sender, EventArgs e)
+		protected void OnRadioCurWeekClicked(object sender, EventArgs e)
 		{
-			if ((sender as RadioButton).Active == false)
+			if((sender as RadioButton).Active == false)
 				return;
 			IsRadioChange = true;
 			int Day;
 			if(DateTime.Now.DayOfWeek == DayOfWeek.Sunday)
 				Day = 7;
-			else 
+			else
 				Day = (int)DateTime.Now.DayOfWeek;
-			StartDate.Date = DateTime.Now.AddDays(1-Day);
-			EndDate.Date = DateTime.Now.AddDays(7-Day);
-			EndRadioChange ();
+			StartDate.Date = DateTime.Today.AddDays(1 - Day);
+			EndDate.Date = DateTime.Now.AddDays(7 - Day);
+			EndRadioChange();
 		}
 
-		protected void OnRadioCurMonthClicked (object sender, EventArgs e)
+		protected void OnRadioCurMonthClicked(object sender, EventArgs e)
 		{
-			if ((sender as RadioButton).Active == false)
+			if((sender as RadioButton).Active == false)
 				return;
 			IsRadioChange = true;
 			int Year = DateTime.Today.Year;
 			int Month = DateTime.Today.Month;
 			System.Globalization.Calendar cal = System.Globalization.CultureInfo.CurrentCulture.Calendar;
-			int LastDay = cal.GetDaysInMonth (Year, Month);
+			int LastDay = cal.GetDaysInMonth(Year, Month);
 			StartDate.Date = new DateTime(Year, Month, 1);
 			EndDate.Date = new DateTime(Year, Month, LastDay);
-			EndRadioChange ();
+			EndRadioChange();
 		}
 
-		protected void OnRadioCurQuarterClicked (object sender, EventArgs e)
+		protected void OnRadioCurQuarterClicked(object sender, EventArgs e)
 		{
-			if ((sender as RadioButton).Active == false)
+			if((sender as RadioButton).Active == false)
 				return;
 			IsRadioChange = true;
-			int quarterNumber = (DateTime.Today.Month-1)/3+1;
-			StartDate.Date = new DateTime(DateTime.Today.Year, (quarterNumber-1)*3+1,1);
+			int quarterNumber = (DateTime.Today.Month - 1) / 3 + 1;
+			StartDate.Date = new DateTime(DateTime.Today.Year, (quarterNumber - 1) * 3 + 1, 1);
 			EndDate.Date = StartDate.Date.AddMonths(3).AddDays(-1);
-			EndRadioChange ();
+			EndRadioChange();
 		}
 
-		protected void OnRadioCurYearClicked (object sender, EventArgs e)
+		protected void OnRadioCurYearClicked(object sender, EventArgs e)
 		{
-			if ((sender as RadioButton).Active == false)
+			if((sender as RadioButton).Active == false)
 				return;
 			IsRadioChange = true;
 			StartDate.Date = new DateTime(DateTime.Today.Year, 1, 1);
 			EndDate.Date = new DateTime(DateTime.Today.Year, 12, 31);
-			EndRadioChange ();
+			EndRadioChange();
 		}
 
 		protected void OnRepackRadios()
 		{
 			uint Count = 0;
-			foreach(KeyValuePair<Period, bool> pair in ShowRadio)
-			{
+			foreach(KeyValuePair<Period, bool> pair in ShowRadio) {
 				if(pair.Value)
 					Count++;
 			}
 			uint MaxRow, MaxCol;
-			if(_RadioMode == RadioModeType.DoubleColumn)
-			{
+			if(_RadioMode == RadioModeType.DoubleColumn) {
 				MaxCol = 2;
-				MaxRow = (Count/2) + (Count%2);
-			}
-			else
-			{
+				MaxRow = (Count / 2) + (Count % 2);
+			} else {
 				MaxCol = 1;
 				MaxRow = Count;
 			}
-			foreach(Widget item in RadiosTable.AllChildren)
-			{
-				RadiosTable.Remove (item);
+			foreach(Widget item in RadiosTable.AllChildren) {
+				RadiosTable.Remove(item);
 			}
 
 			RadiosTable.NRows = MaxRow;
 			RadiosTable.NColumns = MaxCol;
 			uint CurRow = 0;
 			uint CurCol = 0;
-			foreach(KeyValuePair<Period, bool> pair in ShowRadio)
-			{
-				if(pair.Value)
-				{
-					RadiosTable.Attach (RadioButtons[pair.Key], CurCol, CurCol+1, CurRow, CurRow+1);
+			foreach(KeyValuePair<Period, bool> pair in ShowRadio) {
+				if(pair.Value) {
+					RadiosTable.Attach(RadioButtons[pair.Key], CurCol, CurCol + 1, CurRow, CurRow + 1);
 					CurRow++;
-					if(_RadioMode == RadioModeType.DoubleColumn && MaxRow <= CurRow)
-					{
+					if(_RadioMode == RadioModeType.DoubleColumn && MaxRow <= CurRow) {
 						CurRow = 0;
 						CurCol++;
 					}
@@ -336,13 +357,13 @@ namespace QSWidgetLib
 			RadiosTable.ShowAll();
 		}
 
-		protected void OnStartDateDateChanged (object sender, EventArgs e)
+		protected void OnStartDateDateChanged(object sender, EventArgs e)
 		{
 			if(DatesChanged != null && !IsRadioChange)
 				DatesChanged(this, EventArgs.Empty);
 		}
 
-		protected void OnEndDateDateChanged (object sender, EventArgs e)
+		protected void OnEndDateDateChanged(object sender, EventArgs e)
 		{
 			if(DatesChanged != null && !IsRadioChange)
 				DatesChanged(this, EventArgs.Empty);
@@ -351,8 +372,7 @@ namespace QSWidgetLib
 		private void EndRadioChange()
 		{
 			IsRadioChange = false;
-			if(DatesChanged != null)
-				DatesChanged(this, EventArgs.Empty);
+			DatesChanged?.Invoke(this, EventArgs.Empty);
 		}
 	}
 }
--- QSWidgetLib/gtk-gui/QSWidgetLib.SelectPeriod.cs
+++ QSWidgetLib/gtk-gui/QSWidgetLib.SelectPeriod.cs
@@ -5,93 +5,96 @@ namespace QSWidgetLib
 	public partial class SelectPeriod
 	{
 		private global::Gtk.VBox vboxMain;
-		
+
 		private global::Gtk.Label label1;
-		
+
 		private global::Gtk.Table RadiosTable;
-		
+
 		private global::Gtk.HBox hbox1;
-		
+
 		private global::QSWidgetLib.DatePicker StartDate;
-		
+
 		private global::Gtk.Label label2;
-		
+
 		private global::QSWidgetLib.DatePicker EndDate;
 
-		protected virtual void Build ()
+		protected virtual void Build()
 		{
-			global::Stetic.Gui.Initialize (this);
+			global::Stetic.Gui.Initialize(this);
 			// Widget QSWidgetLib.SelectPeriod
-			global::Stetic.BinContainer.Attach (this);
+			global::Stetic.BinContainer.Attach(this);
 			this.Name = "QSWidgetLib.SelectPeriod";
 			// Container child QSWidgetLib.SelectPeriod.Gtk.Container+ContainerChild
-			this.vboxMain = new global::Gtk.VBox ();
+			this.vboxMain = new global::Gtk.VBox();
 			this.vboxMain.Name = "vboxMain";
 			this.vboxMain.Spacing = 6;
 			// Container child vboxMain.Gtk.Box+BoxChild
-			this.label1 = new global::Gtk.Label ();
+			this.label1 = new global::Gtk.Label();
 			this.label1.Name = "label1";
-			this.label1.LabelProp = global::Mono.Unix.Catalog.GetString ("<b>Выбор периода</b>");
+			this.label1.LabelProp = global::Mono.Unix.Catalog.GetString("<b>Выбор периода</b>");
 			this.label1.UseMarkup = true;
-			this.vboxMain.Add (this.label1);
-			global::Gtk.Box.BoxChild w1 = ((global::Gtk.Box.BoxChild)(this.vboxMain [this.label1]));
+			this.vboxMain.Add(this.label1);
+			global::Gtk.Box.BoxChild w1 = ((global::Gtk.Box.BoxChild)(this.vboxMain[this.label1]));
 			w1.Position = 0;
 			w1.Expand = false;
 			w1.Fill = false;
 			// Container child vboxMain.Gtk.Box+BoxChild
-			this.RadiosTable = new global::Gtk.Table (((uint)(1)), ((uint)(2)), false);
+			this.RadiosTable = new global::Gtk.Table(((uint)(1)), ((uint)(2)), false);
 			this.RadiosTable.Name = "RadiosTable";
 			this.RadiosTable.RowSpacing = ((uint)(6));
 			this.RadiosTable.ColumnSpacing = ((uint)(6));
-			this.vboxMain.Add (this.RadiosTable);
-			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vboxMain [this.RadiosTable]));
+			this.vboxMain.Add(this.RadiosTable);
+			global::Gtk.Box.BoxChild w2 = ((global::Gtk.Box.BoxChild)(this.vboxMain[this.RadiosTable]));
 			w2.Position = 1;
 			w2.Expand = false;
 			// Container child vboxMain.Gtk.Box+BoxChild
-			this.hbox1 = new global::Gtk.HBox ();
+			this.hbox1 = new global::Gtk.HBox();
 			this.hbox1.Name = "hbox1";
 			this.hbox1.Spacing = 6;
 			// Container child hbox1.Gtk.Box+BoxChild
-			this.StartDate = new global::QSWidgetLib.DatePicker ();
+			this.StartDate = new global::QSWidgetLib.DatePicker();
 			this.StartDate.Events = ((global::Gdk.EventMask)(256));
 			this.StartDate.Name = "StartDate";
-			this.StartDate.Date = new global::System.DateTime (0);
+			this.StartDate.WithTime = false;
+			this.StartDate.Date = new global::System.DateTime(0);
 			this.StartDate.IsEditable = true;
 			this.StartDate.AutoSeparation = true;
-			this.hbox1.Add (this.StartDate);
-			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.StartDate]));
+			this.hbox1.Add(this.StartDate);
+			global::Gtk.Box.BoxChild w3 = ((global::Gtk.Box.BoxChild)(this.hbox1[this.StartDate]));
 			w3.Position = 0;
 			// Container child hbox1.Gtk.Box+BoxChild
-			this.label2 = new global::Gtk.Label ();
+			this.label2 = new global::Gtk.Label();
 			this.label2.Name = "label2";
-			this.label2.LabelProp = global::Mono.Unix.Catalog.GetString ("-");
-			this.hbox1.Add (this.label2);
-			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.label2]));
+			this.label2.LabelProp = global::Mono.Unix.Catalog.GetString("-");
+			this.hbox1.Add(this.label2);
+			global::Gtk.Box.BoxChild w4 = ((global::Gtk.Box.BoxChild)(this.hbox1[this.label2]));
 			w4.Position = 1;
 			w4.Expand = false;
 			w4.Fill = false;
 			// Container child hbox1.Gtk.Box+BoxChild
-			this.EndDate = new global::QSWidgetLib.DatePicker ();
+			this.EndDate = new global::QSWidgetLib.DatePicker();
 			this.EndDate.Events = ((global::Gdk.EventMask)(256));
 			this.EndDate.Name = "EndDate";
-			this.EndDate.Date = new global::System.DateTime (0);
+			this.EndDate.WithTime = false;
+			this.EndDate.Date = new global::System.DateTime(0);
 			this.EndDate.IsEditable = true;
 			this.EndDate.AutoSeparation = true;
-			this.hbox1.Add (this.EndDate);
-			global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.hbox1 [this.EndDate]));
+			this.hbox1.Add(this.EndDate);
+			global::Gtk.Box.BoxChild w5 = ((global::Gtk.Box.BoxChild)(this.hbox1[this.EndDate]));
 			w5.Position = 2;
-			this.vboxMain.Add (this.hbox1);
-			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.vboxMain [this.hbox1]));
+			this.vboxMain.Add(this.hbox1);
+			global::Gtk.Box.BoxChild w6 = ((global::Gtk.Box.BoxChild)(this.vboxMain[this.hbox1]));
 			w6.Position = 2;
 			w6.Expand = false;
 			w6.Fill = false;
-			this.Add (this.vboxMain);
-			if ((this.Child != null)) {
-				this.Child.ShowAll ();
+			this.Add(this.vboxMain);
+			if ((this.Child != null))
+			{
+				this.Child.ShowAll();
 			}
-			this.Hide ();
-			this.StartDate.DateChanged += new global::System.EventHandler (this.OnStartDateDateChanged);
-			this.EndDate.DateChanged += new global::System.EventHandler (this.OnEndDateDateChanged);
+			this.Hide();
+			this.StartDate.DateChanged += new global::System.EventHandler(this.OnStartDateDateChanged);
+			this.EndDate.DateChanged += new global::System.EventHandler(this.OnEndDateDateChanged);
 		}
 	}
 }
