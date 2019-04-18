fd023a54c6c4c0d32d509794ecbc4a2f8351cbb1
Andrey Gankov <gav@qsolution.ru>
2019-04-18 12:08:13 +0300

    Добавлена утилита для расчета диапазонов + тесты ее работы.

---
+++ QS.LibsTest/Utilities/DataRangeTest.cs
@@ -0,0 +1,44 @@
+﻿using NUnit.Framework;
+using QS.Utilities.Dates;
+using System;
+using System.Linq;
+
+namespace QS.Test.Utilities
+{
+	[TestFixture(TestOf = typeof(DateRange))]
+	public class DataRangeTest
+	{
+		[Test(Description = "Проверяем исключение диапазона")]
+		public void GetIntervals_ExcludeTest()
+		{
+			var mainRange = new DateRange(new DateTime(2019, 1, 1), new DateTime(2019, 5, 5));
+			var exclude = new DateRange(new DateTime(2019, 2, 5), new DateTime(2019, 2, 10));
+			mainRange.ExcludedRanges.Add(exclude);
+
+			var intervals = mainRange.GetIntervals.ToArray();
+			Assert.That(intervals[0].Begin, Is.EqualTo(new DateTime(2019, 1, 1)));
+			Assert.That(intervals[0].End, Is.EqualTo(new DateTime(2019, 2, 4)));
+
+			Assert.That(intervals[1].Begin, Is.EqualTo(new DateTime(2019, 2, 11)));
+			Assert.That(intervals[1].End, Is.EqualTo(new DateTime(2019, 5, 5)));
+		}
+
+		[Test(Description = "Проверяем исключение пересекаемых диапазонов")]
+		public void GetIntervals_ExcludeIntersectedTest()
+		{
+			var mainRange = new DateRange(new DateTime(2019, 1, 1), new DateTime(2019, 5, 5));
+			var exclude = new DateRange(new DateTime(2019, 2, 5), new DateTime(2019, 2, 28));
+			var exclude2 = new DateRange(new DateTime(2019, 2, 15), new DateTime(2019, 3, 10));
+
+			mainRange.ExcludedRanges.Add(exclude);
+			mainRange.ExcludedRanges.Add(exclude2);
+
+			var intervals = mainRange.GetIntervals.ToArray();
+			Assert.That(intervals[0].Begin, Is.EqualTo(new DateTime(2019, 1, 1)));
+			Assert.That(intervals[0].End, Is.EqualTo(new DateTime(2019, 2, 4)));
+
+			Assert.That(intervals[1].Begin, Is.EqualTo(new DateTime(2019, 3, 11)));
+			Assert.That(intervals[1].End, Is.EqualTo(new DateTime(2019, 5, 5)));
+		}
+	}
+}
---
+++ QS.Utilities/Dates/DateRange.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace QS.Utilities.Dates
+{
+	public class DateRange
+	{
+		public DateTime Begin { get; set; }
+		public DateTime End { get; set; }
+
+		public List<DateRange> ExcludedRanges { get; set; }
+
+		public DateRange(DateTime begin, DateTime end)
+		{
+			Begin = begin;
+			End = end;
+
+			ExcludedRanges = new List<DateRange>();
+		}
+
+		public IEnumerable<DateRange> GetIntervals{
+			get {
+				var start = Begin;
+				while(start <= End) {
+					var currentExclude = ExcludedRanges.Where(r => r.Begin <= start && r.End >= start).OrderBy(r => r.End).LastOrDefault();
+					if(currentExclude != null)
+						start = FindExcludeEnd(currentExclude).AddDays(1);
+					if (start > End)
+						yield break;
+
+					var nextExclude = ExcludedRanges.Where(r => r.Begin > start).OrderBy(r => r.Begin).FirstOrDefault();
+					DateTime end;
+					if(nextExclude == null || nextExclude.Begin > End)
+						end = End;
+					else
+						end = nextExclude.Begin.AddDays(-1);
+
+					yield return new DateRange(start, end);
+					start = end.AddDays(1);
+				};
+			}
+		}
+
+		private DateTime FindExcludeEnd(DateRange range)
+		{
+			var next = ExcludedRanges.Where(r => r.Begin <= range.End && r.End > range.End).OrderBy(r => r.End).LastOrDefault();
+			if (next == null)
+				return range.End;
+			else
+				return FindExcludeEnd(next);
+		}
+	}
+}
