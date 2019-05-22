e83fe540af621d29d6070e80c129da712dba8f43
Andrey Gankov <gav@qsolution.ru>
2019-05-22 09:03:51 +0300

    Добавлен класс для расширенной манипуляции со строками. В частности замены всего одного вхождения подстроки.

---
+++ QS.Utilities/Text/StringManipulationHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+namespace QS.Utilities.Text
+{
+	public static class StringManipulationHelper
+	{
+		/// <summary>
+		/// Заменяем первое вохждение найденной строки.
+		/// </summary>
+		public static string ReplaceFirstOccurrence(this string Source, string Find, string Replace)
+		{
+			int Place = Source.IndexOf(Find);
+			string result = Source.Remove(Place, Find.Length).Insert(Place, Replace);
+			return result;
+		}
+
+		/// <summary>
+		/// Заменяем последнее вохждение найденной строки.
+		/// </summary>
+		public static string ReplaceLastOccurrence(this string Source, string Find, string Replace)
+		{
+			int Place = Source.LastIndexOf(Find);
+			string result = Source.Remove(Place, Find.Length).Insert(Place, Replace);
+			return result;
+		}
+	}
+}
