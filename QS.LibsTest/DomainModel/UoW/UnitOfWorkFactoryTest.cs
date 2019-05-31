195814a73894576b6f7ad684c1cde60f9c462f03
Andrey Gankov <gav@qsolution.ru>
2019-05-31 19:28:42 +0300

    Добавил тест проверки что имя метода вызвавшего статическую фабрику, в дочерний класс передается корректно. Было подозрение что оно может заменится на сам статический метод.

---
+++ QS.LibsTest/DomainModel/UoW/UnitOfWorkFactoryTest.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using QS.DomainModel.UoW;
+using System;
+namespace QS.Test.DomainModel.UoW
+{
+	[TestFixture(TestOf = typeof(UnitOfWorkFactory))]
+	public class UnitOfWorkFactoryTest
+	{
+		[Test(Description = "Было подозрение что вызов статического метода в результате, по цепочке в реальный класс передадут неправильно информацию о том кто вызвал метод.")]
+		public void StaticCallsCorrectGetNameOfCallersTest()
+		{
+			var uow = UnitOfWorkFactory.CreateWithoutRoot();
+			Assert.That(uow.ActionTitle.CallerMemberName, Is.EqualTo(nameof(StaticCallsCorrectGetNameOfCallersTest)));
+		}
+	}
+}
