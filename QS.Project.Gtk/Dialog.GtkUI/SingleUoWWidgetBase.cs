e9c90faa9adfc4bfe16f6e6fa9a5d1f88ef23c16
Vadim Gordeev <enzogord@gmail.com>
2019-06-11 13:36:14 +0300

    Добавлена база виджета с закрытием UoW при уничтожении

---
+++ QS.Project.Gtk/Dialog.GtkUI/SingleUoWWidgetBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Gtk;
+using QS.DomainModel.UoW;
+
+namespace QS.Dialog.GtkUI
+{
+	public class SingleUoWWidgetBase : Bin, ISingleUoWDialog
+	{
+		public SingleUoWWidgetBase()
+		{
+		}
+
+		public IUnitOfWork UoW { get; set; }
+
+		public override void Destroy()
+		{
+			UoW?.Dispose();
+			base.Destroy();
+		}
+	}
+}
