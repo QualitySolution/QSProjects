416a5b183c3b35dec5416f4d3ad8957a765ff5aa
opopve <opopve@gmail.com>
2019-07-04 12:33:35 +0300

    SingleUowTabBase сделан IDisposable-ом

--- QS.Project.Gtk/Dialog.GtkUI/SingleUowTabBase.cs
+++ QS.Project.Gtk/Dialog.GtkUI/SingleUowTabBase.cs
@@ -3,12 +3,27 @@ using QS.DomainModel.UoW;
 
 namespace QS.Dialog.Gtk
 {
-	public abstract class SingleUowTabBase : TdiTabBase, ISingleUoWDialog
+	public abstract class SingleUowTabBase : TdiTabBase, ISingleUoWDialog, IDisposable
 	{
 		public SingleUowTabBase()
 		{
 		}
 
 		public virtual IUnitOfWork UoW { get; protected set; }
+
+		bool canDisposeUoW = false;
+
+		protected void CreateDisposableUoW()
+		{
+			UoW = UnitOfWorkFactory.CreateWithoutRoot();
+			canDisposeUoW = true;
+		}
+
+		public override void Dispose()
+		{
+			if(canDisposeUoW)
+				UoW?.Dispose();
+			base.Dispose();
+		}
 	}
 }
