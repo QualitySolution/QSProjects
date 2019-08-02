bc69b1edd36c6188b37b848e232efdabaa71bf3d
Vadim Gordeev <enzogord@gmail.com>
2019-08-02 16:34:47 +0300

    Добавлены подписки на изменения свойств сущности для моделей представления виджетов

--- QS.Project/ViewModels/EntityWidgetViewModelBase.cs
+++ QS.Project/ViewModels/EntityWidgetViewModelBase.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using QS.Services;
 using QS.DomainModel.Entity;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using Gamma.Utilities;
 
 namespace QS.ViewModels
 {
@@ -21,5 +24,70 @@ namespace QS.ViewModels
 
 			PermissionResult = commonServices.PermissionService.ValidateUserPermission(typeof(TEntity), commonServices.UserService.CurrentUserId);
 		}
+
+		#region Подписки на изменения свойств
+
+		private Dictionary<string, IList<string>> propertyTriggerRelations = new Dictionary<string, IList<string>>();
+		private Dictionary<string, List<Action>> propertyOnChangeActions = new Dictionary<string, List<Action>>();
+		private List<Action> onAnyPropertyChangedActions = new List<Action>();
+
+		private void Entity_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+		{
+			if(propertyTriggerRelations.ContainsKey(e.PropertyName)) {
+				foreach(var relatedPropertyName in propertyTriggerRelations[e.PropertyName]) {
+					OnPropertyChanged(relatedPropertyName);
+				}
+			}
+
+			if(propertyOnChangeActions.ContainsKey(e.PropertyName)) {
+				foreach(var action in propertyOnChangeActions[e.PropertyName]) {
+					action.Invoke();
+				}
+			}
+
+			foreach(var onChangeAction in onAnyPropertyChangedActions) {
+				onChangeAction.Invoke();
+			}
+		}
+
+		/// <summary>
+		/// Устанавливает зависимость свойства сущности к свойствам модели представления.
+		/// Если произойдет изменение указанного свойства сущности, то вызовутся изменения всех связанных свойств модели представления
+		/// </summary>
+		protected void SetPropertyChangeRelation<T>(Expression<Func<TEntity, object>> entityTriggeredProperty, params Expression<Func<T>>[] vmChangingPropertiesExpressions)
+		{
+			IList<string> vmChangingProperties = new List<string>();
+			string entityPropertyName = Entity.GetPropertyName(entityTriggeredProperty);
+			if(propertyTriggerRelations.ContainsKey(entityPropertyName)) {
+				vmChangingProperties = propertyTriggerRelations[entityPropertyName];
+			} else {
+				propertyTriggerRelations.Add(entityPropertyName, vmChangingProperties);
+			}
+
+			foreach(var cpe in vmChangingPropertiesExpressions) {
+				string vmChangingPropertyName = GetPropertyName(cpe);
+				if(!vmChangingProperties.Contains(vmChangingPropertyName)) {
+					vmChangingProperties.Add(vmChangingPropertyName);
+				}
+			}
+		}
+
+		protected void OnEntityPropertyChanged(Action onChangeAction, params Expression<Func<TEntity, object>>[] entityTriggeredProperty)
+		{
+			foreach(var property in entityTriggeredProperty) {
+				string entityPropertyName = Entity.GetPropertyName(property);
+				if(!propertyOnChangeActions.ContainsKey(entityPropertyName)) {
+					propertyOnChangeActions.Add(entityPropertyName, new List<Action>());
+				}
+				propertyOnChangeActions[entityPropertyName].Add(onChangeAction);
+			}
+		}
+
+		protected void OnEntityAnyPropertyChanged(Action onChangeAction)
+		{
+			onAnyPropertyChangedActions.Add(onChangeAction);
+		}
+
+		#endregion
 	}
 }
