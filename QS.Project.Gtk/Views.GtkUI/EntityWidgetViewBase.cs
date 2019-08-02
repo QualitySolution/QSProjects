24b6e9d645b4d6d8ef1cdd362731f4455d67107c
Vadim Gordeev <enzogord@gmail.com>
2019-08-02 11:23:09 +0300

    Добавлены базовые классы для представления и модели представления виджета с сущностью

---
+++ QS.Project.Gtk/Views.GtkUI/EntityWidgetViewBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using QS.ViewModels;
+namespace QS.Views.GtkUI
+{
+	public abstract class EntityWidgetViewBase<TViewModel> : Gtk.Bin
+		where TViewModel : UoWWidgetViewModelBase
+	{
+		protected TViewModel ViewModel { get; }
+
+		public EntityWidgetViewBase(TViewModel viewModel)
+		{
+			ViewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
+		}
+	}
+}
---
+++ QS.Project/ViewModels/EntityWidgetViewModelBase.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using QS.Services;
+using QS.DomainModel.Entity;
+
+namespace QS.ViewModels
+{
+	public abstract class EntityWidgetViewModelBase<TEntity> : UoWWidgetViewModelBase
+		where TEntity : class, IDomainObject
+	{
+		private readonly ICommonServices commonServices;
+
+		protected IPermissionResult PermissionResult { get; private set; }
+
+		public TEntity Entity { get; private set; }
+
+		protected EntityWidgetViewModelBase(TEntity entity, ICommonServices commonServices) 
+		: base((commonServices ?? throw new ArgumentNullException(nameof(commonServices))).InteractiveService)
+		{
+			Entity = entity;
+			this.commonServices = commonServices ?? throw new ArgumentNullException(nameof(commonServices));
+
+			PermissionResult = commonServices.PermissionService.ValidateUserPermission(typeof(TEntity), commonServices.UserService.CurrentUserId);
+		}
+	}
+}
