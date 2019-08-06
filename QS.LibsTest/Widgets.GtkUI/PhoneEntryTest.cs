62fed01a74d3addd97e93478262db9d5de1c0dd2
Gankov <gav@qsolution.ru>
2019-08-06 09:18:44 +0300

    Добавлен новый виджет для ввода телефонов. Алгоритм виджета полностью переделано. Надеюсь получится легко дополнять другими форматами ввода.
    Добавлено несколько тестов, для фиксации основных моментов.

---
+++ QS.LibsTest/Widgets.GtkUI/PhoneEntryTest.cs
@@ -0,0 +1,81 @@
+﻿using NUnit.Framework;
+using QS.Widgets.GtkUI;
+namespace QS.Test.Widgets.GtkUI
+{
+	[TestFixture(TestOf = typeof(PhoneEntry))]
+	public class PhoneEntryTest
+	{
+		[Test(Description = "Проверяем что корректно конвертируем номер из других вариантов написания.")]
+		[TestCase("+7(812)3098089")]
+		[TestCase("+7(812)309-80-89")]
+		[TestCase("+7(812) 309 80 89")]
+		[TestCase("+7 812 309 - 80 - 89")]
+		[TestCase("88123098089")]
+		[TestCase("+78123098089")]
+		[TestCase("8-812-309-80-89")]
+		[TestCase("8 812 309-8-089")]
+		public void RussiaOnlyHyphenated_ConvertFromAnotherFormatsTest(string insertPhone)
+		{
+			var phoneentry = new PhoneEntry();
+			phoneentry.PhoneFormat = PhoneFormat.RussiaOnlyHyphenated;
+			phoneentry.InsertText(insertPhone);
+
+			Assert.That(phoneentry.Text, Is.EqualTo("+7-812-309-80-89"));
+		}
+
+		[Test(Description = "Проверяем что обрезаем лишнии цифры в номере.")]
+		public void RussiaOnlyHyphenated_RemoveExtraDigitsTest()
+		{
+			var phoneentry = new PhoneEntry();
+			phoneentry.PhoneFormat = PhoneFormat.RussiaOnlyHyphenated;
+			phoneentry.InsertText("+700011122334455");
+
+			Assert.That(phoneentry.Text, Is.EqualTo("+7-000-111-22-33"));
+		}
+
+		[Test(Description = "Проверяем что при вводе номера курсор останется на последней позиции.")]
+		public void RussiaOnlyHyphenated_CursorOnEndTest()
+		{
+			var phoneentry = new PhoneEntry();
+			phoneentry.PhoneFormat = PhoneFormat.RussiaOnlyHyphenated;
+			int pos = 1;
+
+			phoneentry.FormatString("8", ref pos);
+			Assert.That(pos, Is.EqualTo(2));
+
+			pos = 5;
+			phoneentry.FormatString("+7812", ref pos);
+			Assert.That(pos, Is.EqualTo(6));
+
+			pos = 7;
+			phoneentry.FormatString("+7-8123", ref pos);
+			Assert.That(pos, Is.EqualTo(8));
+		}
+
+		[Test(Description = "Проверяем что курсор при вводе останется межу теми же цифрами что и раньше.")]
+		public void RussiaOnlyHyphenated_CursorKeepLocation1Test()
+		{
+			var phoneentry = new PhoneEntry();
+			phoneentry.PhoneFormat = PhoneFormat.RussiaOnlyHyphenated;
+			//Курсор        
+			//+78123098|089
+			int pos = 9;
+			phoneentry.FormatString("+78123098089", ref pos);
+			//+7-812-309-8|0-89
+			Assert.That(pos, Is.EqualTo(12));
+		}
+
+		[Test(Description = "Проверяем что курсор при вводе останется межу теми же цифрами что и раньше.")]
+		public void RussiaOnlyHyphenated_CursorKeepLocation2Test()
+		{
+			var phoneentry = new PhoneEntry();
+			phoneentry.PhoneFormat = PhoneFormat.RussiaOnlyHyphenated;
+			//Курсор        
+			//881230|98089
+			int pos = 6;
+			phoneentry.FormatString("88123098089", ref pos);
+			//+7-812-30|9-80-89
+			Assert.That(pos, Is.EqualTo(9));
+		}
+	}
+}
