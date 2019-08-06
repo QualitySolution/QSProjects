0f06db2ed583375307bb00d177fb59605d9bf52b
opopve <opopve@gmail.com>
2019-08-06 08:18:05 +0300

    в EntityWidgetViewBase добавлен конструктор по умолчанию

--- QS.Project.Gtk/Views.GtkUI/EntityWidgetViewBase.cs
+++ QS.Project.Gtk/Views.GtkUI/EntityWidgetViewBase.cs
@@ -5,11 +5,22 @@ namespace QS.Views.GtkUI
 	public abstract class EntityWidgetViewBase<TViewModel> : Gtk.Bin
 		where TViewModel : UoWWidgetViewModelBase
 	{
-		protected TViewModel ViewModel { get; }
+		TViewModel viewModel;
+		public TViewModel ViewModel {
+			get => viewModel;
+			set {
+				viewModel = value;
+				ConfigureWidget();
+			}
+		}
 
 		protected EntityWidgetViewBase(TViewModel viewModel)
 		{
 			ViewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
 		}
+
+		protected EntityWidgetViewBase() { }
+
+		protected virtual void ConfigureWidget() => Sensitive = ViewModel != null;
 	}
 }
