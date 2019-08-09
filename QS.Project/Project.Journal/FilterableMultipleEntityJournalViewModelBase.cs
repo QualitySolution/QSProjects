e59f691f97109e1ce6696532650c8196c707b4cd
ZhuchkovMichael <Taenga.Zhu@yandex.ru>
2019-08-09 17:25:25 +0300

    Добавлен класс журнала для множества документов с фильтром

---
+++ QS.Project/Project.Journal/FilterableMultipleEntityJournalViewModelBase.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using QS.DomainModel.Config;
+using QS.Project.Filter;
+using QS.Services;
+
+namespace QS.Project.Journal
+{
+	public class FilterableMultipleEntityJournalViewModelBase<TNode, TFilterViewModel> : MultipleEntityJournalViewModelBase<TNode>
+		where TNode : JournalEntityNodeBase
+		where TFilterViewModel : FilterViewModelBase<TFilterViewModel>
+	{
+
+		private TFilterViewModel filterViewModel;
+		public TFilterViewModel FilterViewModel {
+			get => filterViewModel;
+			set {
+				filterViewModel = value;
+				Filter = filterViewModel;
+			}
+		}
+
+		public FilterableMultipleEntityJournalViewModelBase(TFilterViewModel filterViewModel, IEntityConfigurationProvider entityConfigurationProvider, ICommonServices commonServices) : base(entityConfigurationProvider, commonServices)
+		{
+			FilterViewModel = filterViewModel;
+		}
+	}
+}
