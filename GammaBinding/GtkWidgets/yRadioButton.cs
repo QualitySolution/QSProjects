e08a9a2f8ea6367c52e533cc873a3caf31a58518
Gankov <gav@qsolution.ru>
2019-08-12 18:56:33 +0300

    Added RadioButton Widget

---
+++ GammaBinding/GtkWidgets/yRadioButton.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Gtk;
+using Gamma.Binding.Core;
+using System.Linq.Expressions;
+
+namespace Gamma.GtkWidgets
+{
+	[System.ComponentModel.ToolboxItem (true)]
+	[System.ComponentModel.Category ("Gamma Gtk")]
+	public class yRadioButton : RadioButton
+	{
+		public BindingControler<yRadioButton> Binding { get; private set; }
+
+		public yRadioButton() : base("")
+		{
+			Binding = new BindingControler<yRadioButton> (this, new Expression<Func<yRadioButton, object>>[] {
+				(w => w.Active),
+				(w => w.BindedValue)
+			});
+
+			Toggled += YRadioButton_Toggled;
+		}
+
+
+		public yRadioButton(object tag) : this()
+		{
+			Tag = tag;
+		}
+
+		/// <summary>
+		/// For store user data, like WinForms Tag.
+		/// </summary>
+		public object Tag;
+
+		void YRadioButton_Toggled(object sender, EventArgs e)
+		{
+			Console.WriteLine($"togleed {BindValueWhenActivated} = {Active}");
+			Binding.FireChange(w => w.Active);
+			if (Active)
+				Binding.FireChange(w => w.BindedValue);
+		}
+
+		public object BindValueWhenActivated { get; set; }
+
+		public object BindedValue
+		{
+			get => Active ? BindValueWhenActivated : null;
+			set => Active = value == BindValueWhenActivated;
+		}
+	}
+}
