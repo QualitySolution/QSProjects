d9230c37838c4f1974c4383051e52e1d6359e6c3
Gankov <gav@qsolution.ru>
2019-08-16 15:39:17 +0300

    Уменьшил используемую память при загрузке, так как на новом сервере всего 20Гб.

--- QSOsmBaseScripts/CreateBaseOnPostgres.sh
+++ QSOsmBaseScripts/CreateBaseOnPostgres.sh
@@ -3,4 +3,4 @@ cd "$(dirname "$0")"
 
 wget -N http://download.geofabrik.de/russia/northwestern-fed-district-latest.osm.pbf
 
-osm2pgsql --create --number-processes 4 --cache 20000 -d osmgis -U postgres -j northwestern-fed-district-latest.osm.pbf
+osm2pgsql --create --number-processes 4 --cache 13000 -d osmgis -U postgres -j northwestern-fed-district-latest.osm.pbf
