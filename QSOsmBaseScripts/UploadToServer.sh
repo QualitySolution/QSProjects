21900b68e03896013d50801c0c04857d863ed7e4
Gankov <gav@qsolution.ru>
2019-08-16 09:35:34 +0300

    Обновил скрипт загрузки.

--- QSOsmBaseScripts/UploadToServer.sh
+++ QSOsmBaseScripts/UploadToServer.sh
@@ -1,4 +1,18 @@
 #!/bin/bash
+echo "Which server?"
+echo "1) 192.168.0.16"
+echo "2) osm.vod.qsolution.ru"
+read case;
+
+case $case in
+    1)
+SERVER="192.168.0.16"
+;;
+    2)
+SERVER="osm.vod.qsolution.ru"
+;;
+esac
+
 echo "Which version to upload?"
 echo "1) Release"
 echo "2) Debug"
@@ -8,10 +22,10 @@ read case;
 
 case $case in
     1)
-rsync -vizaP ./bin/Release/ admin@vod.qsolution.ru:/home/admin/OsmBaseScripts
+rsync -vizaP ./bin/Release/ admin@$SERVER:/home/admin/OsmBaseScripts
 ;;
     2)
-rsync -vizaP ./bin/Debug/ admin@vod.qsolution.ru:/home/admin/OsmBaseScripts
+rsync -vizaP ./bin/Debug/ admin@$SERVER:/home/admin/OsmBaseScripts
 ;;
 esac
 
