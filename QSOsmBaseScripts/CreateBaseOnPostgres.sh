018d7a3bb2fe06300b5689eb4f60d6f0b437ebfc
Gankov <gav@qsolution.ru>
2019-08-16 10:55:52 +0300

    Заменил линк на скачивание дампа карты, гис лаб больше не распространяет дампы. Поэтому приходится качать с геофабрики весь северозапад.

--- QSOsmBaseScripts/CreateBaseOnPostgres.sh
+++ QSOsmBaseScripts/CreateBaseOnPostgres.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-wget -N http://data.gis-lab.info/osm_dump/dump/latest/RU-LEN.osm.pbf
+wget -N http://download.geofabrik.de/russia/northwestern-fed-district-latest.osm.pbf
 
-osm2pgsql --create --number-processes 4 --cache 20000 -d osmgis -U postgres -j RU-LEN.osm.pbf
+osm2pgsql --create --number-processes 4 --cache 20000 -d osmgis -U postgres -j northwestern-fed-district-latest.osm.pbf
