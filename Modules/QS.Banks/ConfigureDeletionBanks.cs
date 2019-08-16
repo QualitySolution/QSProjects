e116c469b36ac6f52104a622cc3538451ce33348
Vadim Gordeev <enzogord@gmail.com>
2019-08-16 16:29:07 +0300

    Перенесена настройка удаления в модель

---
+++ Modules/QS.Banks/ConfigureDeletionBanks.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using QS.Banks.Domain;
+using QS.Deletion;
+
+namespace QS.Banks
+{
+	public static class ConfigureDeletionBanks
+	{
+		private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+
+		public static void ConfigureDeletion()
+		{
+			logger.Info("Настройка параметров удаления в модуле Banks...");
+
+			DeleteConfig.AddHibernateDeleteInfo<BankRegion>()
+				.AddDeleteDependence<Bank>(item => item.Region);
+
+			DeleteConfig.AddHibernateDeleteInfo<Bank>()
+				.AddDeleteDependence<Account>(item => item.InBank)
+				.AddDeleteDependenceFromCollection(item => item.CorAccounts);
+
+			DeleteConfig.AddHibernateDeleteInfo<Account>()
+				.AddClearDependence<Bank>(item => item.DefaultCorAccount);
+		}
+	}
+}
--- Modules/QSContacts/ConfigureDeletion.cs
+++ Modules/QSContacts/ConfigureDeletion.cs
@@ -1,6 +1,4 @@
-﻿using System.Collections.Generic;
-using QS.Contacts;
-using QS.Deletion;
+﻿using QS.Contacts;
 
 namespace QSContacts
 {
@@ -8,55 +6,7 @@ namespace QSContacts
 	{
 		public static void ConfigureDeletion ()
 		{
-			logger.Info ("Настройка параметров удаления в модуле Contacts...");
-
-			DeleteConfig.AddDeleteInfo (new DeleteInfo {
-				ObjectClass = typeof(PhoneType),
-				SqlSelect = "SELECT id, name FROM @tablename ",
-				DisplayString = "Тип телефона {1}",
-				DeleteItems = new List<DeleteDependenceInfo> {
-					DeleteDependenceInfo.Create<Phone> (item => item.NumberType)
-				}
-			}.FillFromMetaInfo ()
-			);
-
-			DeleteConfig.AddDeleteInfo (new DeleteInfo {
-				ObjectClass = typeof(Phone),
-				SqlSelect = "SELECT id, number, additional FROM @tablename ",
-				DisplayString = "Телефон {1} доб. {2}"
-			}.FillFromMetaInfo ()
-			);
-
-			DeleteConfig.AddDeleteInfo (new DeleteInfo {
-				ObjectClass = typeof(EmailType),
-				SqlSelect = "SELECT id, name FROM @tablename ",
-				DisplayString = "Тип E-mail {1}",
-				DeleteItems = new List<DeleteDependenceInfo> {
-					DeleteDependenceInfo.Create<Email> (item => item.EmailType)
-				}
-			}.FillFromMetaInfo ()
-			);
-
-			DeleteConfig.AddDeleteInfo (new DeleteInfo {
-				ObjectClass = typeof(Email),
-				SqlSelect = "SELECT id, address FROM @tablename ",
-				DisplayString = "{1}"
-			}.FillFromMetaInfo ()
-			);
-
-			DeleteConfig.AddDeleteInfo (new DeleteInfo {
-				ObjectClass = typeof(Person),
-				SqlSelect = "SELECT id, surname, name FROM @tablename ",
-				DisplayString = "{1} {2}"
-			}.FillFromMetaInfo ()
-			);
-
-			DeleteConfig.AddDeleteInfo (new DeleteInfo {
-				ObjectClass = typeof(Post),
-				SqlSelect = "SELECT id, name FROM @tablename ",
-				DisplayString = "{1}"
-			}.FillFromMetaInfo ()
-			);
+			ConfigureDeletionContacts.ConfigureDeletion();
 		}
 	}
 }
