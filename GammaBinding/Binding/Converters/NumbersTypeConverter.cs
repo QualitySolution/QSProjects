c93fd747b0a6d7c75b6b5a82a3b3505a23c4515c
Gankov <gav@qsolution.ru>
2019-08-16 20:46:59 +0300

    Add NumbersConverters

---
+++ GammaBinding/Binding/Converters/NumbersTypeConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+
+namespace Gamma.Binding.Converters
+{
+	public class NumbersTypeConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if (targetType == typeof(int))
+				return (System.Convert.ToInt32 (value));
+			return null;
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if(targetType == typeof(short))
+				return (System.Convert.ToInt16(value));
+			if (targetType == typeof(ushort))
+				return (System.Convert.ToUInt16(value));
+			if (targetType == typeof(byte))
+				return (System.Convert.ToByte(value));
+			return null;
+		}
+	}
+}
