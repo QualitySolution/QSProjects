f5b3a2ec51400813481c5fba07cc298717a62058
Gankov <gav@qsolution.ru>
2019-09-23 01:08:33 +0300

    Добавлен класс по умолчанию создающий CommonServices для Gtk приложений чтобы не дублировать код в каждом приложении.

---
+++ QS.Project.Gtk/Project.Services.GtkUI/GtkAppServicesConfig.cs
@@ -0,0 +1,64 @@
+﻿using QS.Permissions;
+using QS.Services;
+using QS.Validation;
+using QS.Validation.GtkUI;
+
+namespace QS.Project.Services.GtkUI
+{
+	public static class GtkAppServicesConfig
+	{
+		private static ICommonServices commonServices;
+		public static ICommonServices CommonServices {
+			get {
+				if(commonServices == null) {
+					commonServices = new CommonServices(ValidationService, InteractiveService, PermissionService, UserService);
+				}
+				return commonServices;
+			}
+		}
+
+		private static IValidationService validationService;
+		public static IValidationService ValidationService {
+			get {
+				if(validationService == null) {
+					IValidationViewFactory viewFactory = new GtkValidationViewFactory();
+					validationService = new ValidationService(viewFactory);
+				}
+				return validationService;
+			}
+		}
+
+		private static IInteractiveService interactiveService;
+		public static IInteractiveService InteractiveService {
+			get {
+				if(interactiveService == null) {
+					interactiveService = new GtkInteractiveService();
+				}
+				return interactiveService;
+			}
+		}
+
+		private static IPermissionService permissionService;
+		public static IPermissionService PermissionService {
+			get {
+				if(permissionService == null) {
+					permissionService = new PermissionService(
+						PermissionsSettings.EntityPermissionValidator,
+						PermissionsSettings.PresetPermissionValidator
+					);
+				}
+				return permissionService;
+			}
+		}
+
+		private static IUserService userService;
+		public static IUserService UserService {
+			get {
+				if(userService == null) {
+					userService = new UserService();
+				}
+				return userService;
+			}
+		}
+	}
+}
---
+++ QS.Project.Gtk/Project.Services.GtkUI/GtkInteractiveService.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using QS.Dialog;
+using QS.Dialog.GtkUI;
+using QS.Services;
+
+namespace QS.Project.Services.GtkUI
+{
+	public class GtkInteractiveService : IInteractiveService
+	{
+		private IInteractiveMessage interactiveMessage;
+		public IInteractiveMessage InteractiveMessage {
+			get {
+				if(interactiveMessage == null) {
+					interactiveMessage = new GtkMessageDialogsInteractive();
+				}
+				return interactiveMessage;
+			}
+		}
+
+		private IInteractiveQuestion interactiveQuestion;
+		public IInteractiveQuestion InteractiveQuestion {
+			get {
+				if(interactiveQuestion == null) {
+					interactiveQuestion = new GtkQuestionDialogsInteractive();
+				}
+				return interactiveQuestion;
+			}
+		}
+	}
+}
