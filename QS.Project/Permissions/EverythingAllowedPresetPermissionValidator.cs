b0906ff07c4c939f01fc6c853dbab2364eeab0c6
Gankov <gav@qsolution.ru>
2019-09-23 01:19:56 +0300

    Добвил заглушки для валидатора прав, чтобы была возмоность как-то как то использовать новый механизм диалогов, в случае если проверка прав не требуется.

---
+++ QS.Project/Permissions/EverythingAllowedPresetPermissionValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using QS.DomainModel.Entity.PresetPermissions;
+
+namespace QS.Permissions
+{
+	//FIXME Временная заглушка для валидатора прав, пока не появится возмоность его просто не настраивать, в тех случаях когда права не используются.
+	public class EverythingAllowedPresetPermissionValidator : IPresetPermissionValidator
+	{
+		public bool Validate(string presetPermissionName, int userId)
+		{
+			return true;
+		}
+	}
+}
