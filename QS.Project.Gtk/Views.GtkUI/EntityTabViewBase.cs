ad136e7df4e458ed09c299b541cc9159310c1746
Gankov <gav@qsolution.ru>
2019-09-22 19:18:52 +0300

    Добавлен базовый класс для диалога нового типа в котором напрямую можно обращатся к Entity, для меньшей работе при переделывании старых диалогов.

---
+++ QS.Project.Gtk/Views.GtkUI/EntityTabViewBase.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel;
+using QS.DomainModel.Entity;
+using QS.ViewModels;
+
+namespace QS.Views.GtkUI
+{
+	public class EntityTabViewBase<TViewModel, TEntity> : TabViewBase<TViewModel>
+		where TViewModel : EntityTabViewModelBase<TEntity>
+		where TEntity : class, IDomainObject, INotifyPropertyChanged, new()
+	{
+		protected TEntity Entity => ViewModel.Entity;
+
+		public EntityTabViewBase(TViewModel viewModel): base(viewModel)
+		{
+
+		}
+	}
+}
