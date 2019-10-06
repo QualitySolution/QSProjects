252317763e62fa32dcf5056fc35ab76ca8cdbbe1
Gankov <gav@qsolution.ru>
2019-10-06 21:53:25 +0300

    Добавлен комментарий по запуску тестов на дженкинсе.

--- QS.LibsTest/GtkUI/GtkInit.cs
+++ QS.LibsTest/GtkUI/GtkInit.cs
@@ -7,6 +7,12 @@ namespace QS.Test.GtkUI
 	{
 		private static bool initialized;
 
+		/// <summary>
+		/// Внимание! Для запуска таких тестов на дженкисе(то есть без наличия запущеного XServer)
+		/// тесты необходимо запусткать в виртульном дисплее. Например так:
+		/// <code>xvfb-run mono QSProjects/packages/NUnit.ConsoleRunner.3.10.0/tools/nunit3-console.exe QSProjects/QS.LibsTest/bin/Debug/QS.LibsTest.dll </code>
+		/// Естественно в системе должен быть установлен xvfb-run.s
+		/// </summary>
 		public static void AtOnceInitGtk()
 		{
 			if (initialized)
