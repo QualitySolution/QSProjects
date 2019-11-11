ccb04b0151baf70bd22955ae5664c28275885e42
opopve <opopve@gmail.com>
2019-11-11 15:37:04 +0300

    При завершении работы UoW, пишем в лог где был создан этот UoW.

--- QS.Project/DomainModel/Tracking/UowWatcher.cs
+++ QS.Project/DomainModel/Tracking/UowWatcher.cs
@@ -31,8 +31,10 @@ namespace QS.DomainModel.Tracking
 			RemoveLost();
 			lock (RegisteredUoWs)
 			{
+				var uowLink = RegisteredUoWs.FirstOrDefault(x => x.Value.UnitOfWork == uow).Value;
+				var createdString = $"Создан в {uowLink.Title.CallerMemberName} ({uowLink.Title.CallerFilePath}:{uowLink.Title.CallerLineNumber})";
 				RegisteredUoWs.Remove(uow.Session.GetHashCode());
-				logger.Debug("UnitOfWork завершил работу. {0}", ActiveUowCountText());
+				logger.Debug("UnitOfWork завершил работу. {0}. {1}.", createdString, ActiveUowCountText());
 			}
 		}
 
