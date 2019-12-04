2d9f862f69338497de5110254be0830d47a29490
ZhuchkovMikhail <Taenga.Zhu@yandex.ru>
2019-12-04 13:51:04 +0300

    Добавил конвертер для uint

--- QSOrmProject/BindingsConverters.cs
+++ QSOrmProject/BindingsConverters.cs
@@ -41,6 +41,25 @@ namespace QSOrmProject
 		}
 	}
 
+	public class UintToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return value?.ToString();
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			if(String.IsNullOrWhiteSpace(value as String))
+				return null;
+
+			if(targetType == typeof(uint?) && UInt32.TryParse(value.ToString(), out uint number))
+				return number;
+
+			return null;
+		}
+	}
+
 	public class GuidToStringConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
