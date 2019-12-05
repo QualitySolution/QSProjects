fc7af365c38593f0644a738316879c0fa6e1990a
Gankov <gav@qsolution.ru>
2019-12-05 17:34:55 +0300

    Реализована базовая функциональность конфигуратора\конструктора EntityEntryViewModel
    Так как обычно таких контролов в диалоге не один, а несколько. И передавать всем ViewModel-ям контролов в качестве внешних зависимостей, надо одно и тоже. Плюс конфигурировать то как будет работать контроль нужно с помощью разных классов, то создание таких View моделей под каждой поле быстро превратится в АД. Поэтому реализован специальный билдер, конструктор, который помогает создавать эти контролы максимально просто. Не указывая ничего лишнего.

---
+++ QS.Project/ViewModels/Control/EEVM/CommonEEVMBuilder.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq.Expressions;
+using QS.DomainModel.Entity;
+using QS.Project.Journal;
+using QS.Project.Journal.EntitySelection;
+
+namespace QS.ViewModels.Control.EEVM
+{
+	public class CommonEEVMBuilder<TBindedEntity, TEntity>
+		where TBindedEntity: class, INotifyPropertyChanged
+		where TEntity: class, IDomainObject
+	{
+		#region Обазательные параметры
+		CommonEEVMBuilderFactory<TBindedEntity> factory;
+		IPropertyBinder<TEntity> PropertyBinder;
+		#endregion
+
+		#region Опциональные компоненты
+		IEntitySelector EntitySelector;
+		IEntityDlgOpener EntityDlgOpener;
+		IEntityAutocompleteSelector EntityAutocompleteSelector;
+		#endregion
+
+		public CommonEEVMBuilder(CommonEEVMBuilderFactory<TBindedEntity> builderFactory, Expression<Func<TBindedEntity, TEntity>> bindedProperty)
+		{
+			this.factory = builderFactory;
+			PropertyBinder = new PropertyBinder<TBindedEntity, TEntity>(factory.BindedEntity, bindedProperty);
+		}
+
+		#region Fluent Config
+
+		public CommonEEVMBuilder<TBindedEntity, TEntity> UseViewModelJournal<TJournalViewModel>()
+			where TJournalViewModel : JournalViewModelBase
+		{
+			EntitySelector = new JournalViewModelSelector<TEntity, TJournalViewModel>(factory.DialogViewModel, factory.UnitOfWork, factory.NavigationManager);
+			return this;
+		}
+
+		public CommonEEVMBuilder<TBindedEntity, TEntity> UseViewModelJournalAndAutocompleter<TJournalViewModel>()
+			where TJournalViewModel : JournalViewModelBase
+		{
+			EntitySelector = new JournalViewModelSelector<TEntity, TJournalViewModel>(factory.DialogViewModel, factory.UnitOfWork, factory.NavigationManager);
+			//FIXME Добавить создание автокомплитера
+			return this;
+		}
+
+		public CommonEEVMBuilder<TBindedEntity, TEntity> UseViewModelDialog<TEntityViewModel>()
+			where TEntityViewModel : ViewModelBase
+		{
+			EntityDlgOpener = new EntityViewModelOpener<TEntityViewModel>(factory.NavigationManager, factory.DialogViewModel);
+			return this;
+		}
+
+		public EntityEntryViewModel<TEntity> Finish()
+		{
+			return new EntityEntryViewModel<TEntity>(PropertyBinder, EntitySelector, EntityDlgOpener, EntityAutocompleteSelector);
+		}
+
+		#endregion
+	}
+}
---
+++ QS.Project/ViewModels/Control/EEVM/IVMDialogEEVMBuilderFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+
+namespace QS.ViewModels.Control.EEVM
+{
+	public interface IVMDialogEEVMBuilderFactory<TBindedEntity>
+	{
+		TBindedEntity BindedEntity { get; }
+		ViewModelBase DialogViewModel { get; }
+		IUnitOfWork UnitOfWork { get; }
+		INavigationManager NavigationManager { get; }
+	}
+}
