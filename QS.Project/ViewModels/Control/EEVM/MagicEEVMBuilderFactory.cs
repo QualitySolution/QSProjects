1478de6292a8a165936849d6e8d10391a02c7e20
Gankov <gav@qsolution.ru>
2019-12-06 10:10:55 +0300

    Добавлена еще одна фабрика, которая сама пытается брать все из диалога сущьности.
    Она не универсальна, и слишком много лишних ограничений накладывает на дженерик тип, поэтому вынесена в отдельный класс.

---
+++ QS.Project/ViewModels/Control/EEVM/MagicEEVMBuilderFactory.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel;
+using QS.DomainModel.Entity;
+using QS.Navigation;
+
+namespace QS.ViewModels.Control.EEVM
+{
+
+	/// <summary>
+	/// Пример класса с более простой фабрикой которая может сама взять данные из EntityTabViewModelBase
+	/// </summary>
+	public class MagicEEVMBuilderFactory<TBindedEntity> : CommonEEVMBuilderFactory<TBindedEntity>
+		where TBindedEntity: class, INotifyPropertyChanged, IDomainObject, new()
+	{
+
+		public MagicEEVMBuilderFactory(EntityTabViewModelBase<TBindedEntity> entityViewModel, INavigationManager navigationManager) :
+			base(entityViewModel, entityViewModel.Entity, entityViewModel.UoW, navigationManager)
+		{
+
+		}
+	}
+}
