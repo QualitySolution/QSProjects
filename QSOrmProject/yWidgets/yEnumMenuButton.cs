d152d70b2832c70a7e9ef61bccd7cb611649f249
ShubArseniy <shub.ar@yandex.ru>
2019-12-17 15:51:55 +0300

    Добавил yEnumMenuButton

---
+++ QSOrmProject/yWidgets/yEnumMenuButton.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel;
+using Gamma.Binding.Core;
+using QSOrmProject;
+
+namespace QS.yWidgets
+{
+	[ToolboxItem(true)]
+	[Category("QS Widgets")]
+	public class yEnumMenuButton : EnumMenuButton
+	{
+		public BindingControler<yEnumMenuButton> Binding { get; private set; }
+
+		public yEnumMenuButton()
+		{
+			Binding = new BindingControler<yEnumMenuButton>(this);
+		}
+	}
+}
