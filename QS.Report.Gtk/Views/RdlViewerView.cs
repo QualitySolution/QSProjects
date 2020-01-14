e17bf0733e191b18a278677cb243ffd74011ea78
Gankov <gav@qsolution.ru>
2020-01-14 15:18:58 +0300

    Реализовано открытие Rdl через ViewModel
    Добавлена библиотека QS.Report.Gtk c реализацией View

---
+++ QS.Report.Gtk/Views/RdlViewerView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using QS.Report.ViewModels;
+
+namespace QS.Report.Views
+{
+	public partial class RdlViewerView : Gtk.Bin
+	{
+		RdlViewerViewModel ViewModel;
+
+		public RdlViewerView(RdlViewerViewModel model)
+		{
+			this.Build();
+			ViewModel = model;
+
+			reportviewer1.DefaultExportFileName = ViewModel.ReportInfo.Title;
+
+			//FIXME Реализовать режим работы с панелью. Видимо нужно делать отдельно новый тип панелей на ViewModel.
+			panelParameters.Visible = false;
+
+			if(ViewModel.ReportInfo.Source != null)
+				reportviewer1.LoadReport(ViewModel.ReportInfo.Source, ViewModel.ReportInfo.GetParametersString(), ViewModel.ReportInfo.ConnectionString, true);
+			else
+				reportviewer1.LoadReport(ViewModel.ReportInfo.GetReportUri(), ViewModel.ReportInfo.GetParametersString(), ViewModel.ReportInfo.ConnectionString, true);
+		}
+
+		protected void OnReportviewer1ReportPrinted(object sender, EventArgs e)
+		{
+			ViewModel.RiseReportPrinted();
+		}
+	}
+}
