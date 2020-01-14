9d246da2805cb9db803144c2535ea6ee4ad46edd
Gankov <gav@qsolution.ru>
2020-01-14 20:59:02 +0300

    Добавлен базовый класы View и ViewModel для просто диалога содержащего Uow и имеющего возможность сохранить какие то данные и диалога редактирования сущности.
    Пока реализовал только базой фукционал, в аналогичных класса с Tdi видимо осталось еще много полезного. Но без конкретных примеров использования, не хочется переносить все подряд. Возможно что-то можно сделать под другому. Будем расширять при необходимости.

---
+++ QS.Project.Gtk/Views/Dialog/DialogViewBase.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using QS.ViewModels.Dialog;
+
+namespace QS.Views.Dialog
+{
+	public abstract class DialogViewBase<TViewModel> : Gtk.Bin
+		where TViewModel : DialogViewModelBase
+	{
+		public TViewModel ViewModel { get; private set; }
+
+		protected DialogViewBase(TViewModel viewModel)
+		{
+			ViewModel = viewModel ?? throw new ArgumentNullException(nameof(viewModel));
+		}
+	}
+}
---
+++ QS.Project/ViewModels/Dialog/EntityDialogViewModelBase.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.ComponentModel;
+using QS.DomainModel.Entity;
+using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Project.Domain;
+using QS.Utilities.Text;
+
+namespace QS.ViewModels.Dialog
+{
+	public class EntityDialogViewModelBase<TEntity> : UowDialogViewModelBase
+		where TEntity : class, IDomainObject, new()
+	{
+		public override IUnitOfWork UoW => UoWGeneric;
+
+		public IUnitOfWorkGeneric<TEntity> UoWGeneric { get; private set; }
+
+		public TEntity Entity => UoWGeneric.Root;
+
+		public EntityDialogViewModelBase(IEntityUoWBuilder uowBuilder, IUnitOfWorkFactory unitOfWorkFactory, INavigationManager navigation) : base(unitOfWorkFactory, navigation)
+		{
+			if(uowBuilder == null) {
+				throw new ArgumentNullException(nameof(uowBuilder));
+			}
+			UoWGeneric = uowBuilder.CreateUoW<TEntity>(unitOfWorkFactory);
+			base.Title = GetDialogNameByEntity();
+			if(Entity is INotifyPropertyChanged propertyChanged)
+				propertyChanged.PropertyChanged += Entity_PropertyChanged;
+		}
+
+		#region Создание имени диалога
+
+		bool isAutoTitle = true;
+
+		void Entity_PropertyChanged(object sender, PropertyChangedEventArgs e)
+		{
+			if(isAutoTitle && (e.PropertyName == "Title" || e.PropertyName == "Name"))
+				base.Title = GetDialogNameByEntity();
+		}
+
+		public override string Title {
+			get => base.Title; 
+			set {
+				base.Title = value;
+				isAutoTitle = false;
+			}
+		}
+
+		private string GetDialogNameByEntity()
+		{
+			AppellativeAttribute names = DomainHelper.GetSubjectNames(Entity);
+
+			if(UoW.IsNew) {
+				if(names != null && !string.IsNullOrWhiteSpace(names.Nominative)) {
+					switch(names.Gender) {
+						case GrammaticalGender.Masculine:
+							return "Новый " + names.Nominative;
+						case GrammaticalGender.Feminine:
+							return "Новая " + names.Nominative;
+						case GrammaticalGender.Neuter:
+							return "Новое " + names.Nominative;
+						default:
+							return "Новый(ая) " + names.Nominative;
+					}
+				}
+			}
+			else {
+				var title = DomainHelper.GetObjectTilte(Entity);
+
+				if(String.IsNullOrWhiteSpace(title) && !String.IsNullOrWhiteSpace(names?.Nominative))
+					title = names.Nominative.StringToTitleCase();
+
+				return title;
+			}
+			return Entity.ToString();
+		}
+
+		#endregion
+	}
+}
