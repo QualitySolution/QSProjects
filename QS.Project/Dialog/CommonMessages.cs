58da3ef17c3d016dd7ee7c44b365dd18d1be6241
Gankov <gav@qsolution.ru>
2020-01-16 09:50:28 +0300

    Добавлен класс с концепцией Di для использования стандартных сообщений.

---
+++ QS.Project/Dialog/CommonMessages.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using QS.DomainModel.Entity;
+using QS.Services;
+
+namespace QS.Dialog
+{
+	public class CommonMessages
+	{
+		private readonly IInteractiveService interactive;
+
+		public CommonMessages(IInteractiveService interactive)
+		{
+			this.interactive = interactive ?? throw new ArgumentNullException(nameof(interactive));
+		}
+
+		/// <summary>
+		/// Выводит вопрос "Перед печатью {0}, необходимо сохранить изменения в {1}. Сохранить?"
+		/// Диалог использует название сущьности в предложном падеже (Prepositional)
+		/// </summary>
+		public bool SaveBeforePrint(Type savingEntity, string whatPrint)
+		{
+			string savingName = "НЕ УКАЗАНО";
+
+			var att = savingEntity.GetCustomAttributes(typeof(AppellativeAttribute), true);
+			if(att.Length > 0) {
+				if(!String.IsNullOrWhiteSpace((att[0] as AppellativeAttribute).Prepositional)) {
+					savingName = (att[0] as AppellativeAttribute).Prepositional;
+				}
+				else {
+					savingName = (att[0] as AppellativeAttribute).Nominative;
+				}
+			}
+
+			string message = String.Format("Перед печатью {0}, необходимо сохранить изменения в {1}. Сохранить?",
+				whatPrint,
+				savingName
+			);
+
+			return interactive.Question(message);
+		}
+	}
+}
