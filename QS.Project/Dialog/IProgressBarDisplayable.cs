5d0052c509aef64aa8902a33e5dac4981ca4f16d
Gankov <gav@qsolution.ru>
2020-01-22 20:01:59 +0300

    Добавлен ProgressWidget на Gtk реализующий IProgressBarDisplayable
    Реализация по больше части перенесена из водовоза.
    Просто реализована на базе виджета чтобы было удобнее использовать и находилось все в одном классе.

---
+++ QS.Project/Dialog/IProgressBarDisplayable.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace QS.Dialog
+{
+	public interface IProgressBarDisplayable
+	{
+		void Start(double maxValue, double minValue = 0, string text = null, double startValue = 0);
+
+		void Update(double curValue);
+
+		void Update(string curText);
+
+		void Add(double addValue = 1, string text = null);
+
+		void Close();
+	}
+}
