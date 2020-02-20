e443ff499009ca68fb62352fabacf3fb32a3f71d
Gankov <gav@qsolution.ru>
2020-02-20 10:56:58 +0300

    Добавил доступность событий через интерфейс.
    + доп описание свойств интрфейса.

--- QS.Project/ViewModels/Control/EEVM/IEntityEntryViewModel.cs
+++ QS.Project/ViewModels/Control/EEVM/IEntityEntryViewModel.cs
@@ -6,23 +6,36 @@ namespace QS.ViewModels.Control.EEVM
 {
 	public interface IEntityEntryViewModel : INotifyPropertyChanged
 	{
+		#region Выбранная сущьность
 		string EntityTitle { get; }
 		object Entity { get; set; }
+		#endregion
 
+		#region События для внешних подписчиков
+		event EventHandler Changed;
+		event EventHandler ChangedByUser;
+		#endregion
+
+		#region Доступность функций View
 		bool SensetiveSelectButton { get; }
 		bool SensetiveCleanButton { get; }
 		bool SensetiveAutoCompleteEntry { get; }
 		bool SensetiveViewButton { get; }
+		#endregion
 
+		#region Команды от View
 		void OpenSelectDialog();
 		void CleanEntity();
 		void OpenViewEntity();
+		#endregion
 
+		#region Автодополнение
 		int AutocompleteListSize { get; set; }
 		void AutocompleteTextEdited(string text);
 		string GetAutocompleteTitle(object node);
 		void SetEntityByNode(object node);
 		event EventHandler<AutocompleteUpdatedEventArgs> AutoCompleteListUpdated;
+		#endregion
 	}
 
 	public class AutocompleteUpdatedEventArgs : EventArgs
