3f59dddaa2d6cd0cccab79efe38e661298c86315
Gankov <gav@qsolution.ru>
2020-03-13 13:46:51 +0300

    Add IdToStringConverter

---
+++ GammaBinding/Binding/Converters/IdToStringConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Globalization;
+
+namespace Gamma.Binding.Converters
+{
+	public class IdToStringConverter : IValueConverter
+	{
+		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			int id = (int)value;
+			if(id > 0)
+				return id.ToString();
+			else
+				return "не определён";
+		}
+
+		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+		{
+			return null;
+		}
+	}
+}
