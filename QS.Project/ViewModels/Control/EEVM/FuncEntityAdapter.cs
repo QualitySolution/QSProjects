654c83efa881a84e71028cf6f8454095d39a3e8b
Gankov <gav@qsolution.ru>
2020-06-01 22:26:45 +0300

    Добавлен адаптер позволяющий загрузить добавить пользовательский загрузчик сусальности через метод.

---
+++ QS.Project/ViewModels/Control/EEVM/FuncEntityAdapter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace QS.ViewModels.Control.EEVM
+{
+	public class FuncEntityAdapter<TEntity> : IEntityAdapter<TEntity>
+	{
+		private readonly Func<object, TEntity> getEntityByNode;
+
+		public FuncEntityAdapter(Func<object, TEntity> getEntityByNode)
+		{
+			this.getEntityByNode = getEntityByNode ?? throw new ArgumentNullException(nameof(getEntityByNode));
+		}
+
+		public TEntity GetEntityByNode(object node)
+		{
+			return getEntityByNode(node);
+		}
+	}
+}
