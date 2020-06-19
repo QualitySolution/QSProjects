58c2cf9bc87f6a881475108988ec2addecec9e8f
Gankov <gav@qsolution.ru>
2020-06-19 19:11:20 +0300

    Переименовал название тестовой вью модели.
    Добавил в нее слово таб. Так как она базаируется на классе с табами, и в водит в заблуждение. Плюс надо освободить мето для аналогичного тестового класса не на базе табов.

---
+++ QS.LibsTest/TestApp/ViewModels/EntityTabViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using NSubstitute;
+using QS.DomainModel.UoW;
+using QS.Project.Domain;
+using QS.Services;
+using QS.Test.TestApp.Domain;
+using QS.ViewModels;
+
+namespace QS.Test.TestApp.ViewModels
+{
+	public class EntityTabViewModel : EntityTabViewModelBase<FullEntity>
+	{
+		public EntityTabViewModel(IEntityUoWBuilder entityUoWBuilder, IUnitOfWorkFactory unitOfWorkFactory, ICommonServices commonServices) : base(entityUoWBuilder, unitOfWorkFactory, commonServices)
+		{
+		}
+	}
+}
