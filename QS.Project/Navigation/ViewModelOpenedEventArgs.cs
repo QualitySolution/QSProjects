9391c8168c3324be179ea2786168ea02eb8901e5
Gankov <gav@qsolution.ru>
2020-06-19 12:08:04 +0300

    В менеджер навигации добавлено глобальное события по открытию viewModel-и

---
+++ QS.Project/Navigation/ViewModelOpenedEventArgs.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using QS.ViewModels;
+
+namespace QS.Navigation
+{
+	public class ViewModelOpenedEventArgs : EventArgs
+	{
+		public ViewModelBase ViewModel { get; private set; }
+		public IPage Page { get; private set; }
+
+		public ViewModelOpenedEventArgs(IPage page)
+		{
+			Page = page;
+			ViewModel = page.ViewModel;
+		}
+
+		public ViewModelOpenedEventArgs(ViewModelBase viewModel)
+		{
+			ViewModel = viewModel;
+		}
+	}
+}
