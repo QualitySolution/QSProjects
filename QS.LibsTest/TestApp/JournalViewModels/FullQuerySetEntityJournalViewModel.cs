da7da107c56c348b07b1a7156dec9280a1fb631c
Gankov <gav@qsolution.ru>
2020-06-29 15:48:07 +0300

    Заменил ViewModel в журнале. В рамках уменьшения ссылок в тестах на ViewModel-и базирующиеся на табах.

--- QS.LibsTest/TestApp/JournalViewModels/FullQuerySetEntityJournalViewModel.cs
+++ QS.LibsTest/TestApp/JournalViewModels/FullQuerySetEntityJournalViewModel.cs
@@ -10,7 +10,7 @@ using QS.Test.TestApp.ViewModels;
 
 namespace QS.Test.TestApp.JournalViewModels
 {
-	public class FullQuerySetEntityJournalViewModel : EntityJournalViewModelBase<Document1, EntityTabViewModel, FullQuerySetDocumentJournalNode>
+	public class FullQuerySetEntityJournalViewModel : EntityJournalViewModelBase<Document1, EntityDialogViewModel, FullQuerySetDocumentJournalNode>
 	{
 		public FullQuerySetEntityJournalViewModel (IUnitOfWorkFactory unitOfWorkFactory, IInteractiveService interactiveService, INavigationManager navigationManager) 
 			: base(unitOfWorkFactory, interactiveService, navigationManager)
