0e99bb7221b441142ff33545b506693316ae41ec
Gankov <gav@qsolution.ru>
2020-06-30 09:03:55 +0300

    Добавил конструктор чтобы класс можно было использовать как полноценный View в тестах.
    Ибо классы создающие вью хотят передавать viewModel при создании инстанса.

--- QS.LibsTest/TestApp/Views/EmptyDialogView.cs
+++ QS.LibsTest/TestApp/Views/EmptyDialogView.cs
@@ -1,9 +1,18 @@
-﻿using System;
+using System;
+using NSubstitute;
+using QS.Test.TestApp.ViewModels;
+using QS.Views.Dialog;
+
 namespace QS.Test.TestApp.Views
 {
 	public partial class EmptyDialogView : Gtk.Bin
 	{
 		public EmptyDialogView()
+		{
+			this.Build();
+		}
+
+		public EmptyDialogView(EmptyDialogViewModel viewModel)
 		{
 			this.Build();
 		}
