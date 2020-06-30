4af7a7b24a16937b6cbce4973cebf0fa537ce393
Gankov <gav@qsolution.ru>
2020-06-30 09:31:00 +0300

    Тест для этого случая, при котором переключение на старую ViewModel не происходило.

---
+++ QS.LibsTest/TestApp/ViewModels/DialogWithEntityUoWBuilderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using QS.Navigation;
+using QS.Project.Domain;
+using QS.ViewModels.Dialog;
+
+namespace QS.Test.TestApp.ViewModels
+{
+	/// <summary>
+	/// Диалог с наличием в конструкторе IEntityUoWBuilder, что позволяет генератору хешей сгенерировать для тестов неши с Id сущьности.
+	/// </summary>
+	public class DialogWithEntityUoWBuilderViewModel : DialogViewModelBase
+	{
+		public DialogWithEntityUoWBuilderViewModel(INavigationManager navigation, IEntityUoWBuilder entityUoWBuilder) : base(navigation)
+		{
+		}
+	}
+}
---
+++ QS.LibsTest/TestApp/Views/DialogWithEntityUoWBuilderView.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using QS.Test.TestApp.ViewModels;
+using QS.Views.Dialog;
+
+namespace QS.Test.TestApp.Views
+{
+	public partial class DialogWithEntityUoWBuilderView : DialogViewBase<DialogWithEntityUoWBuilderViewModel>
+	{
+		public DialogWithEntityUoWBuilderView(DialogWithEntityUoWBuilderViewModel viewModel) : base(viewModel)
+		{
+			this.Build();
+		}
+	}
+}
