386c5979a9358b58c17a6ba9285eb1a4fbbdce0d
Gankov <gav@qsolution.ru>
2020-07-29 17:42:00 +0300

    Добавлен класс и интерфейс позволяющий работать главным потоком Gui
    При этом не быть привязанным к реализации интерфейса.

---
+++ QS.Project.Gtk/Dialog/GtkGuiDispatcher.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Threading;
+using Gtk;
+using QS.Utilities;
+
+namespace QS.Dialog
+{
+	public class GtkGuiDispatcher : IGuiDispatcher
+	{
+		public static Thread GuiThread;
+
+		public GtkGuiDispatcher()
+		{
+		}
+
+		Thread IGuiDispatcher.GuiThread => GuiThread;
+
+		public void RunInGuiTread(System.Action action)
+		{
+			Application.Invoke((sender, e) => action());
+		}
+
+		public void WaitRedraw()
+		{
+			GtkHelper.WaitRedraw();
+		}
+	}
+}
---
+++ QS.Project/Dialog/IGuiDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading;
+
+namespace QS.Dialog
+{
+	public interface IGuiDispatcher
+	{
+		Thread GuiThread { get; }
+		void WaitRedraw();
+		void RunInGuiTread(Action action);
+	}
+}
