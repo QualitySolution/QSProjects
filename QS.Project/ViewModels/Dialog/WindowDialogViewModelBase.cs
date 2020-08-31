af8bdb1e3297f9d1cfbf2c9c32a2b082b3b688f3
Gankov <gav@qsolution.ru>
2020-08-31 08:36:09 +0300

    По умолчанию все же модальный диалог
    Аргументы за:
    - Все таки кажется что это более частый случай использования.
    - Все первые диалоги были модальными. Поэтому проще просто переименовать их класс.

--- QS.Project/ViewModels/Dialog/WindowDialogViewModelBase.cs
+++ QS.Project/ViewModels/Dialog/WindowDialogViewModelBase.cs
@@ -6,7 +6,7 @@ namespace QS.ViewModels.Dialog
 {
 	public abstract class WindowDialogViewModelBase : DialogViewModelBase
 	{
-		public bool IsModal { get; protected set; }
+		public bool IsModal { get; protected set; } = true;
 		public WindowGravity WindowPosition { get; protected set; }
 
 		protected WindowDialogViewModelBase(INavigationManager navigation) : base(navigation)
