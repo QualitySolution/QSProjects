97d4b9ea921afdadeaaebe57774af5e2f57c5341
ShubArseniy <shub.ar@yandex.ru>
2020-09-01 19:14:41 +0300

    Добавлен статический метод получения корневого родителя сущности Почищен код

--- QS.Project/DomainModel/Entity/DomainTreeNodeBase.cs
+++ QS.Project/DomainModel/Entity/DomainTreeNodeBase.cs
@@ -7,27 +7,24 @@ namespace QS.DomainModel.Entity
 	public abstract class DomainTreeNodeBase<TEntity> : PropertyChangedBase, IDomainObject 
 		where TEntity : DomainTreeNodeBase<TEntity>
 	{
-		protected static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+		protected static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
 
 		#region Свойства
 
 		public virtual int Id { get; set; }
 
-		TEntity parent;
-
+		private TEntity parent;
 		[Display(Name = "Родитель")]
 		public virtual TEntity Parent {
-			get { return parent; }
+			get => parent;
 			set {
 				if(NHibernate.NHibernateUtil.IsInitialized(Parent)) {
-					if(parent != null)
-						parent.Childs.Remove((TEntity)this);
+					parent?.Childs.Remove((TEntity)this);
 					if(value != null && value.CheckCircle((TEntity)this)) {
 						logger.Warn("Родитель не назначен, так как возникает зацикливание дерева.");
 						return;
 					}
 				}
-
 				SetField(ref parent, value, () => Parent);
 
 				if(parent != null && NHibernate.NHibernateUtil.IsInitialized(Parent))
@@ -35,12 +32,11 @@ namespace QS.DomainModel.Entity
 			}
 		}
 
-		IList<TEntity> childs = new List<TEntity>();
-
+		private IList<TEntity> childs = new List<TEntity>();
 		[Display(Name = "Дочерние элементы")]
 		public virtual IList<TEntity> Childs {
-			get { return childs; }
-			set { SetField(ref childs, value, () => Childs); }
+			get => childs;
+			set => SetField(ref childs, value, () => Childs);
 		}
 
 		#endregion
@@ -59,5 +55,10 @@ namespace QS.DomainModel.Entity
 		}
 
 		#endregion
+		
+		public static TEntity GetRootParent(TEntity entity)
+		{
+			return entity.Parent == null ? entity : GetRootParent(entity.Parent);
+		}
 	}
 }
