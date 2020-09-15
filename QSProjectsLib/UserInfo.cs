12c8d7517203f9afd3ff649270ae0dd5ca51184f
Gankov <gav@qsolution.ru>
2020-09-15 10:49:06 +0300

    Удалил устаревшие методы.

--- QSProjectsLib/UserInfo.cs
+++ QSProjectsLib/UserInfo.cs
@@ -25,82 +25,10 @@ namespace QSProjectsLib
 		public string Name;
 		public Dictionary<string, bool> Permissions;
 
-		[Obsolete ("Используйте конструктор с логином. P.S. В новых версиях экземпляр класса создается автоматически из диалога входа.")]
-		public UserInfo ()
-		{
-
-		}
-
 		internal UserInfo (string login)
 		{
 			this.login = login;
 			LoadUserInfo ();
-		}
-
-		[Obsolete ("Устаревший интерфейс, метод вызывать не нужно, проверка происходит в конструкторе.")]
-		public bool TestUserExistByLogin (bool CreateNotExist)
-		{
-			logger.Info ("Проверка наличия пользователя в базе...");
-			if (loadedInConstructor) {
-				logger.Warn ("Информация о пользователе загружена в момент входа, это старый метод, его вызов не требуется.");
-				return true;
-			}
-			try {
-				QSMain.CheckConnectionAlive ();
-
-				bool Exist = false;
-				string sql = "SELECT COUNT(*) AS cnt FROM users WHERE login = @login";
-
-				MySqlCommand cmd = new MySqlCommand (sql, QSMain.connectionDB);
-				cmd.Parameters.AddWithValue ("@login", login);
-
-				using (var rdr = cmd.ExecuteReader ()) {
-					rdr.Read ();
-					Exist = (rdr ["cnt"].ToString () != "0");
-				}
-				
-				if (CreateNotExist && !Exist) {
-					CreateUserRow ();
-					Exist = true;
-				}
-				return Exist;
-			} catch (Exception ex) {
-				logger.ErrorException ("Ошибка проверки пользователя!", ex);
-				return false;
-			}
-		}
-
-		[Obsolete ("Устаревший интерфейс, метод вызывать не нужно! загрузка инфы происходит в конструкторе.")]
-		public void UpdateUserInfoByLogin ()
-		{
-			logger.Info ("Чтение информации о пользователе...");
-			if (loadedInConstructor) {
-				logger.Warn ("Информация о пользовтеле загружена в момент входа, вызов функции не требуется при старте программы. Если нужно обновить информацию используйте метод LoadUserInfo");
-				return;
-			}
-			try {
-				string sql = "SELECT * FROM users WHERE login = @login";
-				QSMain.CheckConnectionAlive ();
-				MySqlCommand cmd = new MySqlCommand (sql, QSMain.connectionDB);
-				cmd.Parameters.AddWithValue ("@login", login);
-				using (var rdr = cmd.ExecuteReader ()) {
-					rdr.Read ();
-
-					Name = rdr ["name"].ToString ();
-					Id = rdr.GetInt32 ("id");
-					Admin = rdr.GetBoolean (QSMain.AdminFieldName);
-
-					Permissions = new Dictionary<string, bool> ();
-					foreach (KeyValuePair<string, UserPermission> Right in QSMain.ProjectPermission) {
-						string FieldName = Right.Value.DataBaseName;
-						Permissions.Add (Right.Key, rdr.GetBoolean (FieldName));
-					}
-
-				}
-				Email = tryGetUserEmail ();
-			} catch (Exception ex) {
-				logger.ErrorException ("Ошибка чтения информации о пользователе!", ex);
-			}	
 		}
 
 		private void CreateUserRow ()
