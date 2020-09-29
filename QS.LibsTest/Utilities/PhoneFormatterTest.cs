d2e4f0016fdae18c512e5f2badce1d94e08049ac
Danila <d.PotapovAnt@yandex.ru>
2020-09-29 15:46:50 +0300

    В PhoneFormatter добавлены новые форматы телефонов, класс стал более универсальным

--- QS.LibsTest/Utilities/PhoneFormatterTest.cs
+++ QS.LibsTest/Utilities/PhoneFormatterTest.cs
@@ -6,6 +6,8 @@ namespace QS.Test.Utilities
 	[TestFixture(TestOf = typeof(PhoneEntry))]
 	public class PhoneFormatterTest
 	{
+		#region RussiaOnlyHyphenated
+
 		[Test(Description = "Проверяем что корректно конвертируем номер из других вариантов написания.")]
 		[TestCase("+7(812)3098089")]
 		[TestCase("+7(812)309-80-89")]
@@ -20,8 +22,7 @@ namespace QS.Test.Utilities
 		[TestCase("fjklsj ds +7-8123098089")]
 		public void RussiaOnlyHyphenated_ConvertFromAnotherFormatsTest(string insertPhone)
 		{
-			var phoneFormatter = new PhoneFormatter();
-			phoneFormatter.Format = PhoneFormat.RussiaOnlyHyphenated;
+			var phoneFormatter = new PhoneFormatter(PhoneFormat.RussiaOnlyHyphenated);
 			var result = phoneFormatter.FormatString(insertPhone);
 
 			Assert.That(result, Is.EqualTo("+7-812-309-80-89"));
@@ -30,8 +31,7 @@ namespace QS.Test.Utilities
 		[Test(Description = "Проверяем что обрезаем лишнии цифры в номере.")]
 		public void RussiaOnlyHyphenated_RemoveExtraDigitsTest()
 		{
-			var phoneFormatter = new PhoneFormatter();
-			phoneFormatter.Format = PhoneFormat.RussiaOnlyHyphenated;
+			var phoneFormatter = new PhoneFormatter(PhoneFormat.RussiaOnlyHyphenated);
 			var result = phoneFormatter.FormatString("+700011122334455");
 
 			Assert.That(result, Is.EqualTo("+7-000-111-22-33"));
@@ -40,8 +40,7 @@ namespace QS.Test.Utilities
 		[Test(Description = "Проверяем что при вводе номера курсор останется на последней позиции.")]
 		public void RussiaOnlyHyphenated_CursorOnEndTest()
 		{
-			var phoneFormatter = new PhoneFormatter();
-			phoneFormatter.Format = PhoneFormat.RussiaOnlyHyphenated;
+			var phoneFormatter = new PhoneFormatter(PhoneFormat.RussiaOnlyHyphenated);
 			int pos = 1;
 
 			phoneFormatter.FormatString("8", ref pos);
@@ -59,8 +58,7 @@ namespace QS.Test.Utilities
 		[Test(Description = "Проверяем что курсор при вводе останется межу теми же цифрами что и раньше.")]
 		public void RussiaOnlyHyphenated_CursorKeepLocation1Test()
 		{
-			var phoneFormatter = new PhoneFormatter();
-			phoneFormatter.Format = PhoneFormat.RussiaOnlyHyphenated;
+			var phoneFormatter = new PhoneFormatter(PhoneFormat.RussiaOnlyHyphenated);
 			//Курсор        
 			//+78123098|089
 			int pos = 9;
@@ -72,8 +70,7 @@ namespace QS.Test.Utilities
 		[Test(Description = "Проверяем что курсор при вводе останется межу теми же цифрами что и раньше.")]
 		public void RussiaOnlyHyphenated_CursorKeepLocation2Test()
 		{
-			var phoneFormatter = new PhoneFormatter();
-			phoneFormatter.Format = PhoneFormat.RussiaOnlyHyphenated;
+			var phoneFormatter = new PhoneFormatter(PhoneFormat.RussiaOnlyHyphenated);
 			//Курсор        
 			//881230|98089
 			int pos = 6;
@@ -85,8 +82,7 @@ namespace QS.Test.Utilities
 		[Test(Description = "Проверяем что курсор при вводе останется межу теми же цифрами что и раньше.")]
 		public void RussiaOnlyHyphenated_CursorKeepLocation3Test()
 		{
-			var phoneFormatter = new PhoneFormatter();
-			phoneFormatter.Format = PhoneFormat.RussiaOnlyHyphenated;
+			var phoneFormatter = new PhoneFormatter(PhoneFormat.RussiaOnlyHyphenated);
 			//Курсор        
 			//df r881230|98089
 			int pos = 10;
@@ -94,5 +90,29 @@ namespace QS.Test.Utilities
 			//+7-812-30|9-80-89
 			Assert.That(pos, Is.EqualTo(9));
 		}
+
+		#endregion
+		#region MyRegion
+
+		[Test(Description = "Проверяем что корректно конвертируем номер из других вариантов написания.")]
+		[TestCase("+7(812)3098089")]
+		[TestCase("+7(812)309-80-89")]
+		[TestCase("+7(812) 309 80 89")]
+		[TestCase("+7 812 309 - 80 - 89")]
+		[TestCase("88123098089")]
+		[TestCase("+78123098089")]
+		[TestCase("8-812-309-80-89")]
+		[TestCase("8 812 309-8-089")]
+		[TestCase("'+7-812-309-80-89'")]
+		[TestCase(" \t 8-812-309-80-89")]
+		[TestCase("fjklsj ds +7-8123098089")]
+		public void BracketWithWhitespaceLastTen_ConvertFromAnotherFormatsTest(string insertPhone)
+		{
+			var phoneFormatter = new PhoneFormatter(PhoneFormat.BracketWithWhitespaceLastTen);
+			var result = phoneFormatter.FormatString(insertPhone);
+
+			Assert.That(result, Is.EqualTo("(812) 309 - 80 - 89"));
+		}
+		#endregion
 	}
 }
