f0246f12c92f20bdcfe52d99a82ff67101e7870f
Gankov <gav@qsolution.ru>
2020-11-06 00:26:22 +0300

    Еще несколько тестов на базовые операции сервиса параметров.

--- QS.LibsTest/BaseParameters/ParametersServiceTest.cs
+++ QS.LibsTest/BaseParameters/ParametersServiceTest.cs
@@ -19,7 +19,7 @@ namespace QS.Test.BaseParameters
 			}
 		}
 
-		[Test(Description = "Проверка что не падаем, а возвращаем null если указанного параметра в базе нет.")]
+		[Test(Description = "Проверка что успешно читаем строковое значение из базы.")]
 		public void GetValue_ReturnStringCase()
 		{
 			using (var connection = new SqliteConnection(connectionString)) {
@@ -30,10 +30,53 @@ namespace QS.Test.BaseParameters
 			}
 		}
 		#endregion
+		#region SetValues
+		[Test(Description = "Проверка что можем создать параметр.")]
+		public void SetValue_NewParameter_StringCase()
+		{
+			using (var connection = new SqliteConnection(connectionString)) {
+				MakeTable(connection);
+				dynamic parameters = new ParametersService(connection);
+				parameters.StringParameter = "String result";
+				Assert.That(parameters.StringParameter, Is.EqualTo("String result"));
+				var inDB = connection.ExecuteScalar<string>(sqlSelect, new { name = "StringParameter" });
+				Assert.That(inDB, Is.EqualTo("String result"));
+			}
+		}
 
+		[Test(Description = "Проверка что можем обновить значение параметра.")]
+		public void SetValue_UpdateParameter_StringCase()
+		{
+			using (var connection = new SqliteConnection(connectionString)) {
+				MakeTable(connection);
+				connection.Execute(sqlInsert, new { name = "StringParameter", str_value = "String result" });
+				dynamic parameters = new ParametersService(connection);
+				parameters.StringParameter = "String2";
+				Assert.That(parameters.StringParameter, Is.EqualTo("String2"));
+				var inDB = connection.ExecuteScalar<string>(sqlSelect, new { name = "StringParameter" });
+				Assert.That(inDB, Is.EqualTo("String2"));
+			}
+		}
+
+		[Test(Description = "Проверка что можем удалить параметр установил значение в null.")]
+		public void SetValue_RemoveParameter_StringCase()
+		{
+			using (var connection = new SqliteConnection(connectionString)) {
+				MakeTable(connection);
+				connection.Execute(sqlInsert, new { name = "StringParameter", str_value = "String result" });
+				dynamic parameters = new ParametersService(connection);
+				parameters.StringParameter = null;
+				Assert.That(parameters.StringParameter, Is.Null);
+				var rows = connection.Execute(sqlSelect, new { name = "StringParameter" });
+				Assert.That(rows, Is.EqualTo(0));
+			}
+		}
+
+		#endregion
 		#region Утилиты
 		const string connectionString = "Data Source=:memory:;Version=3;New=True;";
 		const string sqlInsert = "INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);";
+		const string sqlSelect = "SELECT str_value FROM base_parameters WHERE name = @name;";
 
 		void MakeTable(SqliteConnection connection)
 		{
