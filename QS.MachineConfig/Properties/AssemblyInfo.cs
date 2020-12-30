e28e01ee45153b9fbc21a2ef144c717045dcba70
ShubArseniy <shub.ar@yandex.ru>
2020-12-30 10:30:32 +0300

    Добавлен нестатический вариант MachineConfig. Переименовал проект QSMachineConfig в QS.MachineConfig

--- QS.MachineConfig/Properties/AssemblyInfo.cs
+++ QS.MachineConfig/Properties/AssemblyInfo.cs
@@ -4,7 +4,7 @@ using System.Runtime.CompilerServices;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle ("QSMachineConfig")]
+[assembly: AssemblyTitle ("QS.MachineConfig")]
 [assembly: AssemblyDescription ("")]
 [assembly: AssemblyConfiguration ("")]
 [assembly: AssemblyCompany ("Quality Solution")]
