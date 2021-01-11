1367e25078b95ea7c210d653c0065e8f5be128b6
Сергей <efimov90@gmail.com>
2021-01-11 11:58:51 +0300

    Добавлен аттрибут для фильтрации истории изменений

---
+++ QS.HistoryLog/Attributes/RestrictedHistoryProperty.cs
@@ -0,0 +1,9 @@
+﻿namespace QS.HistoryLog.Attributes
+{
+    /// <summary>
+    /// Аттрибут указывающий на необходимость проверки разрешений на просмотр обновлений свойства объекта
+    /// </summary>
+    public class RestrictedHistoryProperty : System.Attribute
+    {
+    }
+}
