f2a7f8fc254fa0b8846c3be5c7e0ff8257310377
Gankov <gav@qsolution.ru>
2021-02-19 13:52:59 +0300

    Добавил виджет таблицы с биндингом.

---
+++ GammaBinding/GtkWidgets/yTable.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Gamma.Binding.Core;
+using Gtk;
+
+namespace Gamma.GtkWidgets
+{
+	[System.ComponentModel.ToolboxItem(true)]
+	[System.ComponentModel.Category("Gamma Gtk")]
+	public class yTable : Table
+	{
+		public BindingControler<yTable> Binding { get; private set; }
+
+		public yTable(uint rows, uint columns, bool homogeneous) : base(rows, columns, homogeneous)
+		{
+			Binding = new BindingControler<yTable>(this);
+		}
+	}
+}
