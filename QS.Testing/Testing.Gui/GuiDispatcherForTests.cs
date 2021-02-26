78dcac10b875779d8d85533e83bfb1a0ef077f33
Gankov <gav@qsolution.ru>
2021-02-26 16:46:24 +0300

    Добавлен класс GuiDispatcher для позволяющий тестировать ViewModel требующие в зависимости интерфейс IGuiDispatcher

---
+++ QS.Testing/Testing.Gui/GuiDispatcherForTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Threading;
+using QS.Dialog;
+
+namespace QS.Testing.Gui
+{
+	public class GuiDispatcherForTests : IGuiDispatcher
+	{
+
+		public Thread GuiThread => throw new NotImplementedException();
+
+		public void RunInGuiTread(Action action)
+		{
+			//Для тестирования ViewModel команды можно выполнять в том же потоке.
+			//FIXME для тестирования View необходимо реализовать получение потока в котором нужно выполнять команды.
+			action();
+		}
+
+		public void WaitInMainLoop(Func<bool> checkStop, uint sleepMilliseconds = 20)
+		{
+			throw new NotImplementedException();
+		}
+
+		public void WaitRedraw()
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
