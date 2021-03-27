0c7197858ca526a46c86f638e95092d73811e1c7
Gankov <gav@qsolution.ru>
2021-03-27 14:16:18 +0300

    Добавлена возможность получить информацию является ли база данных демонстрационной
    Пока достаточно кривое и не универсальное решение. Реализовано до момента более кардинальных изменений в демонстрационной версии.

--- QS.Project/Project.DB/NhDataBaseInfo.cs
+++ QS.Project/Project.DB/NhDataBaseInfo.cs
@@ -9,9 +9,12 @@ namespace QS.Project.DB
 			//Наверно фиговый способ получить имя базы. Но ничего лучше не придумал.
 			var session = OrmConfig.OpenSession();
 			Name = session.Connection.Database;
+			IsDemo = session.Connection.DataSource == "demo.qsolution.ru";
 			session.Close();
 		}
 
 		public string Name { get; private set; }
+
+		public bool IsDemo { get; private set; }
 	}
 }
--- QS.Project/Project.Versioning/IDataBaseInfo.cs
+++ QS.Project/Project.Versioning/IDataBaseInfo.cs
@@ -3,6 +3,15 @@ namespace QS.Project.Versioning
 {
 	public interface IDataBaseInfo
 	{
+		/// <summary>
+		/// Возвращает реальное имя базы данных на сервере.
+		/// </summary>
+		/// <value>The name.</value>
 		string Name { get; }
+
+		/// <summary>
+		/// Возвращаяет <see langword="true"/> если подключены к демонстрационной базе данных.
+		/// </summary>
+		bool IsDemo { get; }
 	}
 }
