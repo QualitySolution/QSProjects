96c57df608604593a95bbb617091a89e03c3ab12
Gankov <gav@qsolution.ru>
2021-05-20 16:31:19 +0300

    Конвертер GuidToStringConverter перемещен в библиотеку Gamma

--- QSOrmProject/BindingsConverters.cs
+++ QSOrmProject/BindingsConverters.cs
@@ -62,32 +62,6 @@ namespace QSOrmProject
 		}
 	}
 
-	public class GuidToStringConverter : IValueConverter
-	{
-		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			if(value == null)
-				return null;
-
-			return ((Guid)value).ToString() ;
-		}
-
-		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
-		{
-			if(targetType == typeof(Guid)) {
-				return new Guid((string)value);
-			}
-
-			if(targetType == typeof(Guid?)) {
-				if(String.IsNullOrWhiteSpace((string)value))
-					return null;
-				return new Guid((string)value);
-			}
-
-			return null;
-		}
-	}
-
 	public class MultiplierToPercentConverter : IValueConverter
 	{
 		public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
