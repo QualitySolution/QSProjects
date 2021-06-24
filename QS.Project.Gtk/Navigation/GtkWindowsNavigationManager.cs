de8ec53f8cf588ac12d7f920e5e9f19696061c4f
Gankov <gav@qsolution.ru>
2021-06-24 15:10:38 +0300

    Переделал GtkWindowsNavigationManager для того чтобы работали обычный view модели.

--- QS.Project.Gtk/Navigation/GtkWindowsNavigationManager.cs
+++ QS.Project.Gtk/Navigation/GtkWindowsNavigationManager.cs
@@ -56,18 +56,19 @@ namespace QS.Navigation
 			page.ViewModel.NavigationManager = this;
 			pages.Add(page);
 			var gtkPage = (IGtkWindowPage)page;
-			WindowDialogViewModelBase viewModel = (WindowDialogViewModelBase)page.ViewModel;
+			WindowDialogViewModelBase windowDialog = page.ViewModel as WindowDialogViewModelBase;
 			var gtkMasterPage = (IGtkWindowPage)masterPage;
 			gtkPage.GtkView = viewResolver.Resolve(page.ViewModel);
 			if(gtkPage.GtkView == null)
 				throw new InvalidOperationException($"View для {page.ViewModel.GetType()} не создано через {viewResolver.GetType()}.");
-			gtkPage.GtkDialog = new Gtk.Dialog(gtkPage.ViewModel.Title, 
-				viewModel.IsModal ? gtkMasterPage?.GtkDialog : null, 
-				viewModel.IsModal ? DialogFlags.Modal : DialogFlags.DestroyWithParent);
+			var isModal = windowDialog?.IsModal ?? false;
+			gtkPage.GtkDialog = new Gtk.Dialog(gtkPage.ViewModel.Title,
+				isModal ? gtkMasterPage?.GtkDialog : null,
+				isModal ? DialogFlags.Modal : DialogFlags.DestroyWithParent);
 			var defaultsize = gtkPage.GtkView.GetType().GetAttribute<WindowSizeAttribute>(true);
 			gtkPage.GtkDialog.SetDefaultSize(defaultsize?.DefaultWidth ?? gtkPage.GtkView.WidthRequest, defaultsize?.DefaultHeight ?? gtkPage.GtkView.WidthRequest);
 			gtkPage.GtkDialog.VBox.Add(gtkPage.GtkView);
-			if(viewModel.EnableMinimizeMaximize)
+			if(windowDialog?.EnableMinimizeMaximize ?? true)
 				gtkPage.GtkDialog.TypeHint = Gdk.WindowTypeHint.Normal;
 			gtkPage.GtkView.Show();
 			gtkPage.GtkDialog.Show();
