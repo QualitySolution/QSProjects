45e25a094b0ce153230c924235e777ce71eb0f94
Gankov <gav@qsolution.ru>
2021-07-02 11:33:36 +0300

    Превел диалог получения пароля к базе на MVVM
    Перенес его в библиотеку QS.Updater

---
+++ QS.Updater.Core/DBScripts/ViewModels/AdminLoginViewModel.cs
@@ -0,0 +1,44 @@
+using QS.Navigation;
+using QS.ViewModels.Dialog;
+
+namespace QS.DBScripts.ViewModels
+{
+	public class AdminLoginViewModel : WindowDialogViewModelBase
+	{
+		public AdminLoginViewModel(INavigationManager navigation) : base(navigation)
+		{
+			IsModal = true;
+			WindowPosition = Dialog.WindowGravity.Center;
+		}
+
+		#region Свойства
+
+		private string login;
+		public virtual string Login {
+			get => login;
+			set => SetField(ref login, value);
+		}
+
+		private string password;
+		public virtual string Password {
+			get => password;
+			set => SetField(ref password, value);
+		}
+
+		#endregion
+
+		#region Действия
+
+		public void Accept()
+		{
+			Close(false, CloseSource.Save);
+		}
+
+		public void Cancel()
+		{
+			Close(false, CloseSource.Cancel);
+		}
+
+		#endregion
+	}
+}
---
+++ QS.Updater.Gtk/DBScripts.Views/AdminLoginView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using QS.DBScripts.ViewModels;
+using QS.Views.Dialog;
+
+namespace DBScripts.Views
+{
+	public partial class AdminLoginView : DialogViewBase<AdminLoginViewModel>
+	{
+		public AdminLoginView(AdminLoginViewModel viewModel) : base(viewModel)
+		{
+			this.Build();
+			entryLogin.Binding.AddBinding(ViewModel, v => v.Login, w => w.Text).InitializeFromSource();
+			entryPassword.Binding.AddBinding(ViewModel, v => v.Password, w => w.Text).InitializeFromSource();
+		}
+
+		protected void OnButtonOkClicked(object sender, EventArgs e)
+		{
+			ViewModel.Accept();
+		}
+
+		protected void OnButtonCancelClicked(object sender, EventArgs e)
+		{
+			ViewModel.Cancel();
+		}
+
+		protected void OnEntryLoginActivated(object sender, EventArgs e)
+		{
+			this.ChildFocus(Gtk.DirectionType.TabForward);
+		}
+	}
+}
