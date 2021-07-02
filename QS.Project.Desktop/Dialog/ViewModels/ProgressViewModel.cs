c099c3e29ee268ee1d3d5a2ce71770c957e9e68f
Gankov <gav@qsolution.ru>
2021-07-02 17:07:40 +0300

    Добавлен диалог отображения прогресса.

---
+++ QS.Project.Desktop/Dialog/ViewModels/ProgressViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using QS.Navigation;
+using QS.ViewModels.Dialog;
+
+namespace QS.Dialog.ViewModels
+{
+	public class ProgressWindowViewModel : WindowDialogViewModelBase
+	{
+		public ProgressWindowViewModel(INavigationManager navigation) : base(navigation)
+		{
+			IsModal = true;
+			WindowPosition = WindowGravity.Center;
+		}
+
+		public IProgressBarDisplayable Progress { get; set; }
+	}
+}
---
+++ QS.Project.Gtk/Dialog/Views/ProgressWindowView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using QS.Dialog.ViewModels;
+using QS.Views;
+
+namespace QS.Dialog.Views
+{
+	public partial class ProgressWindowView : ViewBase<ProgressWindowViewModel>
+	{
+		public ProgressWindowView(ProgressWindowViewModel viewModel) : base(viewModel)
+		{
+			this.Build();
+			ViewModel.Progress = progresswidget1;
+		}
+	}
+}
