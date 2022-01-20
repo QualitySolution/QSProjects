1e5011114bcb4f79ecc0ccab42edfb071320d23f
ShubArseniy <shub.@yandex.ru>
2022-01-20 15:43:35 +0300

    Добавил проекцию Concat Почистил код, заменил пробелы на табы

--- QS.Project/Project.DB/CustomProjections.cs
+++ QS.Project/Project.DB/CustomProjections.cs
@@ -2,132 +2,181 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
-using Gamma.Utilities;
 using NHibernate;
 using NHibernate.Criterion;
 using NHibernate.Dialect.Function;
 
 namespace QS.Project.DB
 {
-    public static class CustomProjections
-    {
-        public static IProjection Date(params Expression<Func<object>>[] properties)
-        {
-            return Date(properties.Select(Projections.Property).ToArray());
-        }
+	public static class CustomProjections
+	{
+		#region DATE
 
-        public static IProjection Date(params IProjection[] projections)
-        {
-            return Projections.SqlFunction("DATE", NHibernateUtil.Date, projections);
-        }
+		public static IProjection Date(params Expression<Func<object>>[] properties)
+		{
+			return Date(properties.Select(Projections.Property).ToArray<IProjection>());
+		}
 
-        #region GROUP_CONCAT
+		public static IProjection Date(params IProjection[] projections)
+		{
+			return Projections.SqlFunction("DATE", NHibernateUtil.Date, projections);
+		}
 
-        public static IProjection GroupConcat(
-            Expression<Func<object>> expression,
-            bool useDistinct = false,
-            Expression<Func<object>> orderByExpression = null,
-            OrderByDirection orderByDirection = OrderByDirection.Asc,
-            string separator = ",")
-        {
-            if(expression == null) {
-                throw new ArgumentNullException(nameof(expression));
-            }
+		#endregion
 
-            return GroupConcat(
-                Projections.Property(expression),
-                useDistinct,
-                orderByExpression == null ? null : Projections.Property(orderByExpression),
-                orderByDirection,
-                separator
-            );
-        }
+		#region ABS
 
-        public static IProjection GroupConcat(
-            IProjection projection,
-            bool useDistinct = false,
-            IProjection orderByProjection = null,
-            OrderByDirection orderByDirection = OrderByDirection.Asc,
-            string separator = ",")
-        {
-            if(projection == null) {
-                throw new ArgumentNullException(nameof(projection));
-            }
-            var separatorProjection = Projections.Constant(separator);
+		public static IProjection Abs(params Expression<Func<object>>[] properties)
+		{
+			return Abs(properties.Select(Projections.Property).ToArray<IProjection>());
+		}
 
-            //DISTINCT + ORDER BY
-            if(useDistinct && orderByProjection != null) {
-                switch(orderByDirection) {
-                    case OrderByDirection.Asc:
-                        return Projections.SqlFunction(
-                            "GROUP_CONCAT_DISTINCT_ORDER_BY_ASC",
-                            NHibernateUtil.String, projection, orderByProjection, separatorProjection);
-                    case OrderByDirection.Desc:
-                        return Projections.SqlFunction("GROUP_CONCAT_DISTINCT_ORDER_BY_DESC",
-                            NHibernateUtil.String, projection, orderByProjection, separatorProjection);
-                    default:
-                        throw new NotSupportedException($"{nameof(OrderByDirection)}.{orderByDirection} is not supported");
-                }
-            }
-            //DISTINCT
-            if(useDistinct) {
-                return Projections.SqlFunction("GROUP_CONCAT_DISTINCT", NHibernateUtil.String, projection, separatorProjection);
-            }
-            //ORDER BY
-            if(orderByProjection != null) {
-                switch(orderByDirection) {
-                    case OrderByDirection.Asc:
-                        return Projections.SqlFunction("GROUP_CONCAT_ORDER_BY_ASC",
-                            NHibernateUtil.String, projection, orderByProjection, separatorProjection);
-                    case OrderByDirection.Desc:
-                        return Projections.SqlFunction("GROUP_CONCAT_ORDER_BY_DESC",
-                            NHibernateUtil.String, projection, orderByProjection, separatorProjection);
-                    default:
-                        throw new NotSupportedException($"{nameof(OrderByDirection)}.{orderByDirection} is not supported");
-                }
-            }
+		public static IProjection Abs(params IProjection[] projections)
+		{
+			var firstProjection = projections.FirstOrDefault();
+			if(firstProjection == null)
+			{
+				throw new ArgumentException(@"В SQL функцию ABS не было передано ни одного параметра", nameof(projections));
+			}
 
-            return Projections.SqlFunction("GROUP_CONCAT", NHibernateUtil.String, projection, separatorProjection);
-        }
+			var returnType = firstProjection.GetTypes(null, null).FirstOrDefault();
+			if(returnType == null)
+			{
+				throw new InvalidOperationException("Не удалось получить возвращаемый тип проекции");
+			}
 
-        #endregion
+			return Projections.SqlFunction("ABS", returnType, projections);
+		}
 
-        public static IProjection Abs(params Expression<Func<object>>[] properties)
-        {
-            return Abs(properties.Select(Projections.Property).ToArray());
-        }
+		#endregion
 
-        public static IProjection Abs(params IProjection[] projections)
-        {
-            var firstProjection = projections.FirstOrDefault();
-            if(firstProjection == null) {
-                throw new ArgumentException(@"В SQL функцию ABS не было передано ни одного параметра", nameof(projections));
-            }
+		#region CONCAT
 
-            var returnType = firstProjection.GetTypes(null, null).FirstOrDefault();
-            if(returnType == null) {
-                throw new InvalidOperationException("Не удалось получить возвращаемый тип проекции");
-            }
+		public static IProjection Concat(params Expression<Func<object>>[] properties)
+		{
+			var projectionList = new List<IProjection>();
+			foreach(var propertyExpr in properties)
+			{
+				switch(propertyExpr.Body)
+				{
+					case MemberExpression _:
+						projectionList.Add(Projections.Property(propertyExpr));
+						break;
+					case ConstantExpression constantExpr:
+						projectionList.Add(Projections.Constant(constantExpr.Value));
+						break;
+					default: throw new ArgumentException($"Invalid property expression {propertyExpr}", nameof(properties));
+				}
+			}
 
-            return Projections.SqlFunction("ABS", returnType, projections);
-        }
+			return Concat(projectionList.ToArray());
+		}
 
-        public static IProjection Concat_WS(string separator = " ", params Expression<Func<object>>[] properties)
-        {
-            return Concat_WS(separator, properties.Select(Projections.Property).ToArray());
-        }
+		public static IProjection Concat(params IProjection[] projections)
+		{
+			return Projections.SqlFunction(new StandardSQLFunction("CONCAT", NHibernateUtil.String), NHibernateUtil.String, projections);
+		}
 
-        public static IProjection Concat_WS(string separator = " ", params IProjection[] projections)
-        {
-            List<IProjection> projectionList = new List<IProjection> {Projections.Constant(separator)};
-            projectionList.AddRange(projections);
-            return Projections.SqlFunction("CONCAT_WS", NHibernateUtil.String, projectionList.ToArray());
-        }
-    }
+		#endregion
 
-    public enum OrderByDirection
-    {
-        Asc,
-        Desc
-    }
+		#region CONCAT_WS
+
+		public static IProjection Concat_WS(string separator = " ", params Expression<Func<object>>[] properties)
+		{
+			return Concat_WS(separator, properties.Select(Projections.Property).ToArray<IProjection>());
+		}
+
+		public static IProjection Concat_WS(string separator = " ", params IProjection[] projections)
+		{
+			List<IProjection> projectionList = new List<IProjection> { Projections.Constant(separator) };
+			projectionList.AddRange(projections);
+			return Projections.SqlFunction("CONCAT_WS", NHibernateUtil.String, projectionList.ToArray());
+		}
+
+		#endregion
+
+		#region GROUP_CONCAT
+
+		public static IProjection GroupConcat(
+			Expression<Func<object>> expression,
+			bool useDistinct = false,
+			Expression<Func<object>> orderByExpression = null,
+			OrderByDirection orderByDirection = OrderByDirection.Asc,
+			string separator = ",")
+		{
+			if(expression == null)
+			{
+				throw new ArgumentNullException(nameof(expression));
+			}
+
+			return GroupConcat(
+				Projections.Property(expression),
+				useDistinct,
+				orderByExpression == null ? null : Projections.Property(orderByExpression),
+				orderByDirection,
+				separator
+			);
+		}
+
+		public static IProjection GroupConcat(
+			IProjection projection,
+			bool useDistinct = false,
+			IProjection orderByProjection = null,
+			OrderByDirection orderByDirection = OrderByDirection.Asc,
+			string separator = ",")
+		{
+			if(projection == null)
+			{
+				throw new ArgumentNullException(nameof(projection));
+			}
+			var separatorProjection = Projections.Constant(separator);
+
+			//DISTINCT + ORDER BY
+			if(useDistinct && orderByProjection != null)
+			{
+				switch(orderByDirection)
+				{
+					case OrderByDirection.Asc:
+						return Projections.SqlFunction(
+							"GROUP_CONCAT_DISTINCT_ORDER_BY_ASC",
+							NHibernateUtil.String, projection, orderByProjection, separatorProjection);
+					case OrderByDirection.Desc:
+						return Projections.SqlFunction("GROUP_CONCAT_DISTINCT_ORDER_BY_DESC",
+							NHibernateUtil.String, projection, orderByProjection, separatorProjection);
+					default:
+						throw new NotSupportedException($"{nameof(OrderByDirection)}.{orderByDirection} is not supported");
+				}
+			}
+			//DISTINCT
+			if(useDistinct)
+			{
+				return Projections.SqlFunction("GROUP_CONCAT_DISTINCT", NHibernateUtil.String, projection, separatorProjection);
+			}
+			//ORDER BY
+			if(orderByProjection != null)
+			{
+				switch(orderByDirection)
+				{
+					case OrderByDirection.Asc:
+						return Projections.SqlFunction("GROUP_CONCAT_ORDER_BY_ASC",
+							NHibernateUtil.String, projection, orderByProjection, separatorProjection);
+					case OrderByDirection.Desc:
+						return Projections.SqlFunction("GROUP_CONCAT_ORDER_BY_DESC",
+							NHibernateUtil.String, projection, orderByProjection, separatorProjection);
+					default:
+						throw new NotSupportedException($"{nameof(OrderByDirection)}.{orderByDirection} is not supported");
+				}
+			}
+
+			return Projections.SqlFunction("GROUP_CONCAT", NHibernateUtil.String, projection, separatorProjection);
+		}
+
+		#endregion
+	}
+
+	public enum OrderByDirection
+	{
+		Asc,
+		Desc
+	}
 }
