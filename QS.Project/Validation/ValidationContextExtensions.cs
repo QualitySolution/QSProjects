ce04c611a82232f2f75f2f23e46baa1b931a6394
ShubArseniy <shub.@yandex.ru>
2022-01-20 15:45:05 +0300

    Добавил удобный extension метод для получение сервиса в валидации

---
+++ QS.Project/Validation/ValidationContextExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace QS.Validation
+{
+	public static class ValidationContextExtensions
+	{
+		public static T GetService<T>(this ValidationContext context) where T : class
+		{
+			return (T)context.GetService(typeof(T));
+		}
+	}
+}
