7fa24d6a1091c7b662015c0414f056e1a95cd447
Gankov <gav@qsolution.ru>
2022-01-27 18:59:40 +0300

    Обновлен год копирайта.

--- QSProjectsLib/Properties/AssemblyInfo.cs
+++ QSProjectsLib/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using QSProjectsLib;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Quality Solution")]
 [assembly: AssemblyProduct("")]
-[assembly: AssemblyCopyright("Quality Solution 2021")]
+[assembly: AssemblyCopyright("Quality Solution 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
