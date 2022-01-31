bc223df21185b8942ac9427210b6785182e260c2
Ильяс <mi@qsulution.ru>
2022-01-31 15:06:56 +0300

    Добавил viewModel

---
+++ QS.HistoryLog.Destop/ViewModels/HistoryViewModel.cs
@@ -0,0 +1,17 @@
+﻿using QS.DomainModel.UoW;
+using QS.Navigation;
+using QS.Validation;
+using QS.ViewModels.Dialog;
+
+namespace QS.HistoryLog.ViewModels
+{
+	public class HistoryViewModel : UowDialogViewModelBase
+	{
+		public HistoryViewModel(IUnitOfWorkFactory unitOfWorkFactory, 
+			INavigationManager navigation, IValidator validator = null, 
+			string UoWTitle = null) : base(unitOfWorkFactory, navigation, validator, UoWTitle)
+		{
+			Title = "Просмотр журнала изменений";
+		}
+	}
+}
