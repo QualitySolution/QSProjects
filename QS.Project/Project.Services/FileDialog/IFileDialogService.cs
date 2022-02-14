3605aba1e2f3f9d9f65e54415ff44c06a7f25945
Vadim Gordeev <enzogord@gmail.com>
2022-02-14 13:47:05 +0300

    Обновлен сервис вызова диалогов работы с файлами

---
+++ QS.Project/Project.Services/FileDialog/IFileDialogService.cs
@@ -0,0 +1,12 @@
+﻿namespace QS.Project.Services.FileDialog
+{
+    public interface IFileDialogService
+	{
+		IDialogResult RunOpenFileDialog();
+		IDialogResult RunOpenFileDialog(DialogSettings dialogSettings);
+		IDialogResult RunSaveFileDialog();
+		IDialogResult RunSaveFileDialog(DialogSettings dialogSettings);
+		IDialogResult RunOpenDirectoryDialog();
+		IDialogResult RunOpenDirectoryDialog(DialogSettings dialogSettings);
+	}
+}
