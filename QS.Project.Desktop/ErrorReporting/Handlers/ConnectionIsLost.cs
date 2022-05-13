f482b7d6a5dc51cdc2c2bcae63ad8a27498f6bbe
Gankov <gav@qsolution.ru>
2022-05-13 16:20:54 +0300

    Добавлен перехватчик обрабатывающий 2 варианта проблем с сетью
    Выводим пользователю короткое сообщение об ошибке и в фоне отправляем ошибку к нам на сервер для подсчета количества таких ошибок.

---
+++ QS.Project.Desktop/ErrorReporting/Handlers/ConnectionIsLost.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+using NHibernate.Exceptions;
+using QS.Dialog;
+
+namespace QS.ErrorReporting.Handlers {
+	public class ConnectionIsLost : IErrorHandler {
+		private readonly IInteractiveMessage interactiveMessage;
+		private readonly IErrorReporter errorReporter;
+
+		public ConnectionIsLost(IInteractiveMessage interactiveMessage, IErrorReporter errorReporter) {
+			this.interactiveMessage = interactiveMessage ?? throw new ArgumentNullException(nameof(interactiveMessage));
+			this.errorReporter = errorReporter;
+		}
+
+		public bool Take(Exception exception) {
+			var sEx = exception.FindExceptionTypeInInner<SocketException>();
+			if(sEx != null) {
+				interactiveMessage.ShowMessage(ImportanceLevel.Error, 
+					sEx.Message + "\n\nУбедитесь что сеть или интернет работают и повторите попытку. Если соединение восстановить " +
+					"не удасться, обратитесь к вашему системному администратору.",
+					"Ошибка сети");
+				errorReporter?.SendReport(exception, ErrorType.Known);
+				return true;
+			}
+
+			var timeoutException = exception.FindExceptionTypeInInner<TimeoutException>();
+			if(timeoutException?.Source == "MySql.Data") {
+				interactiveMessage.ShowMessage(ImportanceLevel.Error, 
+					timeoutException.Message + "\n\nУбедитесь что сеть или интернет работают и повторите попытку. Если соединение восстановить " +
+					"не удасться, обратитесь к вашему системному администратору.",
+					"Время ожидания истекло");
+				errorReporter?.SendReport(exception, ErrorType.Known);
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
