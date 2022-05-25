be1fb1eaebc3af38bdad48ae89abc5f97dfa85b9
Andrey Shaklein <as1854@yandex.ru>
2022-05-25 21:28:18 +0300

    Revert "4012 Исключаем платные дороги"
    This reverts commit a7636ff08e3693b113c3a6fe6bc6a279fa51ba8c.

--- QS.Osrm/OsrmClient.cs
+++ QS.Osrm/OsrmClient.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 using RestSharp;
@@ -7,17 +8,11 @@ using RestSharp.Serializers.NewtonsoftJson;
 
 namespace QS.Osrm
 {
-    public class OsrmClient
-    {
-        private readonly string _responseOk = "Ok";
-        private readonly string _urlSegmentPoints = "points";
-        private readonly string _queryParameteExclude = "exclude";
-        private readonly string _queryParameteOverview = "overview";
-        private readonly string _queryParameteAlternatives = "alternatives";
+	public class OsrmClient
+	{
+		private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger ();
 
-        private static readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
-
-        public string ServerUrl { get; set; }
+		public string ServerUrl { get; set; }
 
         public OsrmClient()
         {
@@ -28,83 +23,51 @@ namespace QS.Osrm
             ServerUrl = serverUrl;
         }
 
-        /// <summary>
-        /// Построить маршрут через точки
-        /// </summary>
-        /// <param name="routePOIs">Список точек по которым строим маршрут</param>
-        /// <param name="alt">По умолчанию выключено (ЧТО ЭТО?)</param>
-        /// <param name="geometry">По умолчанию выключено (ЧТО ЭТО?)</param>
-        /// <param name="excludeToll">Исключить из маршрута платные дороги</param>
-        /// <returns></returns>
-        public RouteResponse GetRoute(List<PointOnEarth> routePOIs, bool alt = false, GeometryOverview geometry = GeometryOverview.False, bool excludeToll = false)
-        {
-            var requestTask = GetRouteAsync(routePOIs, alt, geometry, excludeToll);
-            requestTask.Wait();
-            return requestTask.Result;
-        }
+		public RouteResponse GetRoute(List<PointOnEarth> routePOIs, bool alt = false, GeometryOverview geometry = GeometryOverview.False)
+		{
+			var requestTask = GetRouteAsync(routePOIs, alt, geometry);
+			requestTask.Wait();
+			return requestTask.Result;
+		}
 
-		/// <summary>
-		/// Построить маршрут через точки
-		/// </summary>
-		/// <param name="routePOIs">Список точек по которым строим маршрут</param>
-		/// <param name="alt">Показывать альтернативныe маршруты. По умолчанию выключено</param>
-		/// <param name="geometry">Возвращать маршруть по точкам. По умолчанию выключено</param>
-		/// <param name="excludeToll">Исключить из маршрута платные дороги</param>
-		/// <returns></returns>
-		public async Task<RouteResponse> GetRouteAsync(List<PointOnEarth> routePOIs, bool alt = false, GeometryOverview geometry = GeometryOverview.False, bool excludeToll = false)
-        {
-            if (routePOIs.Count < 2)
-            {
-                throw new ArgumentException("Список точке для прокладки маршрута, должен содержать хотя бы 2 точки.", nameof(routePOIs));
-            }
-            var points = ConvertPointsToString(routePOIs);
-            var request = new RestRequest("/route/v1/car/{points}", Method.GET);
-            request.AddUrlSegment(_urlSegmentPoints, points);
+		public async Task<RouteResponse> GetRouteAsync(List<PointOnEarth> routePOIs, bool alt = false, GeometryOverview geometry = GeometryOverview.False)
+		{
+			if (routePOIs.Count < 2)
+				throw new ArgumentException("Список точке для прокладки маршрута, должен содержать хотя бы 2 точки.", nameof(routePOIs));
 
-            if (alt)
-            {
-                request.AddQueryParameter(_queryParameteAlternatives, "true");
-            }
-            if (geometry != GeometryOverview.Simplified)
-            {
-                request.AddQueryParameter(_queryParameteOverview, geometry.ToString().ToLower());
-            }
-            if (excludeToll)
-            {
-                request.AddQueryParameter(_queryParameteExclude, "toll");
-            }
+			DateTime startTime = DateTime.Now;
 
-            var client = new RestClient(ServerUrl);
-            client.UseNewtonsoftJson();
-            var response = await client.ExecuteAsync<RouteResponse>(request);
+			var client = new RestClient(ServerUrl);
+			client.UseNewtonsoftJson();
 
-            Logging(response, routePOIs);
+			var request = new RestRequest("/route/v1/car/{points}", Method.GET);
 
-            return response.Data;
-        }
+			var points = String.Join(";", routePOIs.Select(point => String.Format(CultureInfo.InvariantCulture, "{0},{1}", point.Longitude, point.Latitude)));
 
-        private void Logging(IRestResponse<RouteResponse> response, List<PointOnEarth> routePOIs)
-        {
-            if (response.Data == null)
-            {
-                logger.Error("Ошибка в обработке запроса к osrm status={0} message={1}", response.ResponseStatus, response.ErrorMessage);
-            }
-            else if (response.Data.Code != _responseOk)
-            {
-                logger.Error("Ошибка при получении маршрута со osrm {0}: {1}", response.Data.Code, response.Data.Message);
-                logger.Debug("Запрошен машрут: {0}", ConvertPointsToString(routePOIs, " -> "));
-                logger.Debug("Полный ответ: {0}", response.Content);
-            }
-            else
-            {
-                DateTime startTime = DateTime.Now;
-                logger.Debug("Полный ответ за {0} сек.: {1}", (DateTime.Now - startTime).TotalSeconds, response.Content);
-            }
-        }
+			request.AddUrlSegment("points", points);
 
-        private string ConvertPointsToString(List<PointOnEarth> routePOIs, string separator = ";")
-        {
-            return String.Join(separator, routePOIs.Select(point => $"{point.Longitude},{point.Latitude}"));
-        }
-    }
+			if (alt) // По умолчанию выключено
+				request.AddQueryParameter("alternatives", "true");
+
+			if (geometry != GeometryOverview.Simplified) // По умолчанию включено simplified
+				request.AddQueryParameter("overview", geometry.ToString().ToLower());
+
+			var response = await client.ExecuteAsync<RouteResponse>(request);
+			if (response.Data == null)
+			{
+				logger.Error("Ошибка в обработке запроса к osrm status={0} message={1}", response.ResponseStatus, response.ErrorMessage);
+			}
+			else if (response.Data.Code != "Ok")
+			{
+				logger.Error("Ошибка при получении маршрута со osrm {0}: {1}", response.Data.Code, response.Data.Message);
+				logger.Debug("Запрошен маршрут: {0}", String.Join(" -> ", routePOIs.Select(point => String.Format(CultureInfo.InvariantCulture, "{0},{1}", point.Latitude, point.Longitude))));
+				logger.Debug($"Полный ответ: {response.Content}");
+			}
+			else
+			{
+				logger.Debug($"Полный ответ за {(DateTime.Now - startTime).TotalSeconds} сек.: {response.Content}");
+			}
+			return response.Data;
+		}
+	}
 }
