90238e2c031cd6c684722b4b2e6223686701ae80
Ilyas <neugolok@outlook.com>
2022-06-23 13:25:21 +0500

    Для регистрации изменёных объектов

---
+++ QS.Project/Tools/IChangeMonitor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using QS.DomainModel.Entity;
+
+namespace QS.Tools {
+	public interface IChangeMonitor
+	{
+		void SubscribeAllChange<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject;
+
+		void SubscribeToDelete<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject;
+
+		void SubscribeToUpdates<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject;
+
+		void SubscribeToCreate<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject;
+		
+		HashSet<int> EntityIds { get; }
+		
+		HashSet<int> IdsDeletedEntities { get; }
+		
+		HashSet<int> IdsUpdateEntities { get; }
+		
+		HashSet<int> IdsCreateEntities { get; }
+	}
+}
---
+++ QS.Project/Tools/NHibernateChangeMonitor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using QS.DomainModel.Entity;
+
+namespace QS.Tools {
+	public class NHibernateChangeMonitor : IChangeMonitor
+	{
+		public void SubscribeAllChange<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject {
+			SubscribeToCreate(criteria);
+			SubscribeToDelete(criteria);
+			SubscribeToUpdates(criteria);
+		}
+
+		public void SubscribeToDelete<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject {
+			throw new NotImplementedException();
+		}
+
+		public void SubscribeToUpdates<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject {
+			throw new NotImplementedException();
+		}
+
+		public void SubscribeToCreate<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject {
+			throw new NotImplementedException();
+		}
+
+		public HashSet<int> EntityIds {
+			get {
+				var allIds = new HashSet<int>(IdsCreateEntities);
+				allIds.UnionWith(IdsDeletedEntities);
+				allIds.UnionWith(IdsUpdateEntities);
+				return allIds;
+			}
+		}
+
+		public HashSet<int> IdsDeletedEntities { get; }
+		public HashSet<int> IdsUpdateEntities { get; }
+		public HashSet<int> IdsCreateEntities { get; }
+
+		private Func<IDomainObject, bool> criteriaSubscribeToDeletion;
+		private Func<IDomainObject, bool> criteriaSubscribeToDeletion;
+		private Func<IDomainObject, bool> criteriaSubscribeToDeletion;
+	}
+}
