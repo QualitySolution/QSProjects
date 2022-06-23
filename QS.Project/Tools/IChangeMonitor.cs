36edfcd4c6bfca42cef8881d31f1e1cf01abf2e6
Ilyas <neugolok@outlook.com>
2022-06-23 16:26:21 +0500

    Продолжил добавлять

--- QS.Project/Tools/IChangeMonitor.cs
+++ QS.Project/Tools/IChangeMonitor.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using QS.DomainModel.Entity;
+using QS.DomainModel.UoW;
 
 namespace QS.Tools {
 	public interface IChangeMonitor
 	{
-		void SubscribeAllChange<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject;
+		void SubscribeAllChange<TDomainObject>(Func<TDomainObject, bool> criteria, IUnitOfWork unitOfWork) where TDomainObject : class, IDomainObject;
 
-		void SubscribeToDelete<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject;
+		void SubscribeToDelete<TDomainObject>(Func<TDomainObject, bool> criteria, IUnitOfWork unitOfWork) where TDomainObject : class, IDomainObject;
 
-		void SubscribeToUpdates<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject;
+		void SubscribeToUpdates<TDomainObject>(Func<TDomainObject, bool> criteria, IUnitOfWork unitOfWork) where TDomainObject : class, IDomainObject;
 
-		void SubscribeToCreate<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject;
+		void SubscribeToCreate<TDomainObject>(Func<TDomainObject, bool> criteria, IUnitOfWork unitOfWork) where TDomainObject : class, IDomainObject;
 		
 		HashSet<int> EntityIds { get; }
 		
--- QS.Project/Tools/NHibernateChangeMonitor.cs
+++ QS.Project/Tools/NHibernateChangeMonitor.cs
@@ -1,26 +1,44 @@
 ﻿using System;
 using System.Collections.Generic;
 using QS.DomainModel.Entity;
+using QS.DomainModel.NotifyChange;
+using QS.DomainModel.UoW;
 
 namespace QS.Tools {
-	public class NHibernateChangeMonitor : IChangeMonitor
+	/// <summary>
+	/// Класс создан для отслеживания изменений полей при редактировании сущностей
+	/// </summary>
+	public class NHibernateChangeMonitor : IChangeMonitor, IDisposable
 	{
-		public void SubscribeAllChange<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject {
-			SubscribeToCreate(criteria);
-			SubscribeToDelete(criteria);
-			SubscribeToUpdates(criteria);
+		public void SubscribeAllChange<TDomainObject>(Func<TDomainObject, bool> criteria, IUnitOfWork unitOfWork) 
+			where TDomainObject : class, IDomainObject {
+			SubscribeToCreate(criteria, unitOfWork);
+			SubscribeToDelete(criteria, unitOfWork);
+			SubscribeToUpdates(criteria, unitOfWork);
 		}
 
-		public void SubscribeToDelete<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject {
-			throw new NotImplementedException();
+		public void SubscribeToDelete<TDomainObject>(Func<TDomainObject, bool> criteria, IUnitOfWork unitOfWork) 
+			where TDomainObject : class, IDomainObject {
+			NotifyConfiguration.Instance.BatchSubscribe(Delete)
+				.IfEntity<TDomainObject>()
+				.AndWhere(criteria)
+				.AndChangeType(TypeOfChangeEvent.Delete);
 		}
 
-		public void SubscribeToUpdates<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject {
-			throw new NotImplementedException();
+		public void SubscribeToUpdates<TDomainObject>(Func<TDomainObject, bool> criteria, IUnitOfWork unitOfWork) 
+			where TDomainObject : class, IDomainObject {
+			NotifyConfiguration.Instance.BatchSubscribe(Update)
+				.IfEntity<TDomainObject>()
+				.AndWhere(criteria)
+				.AndChangeType(TypeOfChangeEvent.Update);
 		}
 
-		public void SubscribeToCreate<TDomainObject>(Func<TDomainObject, bool> criteria) where TDomainObject : IDomainObject {
-			throw new NotImplementedException();
+		public void SubscribeToCreate<TDomainObject>(Func<TDomainObject, bool> criteria, IUnitOfWork unitOfWork) 
+			where TDomainObject : class, IDomainObject {
+			NotifyConfiguration.Instance.BatchSubscribe(Insert)
+				.IfEntity<TDomainObject>()
+				.AndWhere(criteria)
+				.AndChangeType(TypeOfChangeEvent.Insert);
 		}
 
 		public HashSet<int> EntityIds {
@@ -32,12 +50,24 @@ namespace QS.Tools {
 			}
 		}
 
-		public HashSet<int> IdsDeletedEntities { get; }
-		public HashSet<int> IdsUpdateEntities { get; }
-		public HashSet<int> IdsCreateEntities { get; }
+		public HashSet<int> IdsDeletedEntities { get; } = new HashSet<int>();
+		public HashSet<int> IdsUpdateEntities { get; } = new HashSet<int>();
+		public HashSet<int> IdsCreateEntities { get; } = new HashSet<int>();
 
-		private Func<IDomainObject, bool> criteriaSubscribeToDeletion;
-		private Func<IDomainObject, bool> criteriaSubscribeToDeletion;
-		private Func<IDomainObject, bool> criteriaSubscribeToDeletion;
+		private void Delete(EntityChangeEvent[] changeEvents) 
+			=> SaveIds(changeEvents, IdsDeletedEntities);
+
+		private void Insert(EntityChangeEvent[] changeEvents) 
+			=> SaveIds(changeEvents, IdsCreateEntities);
+
+		private void Update(EntityChangeEvent[] changeEvents) 
+			=> SaveIds(changeEvents, IdsUpdateEntities);
+
+		private void SaveIds(EntityChangeEvent[] changeEvents, HashSet<int> ids) {
+			foreach(var changeEvent in changeEvents) {
+			}
+		}
+
+		public void Dispose() => NotifyConfiguration.Instance.UnsubscribeAll(this);
 	}
 }
