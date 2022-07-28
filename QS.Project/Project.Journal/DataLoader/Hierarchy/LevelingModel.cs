477e716079d37a89d4e5ea502f9a3ffcefb49a7b
Vadim Gordeev <enzogord@gmail.com>
2022-07-28 14:22:37 +0300

    Добавлен иерархический загрузчик для журналов

---
+++ QS.Project/Project.Journal/DataLoader/Hierarchy/LevelingModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace QS.Project.Journal.DataLoader.Hierarchy {
+	public class LevelingModel<TNode> {
+		internal List<Func<IEnumerable<TNode>, IList<TNode>>> LevelingFunctions { get; } = new List<Func<IEnumerable<TNode>, IList<TNode>>>();
+		public LevelingModel<TNode> AddNextLevelSource(Func<IEnumerable<TNode>, IList<TNode>> queryFunc) {
+			LevelingFunctions.Add(queryFunc);
+			return this;
+		}
+	}
+}
