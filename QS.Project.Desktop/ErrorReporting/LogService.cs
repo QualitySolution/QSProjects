b26ec81cbf756bab554a9ab20533d3b386c0c47c
Gankov <gav@qsolution.ru>
2022-09-26 12:16:32 +0300

    Исправляем ситуацию с чтением открытого на запись лога.
    Старый вариант в этом месте получал исключение:
    System.IO.IOException: Процесс не может получить доступ к файлу "C:\Users\Андрей\AppData\Local\workwear\workwear.log", так как этот файл используется другим процессом.
       в System.IO.__Error.WinIOError(Int32 errorCode, String maybeFullPath)
       в System.IO.FileStream.Init(String path, FileMode mode, FileAccess access, Int32 rights, Boolean useRights, FileShare share, Int32 bufferSize, FileOptions options, SECURITY_ATTRIBUTES secAttrs, String msgPath, Boolean bFromProxy, Boolean useLongPath, Boolean checkHost)
       в System.IO.FileStream..ctor(String path, FileMode mode, FileAccess access, FileShare share, Int32 bufferSize, FileOptions options, String msgPath, Boolean bFromProxy, Boolean useLongPath, Boolean checkHost)
       в System.IO.StreamReader..ctor(String path, Encoding encoding, Boolean detectEncodingFromByteOrderMarks, Int32 bufferSize, Boolean checkHost)
       в System.IO.File.InternalReadAllText(String path, Encoding encoding, Boolean checkHost)
       в System.IO.File.ReadAllText(String path)
       в QS.ErrorReporting.LogService.GetLog(Nullable`1 rowCount)

--- QS.Project.Desktop/ErrorReporting/LogService.cs
+++ QS.Project.Desktop/ErrorReporting/LogService.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using NLog;
@@ -18,11 +19,11 @@ namespace QS.ErrorReporting
 			}
 			try {
 				if(!rowCount.HasValue) {
-					return File.ReadAllText(logFileName);
+					return WriteSafeReadAllText(logFileName);
 				}
 				
-				var allLines = File.ReadAllLines(logFileName);
-				return String.Join("\n", allLines.Skip(Math.Max(0, allLines.Length - rowCount.Value)));
+				var allLines = WriteSafeReadAllLines(logFileName);
+				return String.Join("\n", allLines.Skip(Math.Max(0, allLines.Count - rowCount.Value)));
 			} catch(Exception ex) {
 				logger.Error(ex, "Не получилось прочитать лог");
 				return "";
@@ -33,6 +34,31 @@ namespace QS.ErrorReporting
 		{
 			var fileTarget = LogManager.Configuration.AllTargets.FirstOrDefault(t => t is FileTarget) as FileTarget;
 			return fileTarget == null ? string.Empty : fileTarget.FileName.Render(new LogEventInfo { Level = LogLevel.Debug });
+		}
+		
+		//Так как стандартный метод ReadAllLines не позволяет читать, открытый на запись файл лога.
+		public List<string> WriteSafeReadAllLines(String path)
+		{
+			using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+			using (var sr = new StreamReader(fileStream))
+			{
+				List<string> file = new List<string>();
+				while (!sr.EndOfStream)
+				{
+					file.Add(sr.ReadLine());
+				}
+
+				return file;
+			}
+		}
+		
+		//Так как стандартный метод ReadAllText не позволяет читать, открытый на запись файл лога.
+		public string WriteSafeReadAllText(String path)
+		{
+			using (var fileStream = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite))
+			using (var sr = new StreamReader(fileStream)) {
+				return sr.ReadToEnd();
+			}
 		}
 	}
 }
