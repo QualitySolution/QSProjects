daee8d444794ba1f1e50586ca8d8739fb2a33a5f
Gankov <gav@qsolution.ru>
2022-11-21 15:59:55 +0300

    Добавлен класс реализующий интерфейс IProgressBarDisplayable который создает модальный диалог с прогрессом.

---
+++ QS.Project.Desktop/Dialog/ModalProgressCreator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using QS.Dialog.ViewModels;
+using QS.Navigation;
+
+namespace QS.Dialog {
+	/// <summary>
+	/// Класс реализует интерфейс IProgressBarDisplayable при этом в момент запуска прогресса, класс автоматически создает модальное
+	/// окно с ходом выполнения операции. По завершении прогресса закрываем модальное окно.
+	/// </summary>
+	public class ModalProgressCreator : IProgressBarDisplayable {
+		private readonly INavigationManager navigator;
+		private IPage<ProgressWindowViewModel> activeProgressPage;
+
+		protected IProgressBarDisplayable Progress => activeProgressPage.ViewModel.Progress;
+
+		public ModalProgressCreator(INavigationManager navigator) {
+			this.navigator = navigator ?? throw new ArgumentNullException(nameof(navigator));
+		}
+		
+		#region IProgressBarDisplayable
+		public void Start(double maxValue = 1, double minValue = 0, string text = null, double startValue = 0) {
+			if(activeProgressPage != null)
+				throw new InvalidOperationException("Прежде чем запускать новый прогресс необходимо остановить старый, вызвав Close().");
+
+			activeProgressPage = navigator.OpenViewModel<ProgressWindowViewModel>(null);
+			Progress.Start(maxValue, minValue, text, startValue);
+		}
+
+		public void Update(double curValue) {
+			Progress.Update(curValue);
+		}
+
+		public void Update(string curText) {
+			Progress.Update(curText);
+		}
+
+		public void Add(double addValue = 1, string text = null) {
+			Progress.Add(addValue, text);
+		}
+
+		public double Value => Progress.Value;
+		public void Close() {
+			Progress.Close();
+			navigator.ForceClosePage(activeProgressPage, CloseSource.External);
+			activeProgressPage = null;
+		}
+		#endregion
+	}
+}
