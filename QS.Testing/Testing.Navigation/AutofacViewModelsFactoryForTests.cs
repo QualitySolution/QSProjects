308618118f257f03750cd26530718bf4df8580ea
Gankov <gav@qsolution.ru>
2023-02-14 12:18:58 +0300

    Добавлен менеджер навигации для использования в тестах.
    Менеджер навигации позволяет не мокать интерфейс навигатора, а работать по сути с полноценным менеджером навигации но урезанным до работы с вью моделями.

---
+++ QS.Testing/Testing.Navigation/AutofacViewModelsFactoryForTests.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac;
+using QS.Testing.Testing.Navigation;
+using QS.ViewModels.Dialog;
+
+namespace QS.Navigation
+{
+	public class AutofacViewModelsFactoryForTests : IViewModelsPageFactory
+	{
+		//Здесь используется ILifetimeScope вместо IContainer, чтобы можно было создать
+		//скопы с замененным менеджером навигации, переопределенным только для отдельного скопа.
+		readonly ILifetimeScope Container;
+
+		public AutofacViewModelsFactoryForTests(ILifetimeScope container)
+		{
+			Container = container;
+		}
+
+		public IPage<TViewModel> CreateViewModelNamedArgs<TViewModel>(DialogViewModelBase master, IDictionary<string, object> ctorArgs, string hash, Action<ContainerBuilder> addingRegistrations = null) where TViewModel : DialogViewModelBase
+		{
+			var scope = addingRegistrations == null ? Container.BeginLifetimeScope() : Container.BeginLifetimeScope(addingRegistrations);
+			var viewmodel = scope.Resolve<TViewModel>(ctorArgs.Select(pair => new NamedParameter(pair.Key, pair.Value)));
+
+			var page = new PageForTests<TViewModel>(viewmodel);
+			page.PageClosed += (sender, e) => scope.Dispose();
+			return page;
+		}
+
+		public IPage<TViewModel> CreateViewModelTypedArgs<TViewModel>(DialogViewModelBase master, Type[] ctorTypes, object[] ctorValues, string hash, Action<ContainerBuilder> addingRegistrations = null) where TViewModel : DialogViewModelBase
+		{
+			var scope = addingRegistrations == null ? Container.BeginLifetimeScope() : Container.BeginLifetimeScope(addingRegistrations);
+			var viewmodel = scope.Resolve<TViewModel>(ctorTypes.Zip(ctorValues, (type, val) => new TypedParameter(type, val)));
+
+			var page = new PageForTests<TViewModel>(viewmodel);
+			page.PageClosed += (sender, e) => scope.Dispose();
+			return page;
+		}
+	}
+}
---
+++ QS.Testing/Testing.Navigation/NavigationManagerForTests.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+using Autofac;
+using QS.Dialog;
+using QS.Navigation;
+using QS.ViewModels.Dialog;
+
+namespace QS.Testing.Testing.Navigation {
+	
+	/// <summary>
+	/// Пока не полная реализация прощенного менеджера навигации предназначенного для использования в тестах.
+	/// Будет наполнять по необходимости.
+	/// </summary>
+	public class NavigationManagerForTests : NavigationManagerBase, INavigationManager {
+		private AutofacViewModelsFactoryForTests viewModelsFactory;
+		
+		public NavigationManagerForTests(ILifetimeScope scope, IInteractiveMessage interactive, IPageHashGenerator hashGenerator = null) : base(interactive, hashGenerator) {
+			viewModelsFactory = new AutofacViewModelsFactoryForTests(scope); //Здесь пока не усложняю с зависимостями.
+		}
+
+		public IPage<TViewModel> FindPage<TViewModel>() where TViewModel : DialogViewModelBase
+			=> pages.OfType<IPage<TViewModel>>().FirstOrDefault();
+
+		public IPage CurrentPage => throw new NotImplementedException();
+
+		public override void SwitchOn(IPage page) {
+			throw new NotImplementedException();
+		}
+
+		public bool AskClosePage(IPage page, CloseSource source = CloseSource.External) {
+			ClosePage(page, source);
+			return true;
+		}
+
+		public void ForceClosePage(IPage page, CloseSource source = CloseSource.External) {
+			ClosePage(page, source);
+		}
+
+		protected override IViewModelsPageFactory GetPageFactory<TViewModel>() {
+			return viewModelsFactory;
+		}
+
+		protected override void OpenSlavePage(IPage masterPage, IPage page) {
+			pages.Add(page);
+		}
+
+		protected override void OpenPage(IPage masterPage, IPage page) {
+			pages.Add(page);
+		}
+	}
+}
