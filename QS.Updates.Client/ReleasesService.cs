d8e98b285633dd29dce714eca712f5a41b8dfb35
Gankov <gav@qsolution.ru>
2023-03-04 15:12:45 +0300

    Добавлен проект клиента для нового сервиса релизов.

---
+++ QS.Updates.Client/ReleasesService.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Linq;
+using Grpc.Core;
+
+namespace QS.Updates
+{
+    public class ReleasesService: IDisposable
+    {
+        private static NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger ();
+        
+        public static string ServiceAddress = "updates.cloud.qsolution.ru";
+        public static int ServicePort = 4203;
+
+        public ReleasesService()
+        {
+        }
+        
+        private Channel? channel;
+        private Channel Channel {
+            get {
+                if(channel == null || channel.State == ChannelState.Shutdown)
+                    channel = new Channel(ServiceAddress, ServicePort, ChannelCredentials.Insecure);
+                if (channel.State == ChannelState.TransientFailure)
+                    channel.ConnectAsync();
+                return channel;
+            }
+        }
+        
+        #region Запросы
+        
+        public VersionInfo[] CheckForUpdates(int productCode, string version, string modification = "", string serial = "")
+        {
+            try
+            {
+                var client = new Releases.ReleasesClient(Channel);
+                var request = new CheckForUpdatesRequest() {
+					ProductCode = productCode,
+					Version = version,
+					Modification = modification,
+					SerialNumber = serial
+                };
+                var response = client.CheckForUpdates(request);
+                return response.Releases.ToArray();
+            } catch (Exception e)
+            {
+                logger.Error(e, "Ошибка при отправке отчета об ошибке");
+                return Array.Empty<VersionInfo>();
+            }
+        }
+        
+        #endregion
+
+        public void Dispose()
+        {
+            channel?.ShutdownAsync();
+        }
+    }
+}
