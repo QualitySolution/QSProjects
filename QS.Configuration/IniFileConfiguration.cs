bbee78207d795e2a3ee0a0db4f519facf66cbb80
Gankov <gav@qsolution.ru>
2023-03-04 23:52:58 +0300

    Создана библиотека QS.Configuration
    Туда перенесен имеющийся ранее интерфейс, добавлена реализация на базе другой библиотеки не Nini так как Nini нет под NetStandart.
    Добавлены тесты проверяющие что конфигурация работает так же как раньше.

---
+++ QS.Configuration/IniFileConfiguration.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using IniParser;
+using IniParser.Model;
+
+namespace QS.Configuration
+{
+	public class IniFileConfiguration : IChangeableConfiguration
+	{
+		private readonly NLog.Logger logger = NLog.LogManager.GetCurrentClassLogger();
+		private readonly FileIniDataParser parser = new FileIniDataParser();
+		private IniData config;
+
+		public readonly string IniFile;
+
+		public IniFileConfiguration(string iniFile)
+		{
+			if(string.IsNullOrWhiteSpace(iniFile)) {
+				throw new ArgumentException("Имя ini файла должно быть указано.", nameof(iniFile));
+			}
+			IniFile = iniFile;
+
+			Reload();
+			config.Configuration.NewLineStr = "\r\n"; //Устанавливаем виндовое окончание строк, так как старый вариант на всех системах сохранял так.
+		}
+
+		/// <summary>
+		/// Получаем или устанавливаем элемент. В ключе секцию указываем через ":"
+		/// Например "Секция:параметр"
+		/// </summary>
+		/// <value>The item.</value>
+		public string this[string key] {
+			get {
+				ParseKey(key, out string section, out string parameter);
+				return config[section][parameter];
+			}
+			set {
+				ParseKey(key, out string section, out string parameter);
+				if(value != null) {
+					if(config[section][parameter] != value) {
+						config[section][parameter] = value;
+						parser.WriteFile(IniFile, config);
+					}
+				}
+				else {
+					if(config.Sections.ContainsSection(section) && config[section].ContainsKey(parameter)) {
+						config[section].RemoveKey(parameter);
+						if(config[section].Count == 0) {
+							config.Sections.RemoveSection(section);
+						}
+						parser.WriteFile(IniFile, config);
+					}
+				}
+			}
+		}
+
+		public void Reload()
+		{
+			if(File.Exists(IniFile)) {
+				config = parser.ReadFile(IniFile, Encoding.UTF8);
+			}
+			else {
+				logger.Warn("Конфигурационный файл {0} не найден.", IniFile);
+				config = new IniData();
+			}
+		}
+
+		#region private
+		private void ParseKey(string key, out string section, out string parameter)
+		{
+			section = null;
+			parameter = null;
+
+			var index = key.IndexOf(':');
+			if(index > 0) {
+				section = key.Substring(0, index);
+				parameter = key.Substring(index + 1);
+			}
+			else
+				parameter = key;
+		}
+		#endregion
+	}
+}
