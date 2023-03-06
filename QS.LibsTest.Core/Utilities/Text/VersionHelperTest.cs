1b6b06e745f6ce3c79b6d2589026907dc93c6bbb
Gankov <gav@qsolution.ru>
2023-03-06 22:22:07 +0300

    Добавил еще 2 варианта работы версиями, преобразуем в короткий формат из компонетов.
    Чтобы не приходилось в разных местах кода собирать версию из компонентов.

--- QS.LibsTest.Core/Utilities/Text/VersionHelperTest.cs
+++ QS.LibsTest.Core/Utilities/Text/VersionHelperTest.cs
@@ -30,5 +30,23 @@ namespace QS.Test.Utilities.Text {
 				
 			return version.VersionToShortString();
 		}
+		
+		[TestCase(1, 2, ExpectedResult = "1.2")]
+		[TestCase(2, 0, ExpectedResult = "2.0")]
+		[TestCase(2,0,1, ExpectedResult = "2.0.1")]
+		[TestCase(2, 0, 0, 6, ExpectedResult = "2.0.0.6")]
+		[TestCase(1,2,3,6, ExpectedResult = "1.2.3.6")]
+		public string VersionToShortString_InputIntPartsCases(int major, int minor, int? build = null, int? revision = null) {
+			return VersionHelper.VersionToShortString(major, minor, build, revision);
+		}
+		
+		[TestCase(1u, 2u, ExpectedResult = "1.2")]
+		[TestCase(2u, 0u, ExpectedResult = "2.0")]
+		[TestCase(2u,0u,1u, ExpectedResult = "2.0.1")]
+		[TestCase(2u, 0u, 0u, 6u, ExpectedResult = "2.0.0.6")]
+		[TestCase(1u,2u,3u,6u, ExpectedResult = "1.2.3.6")]
+		public string VersionToShortString_InputUIntPartsCases(uint major, uint minor, uint? build = null, uint? revision = null) {
+			return VersionHelper.VersionToShortString(major, minor, build, revision);
+		}
 	}
 }
--- QS.Utilities/Text/VersionHelper.cs
+++ QS.Utilities/Text/VersionHelper.cs
@@ -21,5 +21,25 @@ namespace QS.Utilities.Text
 			var ver = Version.Parse(version);
 			return ver.ToString(ver.Revision <= 0 ? (ver.Build <= 0 ? 2 : 3) : 4);
 		}
+		
+		/// <summary>
+		/// Метод собирает версиею из компонентов и преобразует в строковой формат, где первые две цифры версии обязательны даже если они 0.
+		/// Вторая и третья только при условии больше 0.
+		/// </summary>
+		public static string VersionToShortString(int major, int minor, int? build = null, int? revision = null)
+		{
+			var ver = new Version(major, minor, build ?? 0, revision ?? 0);
+			return ver.ToString(ver.Revision <= 0 ? (ver.Build <= 0 ? 2 : 3) : 4);
+		}
+		
+		/// <summary>
+		/// Метод собирает версиею из компонентов и преобразует в строковой формат, где первые две цифры версии обязательны даже если они 0.
+		/// Вторая и третья только при условии больше 0.
+		/// </summary>
+		public static string VersionToShortString(uint major, uint minor, uint? build = null, uint? revision = null)
+		{
+			var ver = new Version((int)major, (int)minor, (int?)build ?? 0, (int?)revision ?? 0);
+			return ver.ToString(ver.Revision <= 0 ? (ver.Build <= 0 ? 2 : 3) : 4);
+		}
 	}
 }
