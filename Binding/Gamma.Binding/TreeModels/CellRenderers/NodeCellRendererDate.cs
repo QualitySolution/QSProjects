20b541e95d8871e7d8a0b8cdbded2cd3d0887eef
Gankov <gav@qsolution.ru>
2023-04-03 17:31:56 +0300

    В ячеку редактирования даты, добавлено отображение календаря.

--- Binding/Gamma.Binding/TreeModels/CellRenderers/NodeCellRendererDate.cs
+++ Binding/Gamma.Binding/TreeModels/CellRenderers/NodeCellRendererDate.cs
@@ -45,6 +45,9 @@ namespace Gamma.GtkWidgets.Cells
 		}
 		
 		protected override void OnEdited(string path, string new_text) {
+			popupWindow?.Destroy();
+			popupWindow = null;
+			
 			var node = GetNodeFunc(path);
 			if(DataPropertyInfo != null && DataPropertyInfo.CanWrite) {
 				if(String.IsNullOrWhiteSpace(new_text)) {
@@ -55,21 +58,102 @@ namespace Gamma.GtkWidgets.Cells
 					if(DateTime.TryParse(new_text, out DateTime date))
 						DataPropertyInfo.SetValue(node, date, null);
 				}
-
 			}
+		}
+
+		private Gtk.Window popupWindow;
+		private Calendar popupCalendar;
+		
+		public override CellEditable StartEditing(Event evnt, Widget widget, string path, Rectangle background_area, Rectangle cell_area, CellRendererState flags) {
+			popupWindow = new Gtk.Window(Gtk.WindowType.Popup);
+			popupCalendar = new Calendar();
+			popupCalendar.ShowAll();
+			popupWindow.Add(popupCalendar);
+			popupWindow.ShowAll();
+			SetPosition((TreeView)widget, popupWindow, background_area, CalendarXAlignment, CalendarYAllocation);
+			
+			return base.StartEditing(evnt, widget, path, background_area, cell_area, flags);
+		}
+
+		protected override void OnEditingCanceled() {
+			popupWindow?.Destroy();
+			popupWindow = null;
+			base.OnEditingCanceled();
 		}
 
 		protected override void OnEditingStarted(CellEditable editable, string path) {
 			if(editable is Entry entry) {
 				entry.Changed += delegate(object sender, EventArgs args) {
-					if(DateTime.TryParse(entry.Text, out DateTime outDate))
+					if(DateTime.TryParse(entry.Text, out DateTime outDate)) {
 						entry.ModifyText(StateType.Normal);
+						if(popupCalendar?.Date != outDate)
+							popupCalendar.Date = outDate;
+					}
 					else
 						entry.ModifyText(StateType.Normal, new Color(255,0,0)); 
 				};
+				if(popupCalendar != null)
+					popupCalendar.DaySelected += (sender, args) => entry.Text = popupCalendar.Date.ToShortDateString();
 			}
 			base.OnEditingStarted(editable, path);
 		}
+		
+		public virtual HorizontalAlignment CalendarXAlignment { get; set; } = HorizontalAlignment.Auto;
+		public virtual VerticalAllocation CalendarYAllocation { get; set; } = VerticalAllocation.Auto;
+		
+		public static void SetPosition(TreeView treeView, Gtk.Window popup, Rectangle boundRectangle, HorizontalAlignment horizontal, VerticalAllocation vertical)
+		{
+			int x, y;
+			int widgetX, widgetY;
+			int maxX, maxY;
+			treeView.ConvertWidgetToTreeCoords(treeView.GdkWindow.Screen.Width, treeView.GdkWindow.Screen.Height, out maxX, out maxY);
+			treeView.GdkWindow.GetOrigin(out widgetX, out widgetY);
+			switch(vertical) {
+				case VerticalAllocation.Top:
+					y = boundRectangle.Top + widgetY - popup.Allocation.Height;
+					break;
+				case VerticalAllocation.Bottom:
+					y = boundRectangle.Bottom + widgetY;
+					break;
+				case VerticalAllocation.Auto:
+				default:
+					y = boundRectangle.Bottom + widgetY;
+					if(maxY < y + popup.Allocation.Height)
+						y = widgetY + boundRectangle.Top - popup.Allocation.Height;
+					break;
+			}
+		
+			switch(horizontal) {
+				case HorizontalAlignment.Left:
+					x = boundRectangle.X + widgetX;
+					break;
+				case HorizontalAlignment.Right:
+					x = boundRectangle.X + widgetX + boundRectangle.Width - popup.Allocation.Width;
+					break;
+				case HorizontalAlignment.Auto:
+				default:
+					x = boundRectangle.X + widgetX;
+					if(maxX < x + popup.Allocation.Width)
+						x = widgetX + boundRectangle.Right - popup.Allocation.Width;
+					break;
+			}
+			treeView.ConvertTreeToWidgetCoords(x,y, out x, out y);
+			popup.Move(x, y);
+		}
+	}
+
+	public enum VerticalAllocation
+	{
+		Auto,
+		Top,
+		Bottom
+	}
+
+	public enum HorizontalAlignment
+	{
+		Auto,
+		Left,
+		Right
 	}
 }
 
