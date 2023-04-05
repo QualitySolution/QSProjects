e2d2d6507021bb7bd33a48019b3b464fc321cc8f
Gankov <gav@qsolution.ru>
2023-04-05 16:17:22 +0300

    Не вызываем эксепшена при невозможности конвертировать значение Guid
    Так как этот конвертер в основном для графики. Более корректное поведение, это возвращать null при невозможности сконвертировать.

--- Binding/Gamma.Binding/Binding/Converters/GuidToStringConverter.cs
+++ Binding/Gamma.Binding/Binding/Converters/GuidToStringConverter.cs
@@ -15,16 +15,11 @@ namespace Gamma.Binding.Converters
 
 		public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
 		{
-			if(targetType == typeof(Guid)) {
-				return new Guid((string)value);
+			if(Guid.TryParse((string)value, out Guid result)) {
+				if(targetType == typeof(Guid) || targetType == typeof(Guid?)) {
+					return result;
+				}
 			}
-
-			if(targetType == typeof(Guid?)) {
-				if(String.IsNullOrWhiteSpace((string)value))
-					return null;
-				return new Guid((string)value);
-			}
-
 			return null;
 		}
 	}
