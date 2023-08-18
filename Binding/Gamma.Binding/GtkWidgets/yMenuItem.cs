ce0ec5fc122f00c3c046091bc3b86100358d897d
Иван <space@qsolution.ru>
2023-08-18 16:16:00 +0300

    Добавил lable

--- Binding/Gamma.Binding/GtkWidgets/yMenuItem.cs
+++ Binding/Gamma.Binding/GtkWidgets/yMenuItem.cs
@@ -1,3 +1,4 @@
+using System;
 using Gtk;
 using Gamma.Binding.Core;
 
@@ -14,6 +15,17 @@ namespace Gamma.GtkWidgets {
 
 		public yMenuItem(string label) : base(label) {
 			Binding = new BindingControler<yMenuItem> (this);
+		}
+
+		public string Label {
+			get => ((Label)Child)?.LabelProp;
+			set {
+				if(Child is Label label)
+					label.LabelProp = value;
+				else
+					throw new InvalidOperationException("Данное свойство можно использовать только если дочерний виджет является Label. Возможно использован пустой конструктор либо задан другой виджет");
+				((Label)Child).LabelProp = value;
+			}
 		}
 	}
 }
