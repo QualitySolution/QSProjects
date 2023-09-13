b50e86fd4c9c36496c4f0bfb774bdf429bc997da
Sergey Efimov <efimov90@gmail.com>
2023-09-13 11:33:44 +0300

    Исправлен цвет
    Дефолтный цвет не является черным, вызывает баг при использовании не стандартной светлой темы.

--- QS.Project.Gtk/Widgets.GtkUI/VehicleRegistrationNumberEntry.cs
+++ QS.Project.Gtk/Widgets.GtkUI/VehicleRegistrationNumberEntry.cs
@@ -16,13 +16,16 @@ namespace QS.Widgets.GtkUI
 		private string _number;
 		private Country _country;
 
-		private static readonly Color _colorBlack = new Color(0, 0, 0);
+		private readonly Color _colorBlack;
 		private static readonly Color _colorRed = new Color(255, 0, 0);
 		private static readonly Color _colorDarkRed = new Color(210, 0, 0);
 
 		public VehicleRegistrationNumberEntry()
 		{
 			Build();
+
+			_colorBlack = Rc.GetStyle(entryNumber).Foreground(StateType.Normal);
+
 			Binding = new BindingControler<VehicleRegistrationNumberEntry>(this,
 				new Expression<Func<VehicleRegistrationNumberEntry, object>>[] { w => w.Number });
 
