f77aadcef81221bd289001ab3a32dabbfdc389b3
Vadim Gordeev <enzogord@gmail.com>
2023-12-26 19:26:04 +0300

    Упрощенная реализация биндинга команды для нескольких компонентов

--- Binding/Gamma.Binding/GtkWidgets/yMenuItem.cs
+++ Binding/Gamma.Binding/GtkWidgets/yMenuItem.cs
@@ -1,12 +1,19 @@
 using System;
 using Gtk;
 using Gamma.Binding.Core;
+using System.Windows.Input;
+using System;
 
 namespace Gamma.GtkWidgets {
 	[System.ComponentModel.ToolboxItem(false)]
 	[System.ComponentModel.Category("Gamma Gtk")]
 
-	public class yMenuItem : MenuItem {
+	public class yMenuItem : MenuItem 
+	{
+
+		private ICommand command;
+		private Func<object> commandArgument;
+
 		public BindingControler<yMenuItem> Binding { get; private set;}
 		
 		public yMenuItem() {
@@ -26,6 +33,34 @@ namespace Gamma.GtkWidgets {
 					throw new InvalidOperationException("Данное свойство можно использовать только если дочерний виджет является Label. Возможно использован пустой конструктор либо задан другой виджет");
 				((Label)Child).LabelProp = value;
 			}
+		}
+		
+		public void BindCommand(ICommand command, Func<object> commandArgument = null) {
+			if(this.command != null) {
+				throw new InvalidOperationException("Биндинг можно настроить только для одной команды");
+			}
+
+			this.command = command;
+			this.commandArgument = commandArgument;
+			command.CanExecuteChanged += CommandCanExecuteChanged;
+			Sensitive = command.CanExecute(commandArgument);
+		}
+
+		protected override void OnActivated() {
+			base.OnActivated();
+
+			if(command != null) {
+				command.Execute(commandArgument);
+			}
+		}
+
+		private void CommandCanExecuteChanged(object sender, EventArgs e) {
+			Sensitive = command.CanExecute(commandArgument);
+		}
+
+		public override void Destroy() {
+			command.CanExecuteChanged -= CommandCanExecuteChanged;
+			base.Destroy();
 		}
 	}
 }
--- Binding/Gamma.Binding/GtkWidgets/yToggleButton.cs
+++ Binding/Gamma.Binding/GtkWidgets/yToggleButton.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Linq.Expressions;
+using System.Windows.Input;
 using Gamma.Binding.Core;
 using Gtk;
 
@@ -9,6 +10,9 @@ namespace Gamma.GtkWidgets
 	[System.ComponentModel.Category("Gamma Gtk")]
 	public class yToggleButton : ToggleButton
 	{
+		private ICommand command;
+		private Func<object> commandArgument;
+
 		public BindingControler<yToggleButton> Binding { get; private set; }
 
 		public yToggleButton()
@@ -21,6 +25,34 @@ namespace Gamma.GtkWidgets
 		protected override void OnToggled() {
 			Binding.FireChange(w => w.Active);
 			base.OnToggled();
+		}
+
+		public void BindCommand(ICommand command, Func<object> commandArgument = null) {
+			if(this.command != null) {
+				throw new InvalidOperationException("Биндинг можно настроить только для одной команды");
+			}
+
+			this.command = command;
+			this.commandArgument = commandArgument;
+			command.CanExecuteChanged += CommandCanExecuteChanged;
+			Sensitive = command.CanExecute(commandArgument);
+		}
+
+		protected override void OnClicked() {
+			base.OnClicked();
+
+			if(command != null) {
+				command.Execute(commandArgument);
+			}
+		}
+
+		private void CommandCanExecuteChanged(object sender, EventArgs e) {
+			Sensitive = command.CanExecute(commandArgument);
+		}
+
+		public override void Destroy() {
+			command.CanExecuteChanged -= CommandCanExecuteChanged;
+			base.Destroy();
 		}
 	}
 }
