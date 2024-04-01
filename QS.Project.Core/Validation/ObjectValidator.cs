2fa0fce44adf372f66e07b0541c84632cf33bfe7
Gankov Andrey <gav@qsolution.ru>
2024-04-01 17:34:16 +0300

    Merge pull request #583 from QualitySolution/feature/FixValidatorDuplicate
    Убраны дубликаты в результатах валидации

--- QS.Project.Core/Validation/ObjectValidator.cs
+++ QS.Project.Core/Validation/ObjectValidator.cs
@@ -8,7 +8,7 @@ namespace QS.Validation
 	public class ObjectValidator : IValidator
 	{
 		private readonly IValidationViewFactory validationViewFactory;
-		private readonly List<ValidationResult> results = new List<ValidationResult>();
+		private readonly HashSet<ValidationResult> results = new HashSet<ValidationResult>();
 
 		public IEnumerable<ValidationResult> Results => results;
 
