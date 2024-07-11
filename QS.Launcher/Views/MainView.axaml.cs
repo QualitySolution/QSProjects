172515ca5739cca58954e9a98545f8abda6b7757
AnCap-60 <assasin149maks@gmail.com>
2024-07-11 16:39:04 +0300

    Creation container transitions, styles in app file.

--- QS.Launcher/Views/MainView.axaml.cs
+++ QS.Launcher/Views/MainView.axaml.cs
@@ -1,4 +1,10 @@
+using Avalonia;
+using Avalonia.Animation;
 using Avalonia.Controls;
+using Avalonia.Media;
+using Avalonia.Threading;
+using System;
+using System.Threading.Tasks;
 
 namespace QS.Launcher.Views;
 
@@ -7,17 +13,38 @@ public partial class MainView : UserControl
     public MainView()
     {
         InitializeComponent();
-
-		//createConnection.IsVisible = false;
     }
 
 	private void ShowCreationView(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
 	{
-		createConnection.IsVisible = true;
+		createConnection.Classes.Remove("invisible");
+		createConnection.IsEnabled = true;
+
+		loginContainer.Classes.Remove("up");
 	}
 
 	private void HideCreationView(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
 	{
-		createConnection.IsVisible = false;
+		createConnection.Classes.Add("invisible");
+		loginContainer.Classes.Add("up");
+
+		DoThingAfterDelay(() => createConnection.IsEnabled = false, 150);
+		//Dispatcher.UIThread.Post(async () =>
+		//{ 
+		//	await Task.Delay(150);
+		//	createConnection.IsEnabled = false;
+		//});
+	}
+
+	public async void HideControlAfterDelay(Control control, int delayMilliseconds)
+	{
+		DoThingAfterDelay(() => control.IsVisible = false, delayMilliseconds);
+	}
+
+	public async void DoThingAfterDelay(Action action, int delayMilliseconds)
+	{
+		var t = Task.Delay(delayMilliseconds);
+
+		action();
 	}
 }
