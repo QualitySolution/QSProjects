dc20570fb53e54cf857f0a694f6fc87e5d2ac231
AnCap-60 <assasin149maks@gmail.com>
2024-08-28 11:50:27 +0300

    Observable для выбранной базы, привязка покдлючения к комманде.

--- QS.Launcher/ViewModels/PageViewModels/DataBasesVM.cs
+++ QS.Launcher/ViewModels/PageViewModels/DataBasesVM.cs
@@ -1,9 +1,11 @@
+using Avalonia.Controls.ApplicationLifetimes;
 using DynamicData.Kernel;
 using QS.DbManagement;
 using QS.Launcher.ViewModels.Commands;
-using QS.Launcher.ViewModels.TypeViewModels;
 using ReactiveUI;
+using System;
 using System.Collections.Generic;
+using System.Reactive.Linq;
 using System.Windows.Input;
 
 namespace QS.Launcher.ViewModels.PageViewModels;
@@ -22,16 +24,35 @@ public class DataBasesVM : CarouselPageVM {
 
 	public List<DbInfo> Databases { get; set; } = [];
 
-	public DatabaseViewModel? SelectedDatabase { get; set; }
+	private DbInfo? selectedDatabase;
+	public DbInfo? SelectedDatabase {
+		get => selectedDatabase;
+		set => this.RaiseAndSetIfChanged(ref selectedDatabase, value);
+	}
 
 	public bool IsAdmin { get; set; } = false;
 
 	public bool ShouldCloseLauncherAfterStart { get; set; } = false;
 
+	public ICommand ConnectCommand { get; }
+
 	public DataBasesVM(NextPageCommand? nextPageCommand, PreviousPageCommand? previousPageCommand,
 		ChangePageCommand? changePageCommand) : base(nextPageCommand, previousPageCommand, changePageCommand)
 	{
-		
+
+		IObservable<bool>? canExecute = this
+			.WhenAnyValue(x => x.SelectedDatabase)
+			.Select(x => x is not null);
+		ConnectCommand = ReactiveCommand.Create(Connect, canExecute);
 	}
 
+	public void Connect() {
+
+		// Connect
+
+		if (ShouldCloseLauncherAfterStart) {
+			var window = (Avalonia.Application.Current.ApplicationLifetime as IClassicDesktopStyleApplicationLifetime)?.MainWindow;
+			window.Close();
+		}
+	}
 }
