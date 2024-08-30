79bcb8b59983ea41cca0aad7e7db0ed80058e85a
AnCap-60 <assasin149maks@gmail.com>
2024-08-30 10:12:08 +0300

    Сохраняем конфигурацию в джсон.

--- QS.Launcher/App.axaml.cs
+++ QS.Launcher/App.axaml.cs
@@ -10,6 +10,8 @@ using QS.Launcher.Views.Pages;
 using System;
 using QS.DbManagement;
 using QS.Cloud.Client;
+using Microsoft.Extensions.Configuration;
+using System.Text.Json;
 
 namespace QS.Launcher;
 
@@ -22,6 +24,10 @@ public partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+		CreateTestConfigs();
+		var c = Configure();
+		var con = c.GetValue<string>("ExampleConnectionInfo:Title");
+
 		var collection = new ServiceCollection();
 
 		collection.AddViewsAndViewModels();
@@ -35,12 +41,49 @@ public partial class App : Application
 		var mainWindow = services.GetRequiredService<MainWindow>();
 		var mainWindowVM = services.GetRequiredService<MainWindowVM>();
 		mainWindow.DataContext = mainWindowVM;
-
+		
 		if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
 			desktop.MainWindow = mainWindow;
 
         base.OnFrameworkInitializationCompleted();
     }
+
+	IConfiguration configurationJson = new ConfigurationBuilder()
+		.AddJsonFile("launcher.json")
+		.Build();
+	IConfiguration configurationEnv = new ConfigurationBuilder()
+		.AddEnvironmentVariables("QS_")
+		.Build();
+
+	public void CreateTestConfigs() {
+		var conSec = configurationJson.GetSection("Connections");
+		string json = JsonSerializer.Serialize(connections);
+		using(var s = new System.IO.FileStream("launcher.json", System.IO.FileMode.OpenOrCreate)) {
+			JsonSerializer.Serialize(s, connections, new JsonSerializerOptions { WriteIndented = true });
+		}
+	}
+
+	ConnectionInfo[] connections = [
+		new QSCloudConnectionInfo {
+			Title = "QS Cloud",
+			IconBytes = System.IO.File.ReadAllBytes(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "qs.ico")),
+			Parameters = [new ConnectionParameter("Логин")]
+		},
+		new MariaDBConnectionInfo {
+			Title = "MariaDB",
+			IconBytes = System.IO.File.ReadAllBytes(System.IO.Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "Assets", "mariadb.png")),
+			Parameters = [new ConnectionParameter("Адрес")]
+		},
+		new ExampleConnectionInfo {
+			Title = "SQLite",
+			IconBytes = null,
+			Parameters = [new ConnectionParameter("База данных"), new ConnectionParameter("Строка подключения")]
+	} ];
+
+	public IConfiguration Configure() {
+
+		return configurationJson.GetSection("Connections");
+	}
 }
 
 public static class ServiceCollectionExtensions {
---
+++ QS.Launcher/LauncherOptions.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QS.Launcher;
+public class LauncherOptions {
+	public string CompanyImageBase64 { get; set; }
+
+	public string CompanyIconBase64 { get; set; }
+
+	public string AppTitle { get; set; }
+}
