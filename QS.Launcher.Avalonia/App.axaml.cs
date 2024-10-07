9521b3467392c269242f9135f5edfd0fb54e8f5b
AnCap-60 <assasin149maks@gmail.com>
2024-10-07 17:01:42 +0300

    Убрал DI из лаунчера

--- QS.Launcher.Avalonia/App.axaml.cs
+++ QS.Launcher.Avalonia/App.axaml.cs
@@ -1,50 +1,26 @@
 using Avalonia;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Markup.Xaml;
-using Microsoft.Extensions.DependencyInjection;
-using QS.DbManagement;
 using QS.Launcher.Views;
-using System.Collections.Generic;
+using System;
 
 namespace QS.Launcher;
 
-public partial class App : Application {
+public partial class App() : Application
+{
+	public Func<MainWindow> MainWindowGetter { get; set; }
+
 	public override void Initialize() {
 		AvaloniaXamlLoader.Load(this);
 	}
 
-	public App(IEnumerable<ConnectionInfo> connectionInfos, LauncherOptions launcherOptions) {
-		this.connectionInfos = connectionInfos;
-		this.launcherOptions = launcherOptions;
-	}
-
-	IEnumerable<ConnectionInfo> connectionInfos;
-
-	LauncherOptions launcherOptions;
-
-	public App() : base() { }
-
 	public override void OnFrameworkInitializationCompleted() {
-		var collection = new ServiceCollection();
-
-		collection
-			.AddPages()
-			.AddViewModels();
-
-
-		collection
-			.AddConnectionTypes(connectionInfos)
-			.AddConnections(connectionInfos, launcherOptions.OldConfigFilename)
-			.AddCompanyDependencies(launcherOptions)
-			.AddInteractive();
-
-		var services = collection.BuildServiceProvider();
-
-		var mainWindow = services.GetRequiredService<MainWindow>();
+		if (MainWindowGetter is null)
+			throw new ArgumentNullException(nameof(MainWindowGetter));
 
 		if(ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
-			desktop.MainWindow = mainWindow;
-
+			desktop.MainWindow = MainWindowGetter();
+		
 		base.OnFrameworkInitializationCompleted();
 	}
 }
