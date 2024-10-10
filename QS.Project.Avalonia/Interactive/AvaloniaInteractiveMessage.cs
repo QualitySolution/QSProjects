75ae67819a23f27cb3a1cd93b7ae80e0cbb6e7aa
Gankov <gav@qsolution.ru>
2024-10-10 18:45:28 +0300

    Не падаем если вызвали показ сообщения из другого потока.

--- QS.Project.Avalonia/Interactive/AvaloniaInteractiveMessage.cs
+++ QS.Project.Avalonia/Interactive/AvaloniaInteractiveMessage.cs
@@ -1,8 +1,9 @@
+using Avalonia.Threading;
 using QS.Dialog;
 
 namespace QS.Project.Avalonia.Interactive;
 public class AvaloniaInteractiveMessage : IInteractiveMessage {
 	public void ShowMessage(ImportanceLevel level, string message, string title = null) {
-		DialogWindow.Show(level, message, title);
+		Dispatcher.UIThread.Post(() => DialogWindow.Show(level, message, title));
 	}
 }
