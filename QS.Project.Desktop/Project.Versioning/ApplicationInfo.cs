fbc59130a61ec218e2dfb07d6adae4b42da00940
Gankov <gav@qsolution.ru>
2024-10-15 18:38:17 +0300

    Добавлена возможность настройки ApplicationInfo через DI

---
+++ QS.Project.Desktop/Project.Versioning/ApplicationInfo.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Reflection;
+
+namespace QS.Project.Versioning
+{
+	/// <summary>
+	/// Класс для получения информации о приложении.
+	/// Вся информация задается через DI в конструкторе, использовать в приложениях на .NET Core.
+	/// </summary>
+	public class ApplicationInfo : IApplicationInfo
+	{
+		public string ProductName => Assembly.GetName().Name;
+		public string ProductTitle => Assembly.GetCustomAttribute<AssemblyTitleAttribute>()?.Title;
+
+		public string Modification { get; set; }
+		public string ModificationTitle { get; set; }
+		public bool ModificationIsHidden { get; set; }
+		public string[] CompatibleModifications {
+			get {
+				throw new NotImplementedException();
+				// var modificationAttributes = Assembly.GetCustomAttributes<AssemblyCompatibleModificationAttribute>();
+				// var list = modificationAttributes.Select(x => x.Name).ToList();
+				// if (!String.IsNullOrWhiteSpace(Modification) && !list.Contains(Modification))
+				// 	list.Add(Modification);
+				// return list.ToArray();
+				}
+			}
+
+		public Version Version => Assembly.GetName().Version;
+
+		public bool IsBeta { get; set; }
+
+		public DateTime? BuildDate => System.IO.File.GetLastWriteTime(Assembly.Location);
+
+		public byte ProductCode { get; set; }
+
+		#region Внутрение
+		private Assembly Assembly => Assembly.GetEntryAssembly();
+		#endregion
+	}
+}
