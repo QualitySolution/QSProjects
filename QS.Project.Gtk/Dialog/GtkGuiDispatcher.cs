970e6e252a49502d2346905ca0a48eab453202bc
Gankov <gav@qsolution.ru>
2024-10-16 12:47:30 +0300

    Удалил из диспетчера проверку на тред.

--- QS.Project.Gtk/Dialog/GtkGuiDispatcher.cs
+++ QS.Project.Gtk/Dialog/GtkGuiDispatcher.cs
@@ -16,10 +16,7 @@ namespace QS.Dialog
 		Thread IGuiDispatcher.GuiThread => GuiThread;
 
 		public void RunInGuiTread(System.Action action) {
-			if(GuiThread == Thread.CurrentThread)
-				action();
-			else
-				Application.Invoke((sender, e) => action());
+			Application.Invoke((sender, e) => action());
 		}
 
 		public void WaitInMainLoop(Func<bool> checkStop, uint sleepMilliseconds = 20)
