000064202a70f5fcb97946f493529c2425bdc902
Gankov <gav@qsolution.ru>
2024-12-12 15:59:00 +0300

    Перенес метод регистрации гуи трекера в десктоп, так как метод не связан с Gtk

---
+++ QS.Project.Desktop/DependencyInjection.cs
@@ -0,0 +1,24 @@
+using Microsoft.Extensions.DependencyInjection;
+using QS.Dialog;
+using QS.DomainModel.Tracking;
+using QS.DomainModel.UoW;
+
+namespace QS.Project {
+	public static class DependencyInjection {
+		/// <summary>
+		/// Метод регистрирует зависимости UoW с трекером изменений для Gui
+		/// В этом случает трекер будет регистрировать изменения из любого потока 
+		/// в UI потоке, что подойдет для GUI приложений
+		/// <para><b>ВАЖНО: Взаимоисключающий с другими методами Add*UoW(services)</b></para>
+		/// </summary>
+		public static IServiceCollection AddGuiTrackedUoW(this IServiceCollection services) {
+			services
+				.AddSingleton<ITrackerActionInvoker, GuiTrackerInvoker>()
+				.AddSingleton<GlobalUowEventsTracker>()
+				.AddTransient<SingleUowEventsTracker>()
+				.AddSingleton<IUnitOfWorkFactory, TrackedUnitOfWorkFactory>()
+				;
+			return services;
+		}
+	}
+}
--- QS.Project.Gtk/DependencyInjection.cs
+++ QS.Project.Gtk/DependencyInjection.cs
@@ -1,8 +1,6 @@
 using Microsoft.Extensions.DependencyInjection;
 using QS.Dialog;
 using QS.Dialog.GtkUI;
-using QS.DomainModel.Tracking;
-using QS.DomainModel.UoW;
 using QS.Project.Services.GtkUI;
 using QS.Validation;
 
@@ -12,22 +10,6 @@ namespace QS.Project.GtkSharp {
 		public static IServiceCollection AddDesktop(this IServiceCollection services) {
 			services
 				.AddSingleton<IGuiDispatcher, GtkGuiDispatcher>()
-				;
-			return services;
-		}
-
-		/// <summary>
-		/// Метод регистрирует зависимости UoW с трекером изменений для Gui
-		/// В этом случает трекер будет регистрировать изменения из любого потока 
-		/// в UI потоке, что подойдет для GUI приложений
-		/// <para><b>ВАЖНО: Взаимоисключающий с другими методами Add*UoW(services)</b></para>
-		/// </summary>
-		public static IServiceCollection AddGuiTrackedUoW(this IServiceCollection services) {
-			services
-				.AddSingleton<ITrackerActionInvoker, GuiTrackerInvoker>()
-				.AddSingleton<GlobalUowEventsTracker>()
-				.AddTransient<SingleUowEventsTracker>()
-				.AddSingleton<IUnitOfWorkFactory, TrackedUnitOfWorkFactory>()
 				;
 			return services;
 		}
