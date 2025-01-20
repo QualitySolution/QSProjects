b183803dc5bb1165d534171bdd6d019abde30cf9
Gankov <gav@qsolution.ru>
2025-01-20 15:04:28 +0300

    2024->2025

--- QS.DocTemplates/Properties/AssemblyInfo.cs
+++ QS.DocTemplates/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("QS.DocTemplates")]
-[assembly: AssemblyCopyright("Quality Solution 2024")]
+[assembly: AssemblyCopyright("Quality Solution 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
