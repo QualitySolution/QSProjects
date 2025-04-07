bb35107756699d4906f9b52431ba83aa241ca652
Gankov <gav@qsolution.ru>
2025-04-07 18:24:20 +0300

    Пропустил изменения в прошлом комите.

--- QS.Project.Desktop/Permissions/SimplePermissionResult.cs
+++ QS.Project.Desktop/Permissions/SimplePermissionResult.cs
@@ -1,5 +1,8 @@
 namespace QS.Permissions {
-	public class SimplePermissionResult {
-		
+	public class SimplePermissionResult : IPermissionResult {
+		public bool CanCreate { get; set; }
+		public bool CanRead { get; set; }
+		public bool CanUpdate { get; set; }
+		public bool CanDelete { get; set; }
 	}
 }
