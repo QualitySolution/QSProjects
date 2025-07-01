0515da442abb9fdb078e08c9bf71f627705263cd
Vadim Gordeev <enzogord@gmail.com>
2025-07-01 22:52:02 +0300

    Доработки событий CleanableEntry

--- Binding/Gamma.Binding/Widgets/CleanableEntry.cs
+++ Binding/Gamma.Binding/Widgets/CleanableEntry.cs
@@ -2,18 +2,37 @@ using System;
 using System.ComponentModel;
 using System.Linq.Expressions;
 using Gamma.Binding.Core;
+using Gtk;
 
 namespace Gamma.Widgets {
 	[ToolboxItem(true)]
 	[Category("Gamma Widgets")]
 	public partial class CleanableEntry : Gtk.Bin {
 		public BindingControler<CleanableEntry> Binding { get; private set; }
+		public event EventHandler Changed;
 
 		public CleanableEntry() {
 			this.Build();
 			Binding = new BindingControler<CleanableEntry>(this, new Expression<Func<CleanableEntry, object>>[] {
 				(w => w.Text)
 			});
+			entry.Changed += EntryChanged;
+			entry.TextInserted += EntryTextInserted;
+			entry.TextDeleted += EntryTextDeleted;
+		}
+
+		private void EntryTextDeleted(object sender, TextDeletedArgs e) {
+			Changed?.Invoke(sender, e);
+			OnEntryChanged(sender, e);
+		}
+
+		private void EntryTextInserted(object sender, TextInsertedArgs e) {
+			Changed?.Invoke(sender, e);
+			OnEntryChanged(sender, e);
+		}
+
+		private void EntryChanged(object sender, EventArgs e) {
+			Changed?.Invoke(sender, e);
 		}
 
 		#region Свойства
@@ -35,5 +54,14 @@ namespace Gamma.Widgets {
 			Text = String.Empty;
 		}
 		#endregion
+
+		protected override void OnDestroyed() {
+			entry.Changed -= EntryChanged;
+			base.OnDestroyed();
+		}
+
+		public void SetColor(StateType stateType, Gdk.Color color) {
+			entry.ModifyText(stateType, color);
+		}
 	}
 }
