c6e40cfefbf1d1a24bd24833234741fccd474313
Art8m <9artbe@gmail.com>
2025-09-30 15:56:15 +0300

    ImportanceLevel в AbortCreatingPageException

--- QS.Project.Desktop/Navigation/AbortCreatingPageException.cs
+++ QS.Project.Desktop/Navigation/AbortCreatingPageException.cs
@@ -1,13 +1,16 @@
 ﻿using System;
+using QS.Dialog;
+
 namespace QS.Navigation
 {
 	public class AbortCreatingPageException : Exception
 	{
 		public string Title;
+		public ImportanceLevel ImportanceLevel { get; }
 
-		public AbortCreatingPageException(string userMessage, string title) : base(userMessage)
-		{
+		public AbortCreatingPageException(string userMessage, string title, ImportanceLevel importanceLevel = ImportanceLevel.Error) : base(userMessage) {
 			Title = title;
+			ImportanceLevel = importanceLevel;
 		}
 	}
 }
