48d39c50412cc5a9c24439881789a32f7fa8ce48
Artem Maksimov <artembereznoj482@gmail.com>
2025-11-17 15:04:55 +0300

    измение типа userId на нулейбл

--- QS.HistoryLog.Sql/Domain/IChangeSetToSave.cs
+++ QS.HistoryLog.Sql/Domain/IChangeSetToSave.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 namespace QS.HistoryLog.Domain {
 	public interface IChangeSetToSave {
 		string ActionName { get; }
-		int UserId { get; }
+		int? UserId { get; }
 		string UserLogin { get; }
 		IEnumerable<IChangedEntityToSave> Entities { get; } 
 	}
--- QS.HistoryLog.Sql/Domain/SimpleChangeSet.cs
+++ QS.HistoryLog.Sql/Domain/SimpleChangeSet.cs
@@ -7,7 +7,7 @@ namespace QS.HistoryLog.Domain
 	/// </summary>
 	public class SimpleChangeSet : IChangeSetToSave {
 		public string ActionName { get; set; }
-		public int UserId { get; set; }
+		public int? UserId { get; set; }
 		public string UserLogin { get; set; }
 		public List<SimpleChangedEntity> Entities { get; } = new List<SimpleChangedEntity>();
 		IEnumerable<IChangedEntityToSave> IChangeSetToSave.Entities => Entities;
