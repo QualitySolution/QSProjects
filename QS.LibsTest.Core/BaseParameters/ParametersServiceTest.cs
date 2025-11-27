4caafce7634f2938b777821e5c4041accf4b79c1
Gankov <gav@qsolution.ru>
2025-11-27 20:58:16 +0300

    Перенес тесты ParametersService на MariaDB контейнеры

--- QS.LibsTest.Core/BaseParameters/ParametersServiceTest.cs
+++ QS.LibsTest.Core/BaseParameters/ParametersServiceTest.cs
@@ -1,31 +1,66 @@
 using System;
+using System.Threading.Tasks;
 using Dapper;
-using Mono.Data.Sqlite;
 using NUnit.Framework;
 using QS.BaseParameters;
+using QS.Testing.DB;
 
 namespace QS.Test.BaseParameters
 {
 	[TestFixture(TestOf = typeof(ParametersService))]
-	public class ParametersServiceTest
+	public class ParametersServiceTest : MariaDbTestContainerSqlFixtureBase
 	{
+		[OneTimeSetUp]
+		public async Task GlobalSetup()
+		{
+			await InitialiseMariaDb();
+			await PrepareDatabase();
+		}
+
+		private async Task PrepareDatabase()
+		{
+			await RecreateDatabase(DefaultDbName);
+			using (var connection = CreateConnection())
+			{
+				await connection.OpenAsync();
+				await connection.ExecuteAsync(@"
+					CREATE TABLE `base_parameters` (
+						`name` varchar(20) NOT NULL,
+						`str_value` varchar(100) DEFAULT NULL,
+						PRIMARY KEY (`name`)
+					) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+				");
+			}
+		}
+
+		[SetUp]
+		public async Task SetUp()
+		{
+			// Очистка таблицы перед каждым тестом
+			using (var connection = CreateConnection())
+			{
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("DELETE FROM base_parameters;");
+			}
+		}
 		#region GetValueTests
 		[Test(Description = "Проверка что не падаем, а возвращаем null если указанного параметра в базе нет.")]
-		public void GetValue_ReturnNullCase()
+		public async Task GetValue_ReturnNullCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
 				dynamic parameters = new ParametersService(connection);
 				Assert.That(parameters.NotExist, Is.Null);
 			}
 		}
 
 		[Test(Description = "Проверка что успешно читаем строковое значение из базы.")]
-		public void GetValue_ReturnStringCase()
+		public async Task GetValue_ReturnStringCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
-				connection.Execute(sqlInsert, new { name = "GetStringValueTest", str_value = "String result" });
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", 
+					new { name = "GetStringValueTest", str_value = "String result" });
 				dynamic parameters = new ParametersService(connection);
 				Assert.That(parameters.GetStringValueTest, Is.EqualTo("String result"));
 			}
@@ -34,11 +69,12 @@ namespace QS.Test.BaseParameters
 		#region Цифры
 
 		[Test(Description = "Проверка что успешно читаем значение int.")]
-		public void GetValue_ReturnIntCase()
+		public async Task GetValue_ReturnIntCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
-				connection.Execute(sqlInsert, new { name = "Number", str_value = "42" });
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", 
+					new { name = "Number", str_value = "42" });
 				dynamic parameters = new ParametersService(connection);
 				int value = parameters.Number(typeof(int));
 				Assert.That(value, Is.EqualTo(42));
@@ -46,55 +82,60 @@ namespace QS.Test.BaseParameters
 		}
 
 		[Test(Description = "Проверка что успешно читаем значение long.")]
-		public void GetValue_ReturnLongCase()
+		public async Task GetValue_ReturnLongCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
-				connection.Execute(sqlInsert, new { name = "Number", str_value = "9223372036854775807" });
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", 
+					new { name = "Number", str_value = "9223372036854775807" });
 				dynamic parameters = new ParametersService(connection);
 				Assert.That(parameters.Number(typeof(long)), Is.EqualTo(9223372036854775807));
 			}
 		}
 
 		[Test(Description = "Проверка что успешно читаем значение uint.")]
-		public void GetValue_ReturnUIntCase()
+		public async Task GetValue_ReturnUIntCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
-				connection.Execute(sqlInsert, new { name = "Number", str_value = "2" });
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", 
+					new { name = "Number", str_value = "2" });
 				dynamic parameters = new ParametersService(connection);
 				Assert.That(parameters.Number(typeof(uint)), Is.EqualTo(2));
 			}
 		}
 
 		[Test(Description = "Проверка что успешно читаем значение double.")]
-		public void GetValue_ReturnDoubleCase()
+		public async Task GetValue_ReturnDoubleCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
-				connection.Execute(sqlInsert, new { name = "PI", str_value = "3.14159265359" });
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", 
+					new { name = "PI", str_value = "3.14159265359" });
 				dynamic parameters = new ParametersService(connection);
 				Assert.That(parameters.PI(typeof(double)), Is.EqualTo(3.14159265359d));
 			}
 		}
 
 		[Test(Description = "Проверка что успешно читаем значение Decimal.")]
-		public void GetValue_ReturnDecimalCase()
+		public async Task GetValue_ReturnDecimalCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
-				connection.Execute(sqlInsert, new { name = "USD", str_value = "76.76" });
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", 
+					new { name = "USD", str_value = "76.76" });
 				dynamic parameters = new ParametersService(connection);
 				Assert.That(parameters.USD(typeof(decimal)), Is.EqualTo(76.76m));
 			}
 		}
 
 		[Test(Description = "Проверка что успешно читаем значение int?.")]
-		public void GetValue_ReturnIntNulableCase()
+		public async Task GetValue_ReturnIntNulableCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
-				connection.Execute(sqlInsert, new { name = "Number", str_value = 42 });
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", 
+					new { name = "Number", str_value = 42 });
 				dynamic parameters = new ParametersService(connection);
 				int? value = parameters.NotExist(typeof(int?));
 				Assert.That(value, Is.Null);
@@ -106,22 +147,24 @@ namespace QS.Test.BaseParameters
 		#endregion
 
 		[Test(Description = "Проверка что успешно читаем значение Version.")]
-		public void GetValue_ReturnVersionCase()
+		public async Task GetValue_ReturnVersionCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
-				connection.Execute(sqlInsert, new { name = "version", str_value = "2.3.4" });
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", 
+					new { name = "version", str_value = "2.3.4" });
 				dynamic parameters = new ParametersService(connection);
 				Assert.That(parameters.version(typeof(Version)), Is.EqualTo(new Version(2, 3, 4)));
 			}
 		}
 
 		[Test(Description = "Проверка что успешно читаем значение bool.")]
-		public void GetValue_ReturnBooleanCase()
+		public async Task GetValue_ReturnBooleanCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
-				connection.Execute(sqlInsert, new { name = "IsTrue", str_value = "True" });
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", 
+					new { name = "IsTrue", str_value = "True" });
 				dynamic parameters = new ParametersService(connection);
 				Assert.That(parameters.IsTrue(typeof(bool)), Is.EqualTo(true));
 			}
@@ -130,67 +173,67 @@ namespace QS.Test.BaseParameters
 		#endregion
 		#region SetValues
 		[Test(Description = "Проверка что можем создать параметр.")]
-		public void SetValue_NewParameter_StringCase()
+		public async Task SetValue_NewParameter_StringCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
 				dynamic parameters = new ParametersService(connection);
 				parameters.StringParameter = "String result";
 				Assert.That(parameters.StringParameter, Is.EqualTo("String result"));
-				var inDB = connection.ExecuteScalar<string>(sqlSelect, new { name = "StringParameter" });
+				var inDB = await connection.ExecuteScalarAsync<string>("SELECT str_value FROM base_parameters WHERE name = @name;", new { name = "StringParameter" });
 				Assert.That(inDB, Is.EqualTo("String result"));
 			}
 		}
 
 		#region Другие типы
 		[Test(Description = "Проверка что можем создать параметр из int.")]
-		public void SetValue_NewParameter_IntCase()
+		public async Task SetValue_NewParameter_IntCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
 				dynamic parameters = new ParametersService(connection);
 				parameters.Answer = 42;
 				Assert.That(parameters.Answer(typeof(int)), Is.EqualTo(42));
-				var inDB = connection.ExecuteScalar<string>(sqlSelect, new { name = "Answer" });
+				var inDB = await connection.ExecuteScalarAsync<string>("SELECT str_value FROM base_parameters WHERE name = @name;", new { name = "Answer" });
 				Assert.That(inDB, Is.EqualTo("42"));
 			}
 		}
 
 		[Test(Description = "Проверка что можем создать параметр из decimal.")]
-		public void SetValue_NewParameter_DecimalCase()
+		public async Task SetValue_NewParameter_DecimalCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
 				dynamic parameters = new ParametersService(connection);
 				parameters.VodkaPrice = 2.87m;
 				Assert.That(parameters.VodkaPrice(typeof(decimal)), Is.EqualTo(2.87m));
-				var inDB = connection.ExecuteScalar<string>(sqlSelect, new { name = "VodkaPrice" });
+				var inDB = await connection.ExecuteScalarAsync<string>("SELECT str_value FROM base_parameters WHERE name = @name;", new { name = "VodkaPrice" });
 				Assert.That(inDB, Is.EqualTo("2.87"));
 			}
 		}
 
 		[Test(Description = "Проверка что можем создать параметр из bool.")]
-		public void SetValue_NewParameter_BooleanCase()
+		public async Task SetValue_NewParameter_BooleanCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
 				dynamic parameters = new ParametersService(connection);
 				parameters.IsTest = true;
 				Assert.That(parameters.IsTest(typeof(bool)), Is.EqualTo(true));
-				var inDB = connection.ExecuteScalar<string>(sqlSelect, new { name = "IsTest" });
+				var inDB = await connection.ExecuteScalarAsync<string>("SELECT str_value FROM base_parameters WHERE name = @name;", new { name = "IsTest" });
 				Assert.That(inDB, Is.EqualTo("True"));
 			}
 		}
 
 		[Test(Description = "Проверка что можем создать параметр из Version.")]
-		public void SetValue_NewParameter_VersionCase()
+		public async Task SetValue_NewParameter_VersionCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
 				dynamic parameters = new ParametersService(connection);
 				parameters.Version = new Version(2, 4);
 				Assert.That(parameters.Version(typeof(Version)), Is.EqualTo(new Version(2, 4)));
-				var inDB = connection.ExecuteScalar<string>(sqlSelect, new { name = "Version" });
+				var inDB = await connection.ExecuteScalarAsync<string>("SELECT str_value FROM base_parameters WHERE name = @name;", new { name = "Version" });
 				Assert.That(inDB, Is.EqualTo("2.4"));
 			}
 		}
@@ -198,59 +241,48 @@ namespace QS.Test.BaseParameters
 		#endregion
 
 		[Test(Description = "Проверка что можем обновить значение параметра.")]
-		public void SetValue_UpdateParameter_StringCase()
+		public async Task SetValue_UpdateParameter_StringCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
-				connection.Execute(sqlInsert, new { name = "StringParameter", str_value = "String result" });
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", new { name = "StringParameter", str_value = "String result" });
 				dynamic parameters = new ParametersService(connection);
 				parameters.StringParameter = "String2";
 				Assert.That(parameters.StringParameter, Is.EqualTo("String2"));
-				var inDB = connection.ExecuteScalar<string>(sqlSelect, new { name = "StringParameter" });
+				var inDB = await connection.ExecuteScalarAsync<string>("SELECT str_value FROM base_parameters WHERE name = @name;", new { name = "StringParameter" });
 				Assert.That(inDB, Is.EqualTo("String2"));
 			}
 		}
 		
 		[Test(Description = "Проверка что можем обновить значение параметра, если его не было в момент загрузки но появился потом.")]
-		public void SetValue_UpdateParameter_ExistCase()
+		public async Task SetValue_UpdateParameter_ExistCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
 				dynamic parameters = new ParametersService(connection);
 				Assert.That(parameters.StringParameter, Is.Null);
-				connection.Execute(sqlInsert, new { name = "StringParameter", str_value = "String result" });
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", new { name = "StringParameter", str_value = "String result" });
 				parameters.StringParameter = "String2";
 				Assert.That(parameters.StringParameter, Is.EqualTo("String2"));
-				var inDB = connection.ExecuteScalar<string>(sqlSelect, new { name = "StringParameter" });
+				var inDB = await connection.ExecuteScalarAsync<string>("SELECT str_value FROM base_parameters WHERE name = @name;", new { name = "StringParameter" });
 				Assert.That(inDB, Is.EqualTo("String2"));
 			}
 		}
 
-		[Test(Description = "Проверка что можем удалить параметр установил значение в null.")]
-		public void SetValue_RemoveParameter_StringCase()
+		[Test(Description = "Проверка что можем удалить параметр установив значение в null.")]
+		public async Task SetValue_RemoveParameter_StringCase()
 		{
-			using (var connection = new SqliteConnection(connectionString)) {
-				MakeTable(connection);
-				connection.Execute(sqlInsert, new { name = "StringParameter", str_value = "String result" });
+			using (var connection = CreateConnection()) {
+				await connection.OpenAsync();
+				await connection.ExecuteAsync("INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);", new { name = "StringParameter", str_value = "String result" });
 				dynamic parameters = new ParametersService(connection);
 				parameters.StringParameter = null;
 				Assert.That(parameters.StringParameter, Is.Null);
-				var rows = connection.Execute(sqlSelect, new { name = "StringParameter" });
-				Assert.That(rows, Is.EqualTo(0));
+				var count = await connection.ExecuteScalarAsync<int>("SELECT COUNT(*) FROM base_parameters WHERE name = @name;", new { name = "StringParameter" });
+				Assert.That(count, Is.EqualTo(0));
 			}
 		}
 
-		#endregion
-		#region Утилиты
-		const string connectionString = "Data Source=:memory:;Version=3;New=True;";
-		const string sqlInsert = "INSERT INTO base_parameters (name, str_value) VALUES(@name, @str_value);";
-		const string sqlSelect = "SELECT str_value FROM base_parameters WHERE name = @name;";
-
-		void MakeTable(SqliteConnection connection)
-		{
-			connection.Open();
-			connection.Execute("CREATE TABLE `base_parameters` (`name` varchar(20) NOT NULL, `str_value` varchar(100) DEFAULT NULL);");
-		}
 		#endregion
 	}
 }
