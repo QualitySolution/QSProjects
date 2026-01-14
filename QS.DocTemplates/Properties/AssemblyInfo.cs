fdec08a829b82db49cea4a0a6fb681b82ba0ef9b
Gankov <gav@qsolution.ru>
2026-01-14 15:49:35 +0300

    Обновил год авторского права.
    Обновил год авторского права на 2026 во всех файлах AssemblyInfo.cs.

--- QS.DocTemplates/Properties/AssemblyInfo.cs
+++ QS.DocTemplates/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("QS.DocTemplates")]
-[assembly: AssemblyCopyright("Quality Solution 2025")]
+[assembly: AssemblyCopyright("Quality Solution 2026")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
