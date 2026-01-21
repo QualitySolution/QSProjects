55204f72aa653c144a13e35bdb7dcf74a73358f5
Kate <ekaterinatimireva1508@gmail.com>
2026-01-21 11:17:07 +0300

    Добавление значения по умолчанию, переименование параметра

--- QS.Updater.Desktop/VersionCheckerService.cs
+++ QS.Updater.Desktop/VersionCheckerService.cs
@@ -17,10 +17,10 @@ namespace QS.Updater
 			this.dbUpdater = dbUpdater;
 		}
 
-		public UpdateInfo? RunUpdate(bool isOffAutoUpdateChannelActive) 
+		public UpdateInfo? RunUpdate(bool isOffAutoUpdate = false) 
 		{
 			UpdateInfo? updateInfo = null;
-			if (applicationUpdater != null && !isOffAutoUpdateChannelActive) {
+			if (applicationUpdater != null && !isOffAutoUpdate) {
 				updateInfo = applicationUpdater.CheckUpdate(false);
 				if(updateInfo?.Status == UpdateStatus.AppUpdateIsRunning) {
 					return updateInfo;
