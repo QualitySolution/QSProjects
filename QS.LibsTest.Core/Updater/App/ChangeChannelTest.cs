fbdd7a230f121e56b9aadcf7f6ea93fd84292981
Kate <ekaterinatimireva1508@gmail.com>
2026-01-23 18:35:53 +0300

    Добавление передачи параметров в конструктор VersionCheckerService

--- QS.LibsTest.Core/Updater/App/ChangeChannelTest.cs
+++ QS.LibsTest.Core/Updater/App/ChangeChannelTest.cs
@@ -1,5 +1,10 @@
+using Autofac;
 using NSubstitute;
 using NUnit.Framework;
+using QS.BaseParameters;
+using QS.Dialog;
+using QS.Project.Services;
+using QS.Project.Versioning;
 using QS.Updater;
 
 namespace QS.Test.Updater.App {
@@ -9,7 +14,20 @@ namespace QS.Test.Updater.App {
 		[TestCase(true, ExpectedResult = true)]
 		[TestCase(false, ExpectedResult = false)]
 		public bool NotUpdateTest(bool isOffAutoUpdate) {
-			var checker = Substitute.For<VersionCheckerService>();
+			var applicationInfo = Substitute.For<IApplicationInfo>();
+			var parametersService = Substitute.For<ParametersService>();
+			var checkBaseVersion = new CheckBaseVersion(applicationInfo, parametersService);
+			var interactive = Substitute.For<IInteractiveMessage>();
+			var applicationQuitService = Substitute.For<IApplicationQuitService>();
+			var applicationUpdater  = Substitute.For<IAppUpdater>();
+			var dbUpdater = Substitute.For<IDBUpdater>();
+			var checker = Substitute.For<VersionCheckerService>(
+				new TypedParameter(typeof(CheckBaseVersion), checkBaseVersion),
+				new TypedParameter(typeof(IInteractiveMessage), interactive),
+				new TypedParameter(typeof(IApplicationQuitService), applicationQuitService),
+				new TypedParameter(typeof(IAppUpdater), applicationUpdater),
+				new TypedParameter(typeof(IDBUpdater), dbUpdater)
+				);
 			var result = checker.RunUpdate(isOffAutoUpdate);
 			return result == null;
 		}
