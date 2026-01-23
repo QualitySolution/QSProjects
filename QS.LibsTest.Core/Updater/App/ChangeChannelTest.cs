c6149707f7a1853c2d95ec3cef1ac876af7629be
Kate <ekaterinatimireva1508@gmail.com>
2026-01-23 17:43:55 +0300

    Добавление теста проверки получения автообновлений

---
+++ QS.LibsTest.Core/Updater/App/ChangeChannelTest.cs
@@ -0,0 +1,16 @@
+using NSubstitute;
+using NUnit.Framework;
+using QS.Updater;
+
+namespace QS.Test.Updater.App {
+	public class ChangeChannelTest {
+		[Test(Description = "Тест для проверки автообновлений")]
+		[TestCase(true, ExpectedResult = true)]
+		[TestCase(false, ExpectedResult = false)]
+		public bool NotUpdateTest(bool isOffAutoUpdate) {
+			var checker = Substitute.For<VersionCheckerService>();
+			var result = checker.RunUpdate(isOffAutoUpdate);
+			return result == null;
+		}
+	}
+}
