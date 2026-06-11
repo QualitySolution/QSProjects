6cf0c8e2d62703d1a5fcd87cb6957bc2e6cc8512
J0shlerB0y <86416124+J0shlerB0y@users.noreply.github.com>
2026-06-11 14:32:18 +0300

    вернул комментарий

--- QS.Project.Core/Dialog/IProgressBarDisplayable.cs
+++ QS.Project.Core/Dialog/IProgressBarDisplayable.cs
@@ -1,6 +1,9 @@
 using System;
 namespace QS.Dialog
 {
+	/// <summary>
+	/// Интерфейс позволяющий управлять прогресс баром не зависимо от графического тул кита.
+	/// </summary>
 	public interface IProgressBarDisplayable
 	{
 		void Start(double maxValue = 1, double minValue = 0, string text = null, double startValue = 0);
