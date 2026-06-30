34175b47ef2143c835f36f78da6fd764134e762a
Vadim Gordeev <enzogord@gmail.com>
2026-06-30 13:20:19 +0300

    Именованная команда

---
+++ QS.Project.Core/Commands/NamedCommand.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace QS.Commands
+{
+	public class NamedCommand : DelegateCommand {
+
+		public NamedCommand(string name, Action execute) : base(execute) {
+			Name = name;
+		}
+
+		public NamedCommand(string name, Action execute, Func<bool> canExecute) : base(execute, canExecute) {
+			Name = name;
+		}
+		public string Name { get; }
+	}
+}
