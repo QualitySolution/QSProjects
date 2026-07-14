9ad47c256a4fbc7a2edc6c5c7ec2b8258b6add01
J0shlerB0y <86416124+J0shlerB0y@users.noreply.github.com>
2026-07-14 11:08:01 +0300

    доработки управления пользователями

--- QS.Launcher/ViewModels/PageViewModels/UsersVM.cs
+++ QS.Launcher/ViewModels/PageViewModels/UsersVM.cs
@@ -32,7 +32,7 @@ namespace QS.Launcher.ViewModels.PageViewModels {
 			var hasSelectedUser = this.WhenAnyValue(x => x.SelectedUser).Select(u => u != null);
 
 			NewUserCommand = ReactiveCommand.Create(StartNewUser);
-			EditUserCommand = ReactiveCommand.Create(EditUser);
+			EditUserCommand = ReactiveCommand.Create(EditUser, hasSelectedUser);
 			DeleteUserCommand = ReactiveCommand.CreateFromTask(DeleteUserAsync, hasSelectedUser);
 			RefreshUsersCommand = ReactiveCommand.Create(RefreshUsers);
 			BackCommand = ReactiveCommand.Create(() => PopPageCommand?.Execute(null));
@@ -57,17 +57,13 @@ namespace QS.Launcher.ViewModels.PageViewModels {
 		}
 
 		public bool HasSelectedUser => SelectedUser != null;
-		private bool isNewUser;
-		public bool IsNewUser {
-			get => isNewUser;
-			set => this.RaiseAndSetIfChanged(ref isNewUser, value);
-		}
 		public bool CanManageUsers => provider?.CanManageUsers == true;
 
 		public void SetProvider(IDbUserManager userManager) {
 			provider = userManager ?? throw new ArgumentNullException(nameof(userManager));
 
 			this.RaisePropertyChanged(nameof(CanManageUsers));
+			RefreshUsers();
 		}
 
 		public void RefreshUsers() {
@@ -86,15 +82,19 @@ namespace QS.Launcher.ViewModels.PageViewModels {
 		}
 
 		private void EditUser() {
-			var vm = serviceProvider.GetRequiredService<UserManagementVM>();
-			vm.SetContext(provider, SelectedUser, isCreating: false);
-			PushPageCommand?.Execute(vm);
+			OpenEditor(SelectedUser, isCreating: false);
 		}
+
 		private void StartNewUser() {
 			SelectedUser = null;
+			OpenEditor(null, isCreating: true);
+		}
 
+		private void OpenEditor(DbUserInfo user, bool isCreating) {
 			var vm = serviceProvider.GetRequiredService<UserManagementVM>();
-			vm.SetContext(provider, SelectedUser, isCreating: true);
+			vm.SetContext(provider, user, isCreating);
+			vm.OperationCompleted -= RefreshUsers;
+			vm.OperationCompleted += RefreshUsers;
 			PushPageCommand?.Execute(vm);
 		}
 
@@ -115,6 +115,7 @@ namespace QS.Launcher.ViewModels.PageViewModels {
 			try {
 				await Task.Run(() => provider.DeleteUser(user.Login));
 				interactiveMessage.ShowMessage(ImportanceLevel.Success, "Пользователь удалён.", "Управление пользователями");
+				RefreshUsers();
 			}
 			catch(Exception ex) {
 				logger.Error(ex, "Не удалось удалить пользователя {0}", user.Login);
