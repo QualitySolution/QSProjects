bcf426f9d45b231cee9bae4a36cc4fee2957a79f
J0shlerB0y <86416124+J0shlerB0y@users.noreply.github.com>
2026-07-24 18:45:34 +0300

    создание, удаление в QSLauncher

---
+++ QS.DbManagement/Entities/BaseRow.cs
@@ -0,0 +1,22 @@
+using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QS.DbManagement.Entities {
+	internal struct BaseRow {
+		public int AccountId;
+		public byte ProductId;
+		public string Title;
+		public string Name;
+		public string Version;
+
+		public BaseRow(int accountId, byte productId, string title, string name, string version) {
+			AccountId = accountId;
+			ProductId = productId;
+			Title = title;
+			Name = name;
+			Version = version;
+		}
+	}
+}
