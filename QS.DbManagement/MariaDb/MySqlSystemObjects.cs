e26c6b1cc14bfbe794039a1f53ea915b6aaa66e9
J0shlerB0y <86416124+J0shlerB0y@users.noreply.github.com>
2026-08-03 14:37:49 +0300

    вынес логику по обработке доступа в провайдере
    + доработки по тестам провайдера

---
+++ QS.DbManagement/MariaDb/MySqlSystemObjects.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+
+namespace QS.DbManagement.MariaDb {
+	internal static class MySqlSystemObjects {
+		public static readonly IReadOnlyCollection<string> Databases =
+			new[] { "information_schema", "mysql", "performance_schema", "sys" };
+
+		public static readonly IReadOnlyCollection<string> Users =
+			new[] { "root", "mariadb.sys", "mysql", "PUBLIC" };
+	}
+}
