40110fe0f551d7838f292fd61a0d1729c5c9218b
J0shlerB0y <86416124+J0shlerB0y@users.noreply.github.com>
2026-08-29 11:18:23 +0300

    убрал LauncherColumnMapper

--- QS.DbManagement/Entities/BaseUserRow.cs
+++ QS.DbManagement/Entities/BaseUserRow.cs
@@ -1,11 +1,9 @@
 ﻿namespace QS.DbManagement.Entities {
 	internal class BaseUserRow {
-		public int Id { get; set; }
-		public string Name { get; set; }
 		public string Login { get; set; }
-		public bool Deactivated { get; set; }
+		public string Name { get; set; }
 		public string Email { get; set; }
-		public string Description { get; set; }
 		public bool Admin { get; set; }
+		public bool Deactivated { get; set; }
 	}
 }
