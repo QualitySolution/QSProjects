ad5e1941cd0345e87b12d3f98a66c2b0adcb2cff
J0shlerB0y <86416124+J0shlerB0y@users.noreply.github.com>
2026-09-04 22:23:57 +0300

    правильный резолв вм в фабрике и сервисы

--- QS.Project.Avalonia/Navigation/AvaloniaViewFactory.cs
+++ QS.Project.Avalonia/Navigation/AvaloniaViewFactory.cs
@@ -1,21 +1,22 @@
 using Avalonia.Controls;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 
 namespace QS.Navigation;
 
-public class AvaloniaViewFactory(Func<IAvaloniaViewResolver> getViewResolver)
+/// <param name="getService">Достаёт сервис из контейнера приложения или null, если такого нет.
+/// Делегатом, а не контейнером, чтобы библиотека не зависела от конкретного DI.</param>
+public class AvaloniaViewFactory(Func<IAvaloniaViewResolver> getViewResolver, Func<Type, object?> getService)
 {
 	public Control Create(Type viewClass, object viewModel) {
-		// Пытаемся найти конструктор с (ViewModel, IAvaloniaViewResolver)
-		var constructorWithResolver = viewClass.GetConstructor([viewModel.GetType(), typeof(IAvaloniaViewResolver)]);
-		if(constructorWithResolver != null)
-			return (Control)constructorWithResolver.Invoke([viewModel, getViewResolver()]);
-		
-		// Пытаемся найти конструктор с (ViewModel)
-		var constructorWithViewModel = viewClass.GetConstructor([viewModel.GetType()]);
-		if(constructorWithViewModel != null)
-			return (Control)constructorWithViewModel.Invoke([viewModel]);
-		
+		foreach(var constructor in SuitableConstructors(viewClass, viewModel)) {
+			var arguments = GetArgumentsOrDefault(constructor, viewModel);
+			if(arguments != null)
+				return (Control)constructor.Invoke(arguments);
+		}
+
 		// Пытаемся создать через конструктор без параметров
 		var parameterlessConstructor = viewClass.GetConstructor(Type.EmptyTypes);
 		if(parameterlessConstructor != null) {
@@ -24,12 +25,47 @@ public class AvaloniaViewFactory(Func<IAvaloniaViewResolver> getViewResolver)
 			view.DataContext = viewModel;
 			return view;
 		}
-		
+
 		throw new InvalidOperationException(
 			$"Не удалось создать View типа '{viewClass.FullName}'. " +
-			$"View должна иметь один из следующих конструкторов: " +
-			$"({viewModel.GetType().Name}, IAvaloniaViewResolver), " +
-			$"({viewModel.GetType().Name}), " +
-			$"или конструктор без параметров.");
+			$"View должна иметь конструктор, первым параметром которого идёт {viewModel.GetType().Name} " +
+			$"(допускается базовый тип или интерфейс), а остальные параметры разрешаются контейнером, " +
+			$"либо конструктор без параметров.");
 	}
+
+	/// <summary>
+	/// ViewModel подбирается по совместимости типов
+	/// </summary>
+	private static IEnumerable<ConstructorInfo> SuitableConstructors(Type viewClass, object viewModel) =>
+		viewClass.GetConstructors()
+			.Select(constructor => new { Constructor = constructor, Parameters = constructor.GetParameters() })
+			.Where(candidate => candidate.Parameters.Length > 0
+				&& candidate.Parameters[0].ParameterType.IsInstanceOfType(viewModel))
+			// Конструктор под конкретную ViewModel предпочитаем конструктору под интерфейс,
+			// затем куда влезает больше зависимостей
+			.OrderByDescending(candidate => candidate.Parameters[0].ParameterType == viewModel.GetType())
+			.ThenByDescending(candidate => candidate.Parameters.Length)
+			.Select(candidate => candidate.Constructor);
+
+	/// <summary>
+	/// Возвращает аргументы конструктора или null, если какой-то зависимости в контейнере нет
+	/// </summary>
+	private object[]? GetArgumentsOrDefault(ConstructorInfo constructor, object viewModel) {
+		var parameters = constructor.GetParameters();
+		var arguments = new object[parameters.Length];
+		arguments[0] = viewModel;
+
+		for(int i = 1; i < parameters.Length; i++) {
+			var service = GetService(parameters[i].ParameterType);
+			if(service == null)
+				return null;
+			arguments[i] = service;
+		}
+
+		return arguments;
+	}
+
+	// Резолвер вью приходит отложенно: он сам зависит от фабрики, и через контейнер получилась бы петля
+	private object? GetService(Type serviceType) =>
+		serviceType == typeof(IAvaloniaViewResolver) ? getViewResolver() : getService(serviceType);
 }
