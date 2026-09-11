16efe58cd28704f8feaf6d64ba3b3ca2c4185a32
J0shlerB0y <86416124+J0shlerB0y@users.noreply.github.com>
2026-09-11 17:05:53 +0300

    обработка ошибок в журнале

--- QS.Project.Avalonia/Journal/Views/JournalView.axaml.cs
+++ QS.Project.Avalonia/Journal/Views/JournalView.axaml.cs
@@ -66,6 +66,7 @@ public partial class JournalView : UserControl, IDisposable
 		// Подписываемся на события
 		ViewModel.DataLoader.ItemsListUpdated += ViewModel_ItemsListUpdated;
 		ViewModel.DataLoader.LoadingStateChanged += DataLoader_LoadingStateChanged;
+		ViewModel.DataLoader.LoadError += DataLoader_LoadError;
 		ViewModel.PropertyChanged += OnViewModelPropertyChanged;
 
 		UpdateFooter();
@@ -228,6 +229,11 @@ public partial class JournalView : UserControl, IDisposable
 	// FooterInfo считается по загруженным строкам и об изменении не уведомляет
 	private void UpdateFooter() => labelFooter.Text = ViewModel!.FooterInfo;
 
+	// Загрузчик работает в фоне и ошибку запроса только пишет в лог. Перебрасываем её
+	// на поток GUI — оттуда её подхватит общий обработчик, так же делает GTK-версия
+	private void DataLoader_LoadError(object? sender, QS.Project.Journal.DataLoader.LoadErrorEventArgs e) =>
+		guiDispatcher!.RunInGuiTread(() => throw e.Exception);
+
 	private void DataLoader_LoadingStateChanged(object? sender, QS.Project.Journal.DataLoader.LoadingStateChangedEventArgs e)
 	{
 		// Событие может вызываться из фонового потока
@@ -258,6 +264,7 @@ public partial class JournalView : UserControl, IDisposable
 		{
 			ViewModel.DataLoader.ItemsListUpdated -= ViewModel_ItemsListUpdated;
 			ViewModel.DataLoader.LoadingStateChanged -= DataLoader_LoadingStateChanged;
+			ViewModel.DataLoader.LoadError -= DataLoader_LoadError;
 			ViewModel.PropertyChanged -= OnViewModelPropertyChanged;
 		}
 
